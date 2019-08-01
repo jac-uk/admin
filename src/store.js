78ad610fbcdfecb72e9408916f51335eae28bcb5
Ollie Treend <ollie@treend.com>
2019-08-01 12:57:31 +0100

    Configure Vuex
    I've configured Vuex in the new `apply-admin` Vue app to be consistent with configuration in our other apps.
    
    In particular, the use of strict mode in non-production environments is something which I've enabled. It allows us to make sure we're using the Vuex state appropriately as we work with it in development. However due to its performance impact, strict mode is disabled in production builds.

--- src/store.js
+++ src/store.js
@@ -3,14 +3,18 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {
+const store = new Vuex.Store({
+  // Don't use strict mode in production for performance reasons (https://vuex.vuejs.org/guide/strict.html)
+  strict: process.env.NODE_ENV !== 'production',
 
+  modules: {
+    // Add Vuex modules here
   },
-  mutations: {
 
-  },
-  actions: {
-
-  },
+  state: {},
+  mutations: {},
+  actions: {},
+  getters: {},
 });
+
+export default store;
