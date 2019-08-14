6e9f16898590613fee1993ba9ac6718c6ca7239e
alexbokii <vanilinka33@gmail.com>
2019-08-14 17:50:14 +0100

    Add type component

--- tests/unit/views/CreateExercise.spec.js
+++ tests/unit/views/CreateExercise.spec.js
@@ -2,6 +2,7 @@ import Vuex from 'vuex';
 import { shallowMount, createLocalVue } from '@vue/test-utils';
 import CreateExercise from '@/views/CreateExercise';
 import NewExerciseName from '@/components/NewExercise/NewExerciseName';
+import NewExerciseType from '@/components/NewExercise/NewExerciseType';
 import saveNewExercise from '@/helpers/saveNewExercise';
 import VueRouter from 'vue-router';
 
@@ -11,47 +12,112 @@ const localVue = createLocalVue();
 localVue.use(Vuex);
 localVue.use(VueRouter);
 
-let store, wrapper, router;
+describe('CreateExercise', () => {
+  let store, wrapper, router, getters, actions;
 
-beforeEach(() => {
-  const getters = {
-    exerciseData: jest.fn(() => ({ title: 'Test' })),
-  };
+  beforeEach(() => {
+    getters = {
+      exerciseData: jest.fn(() => ({ title: 'Test', type: 'legal' })),
+      exerciseTitle: jest.fn(() => ' Salaried Judge of the Upper Tribunal, Immigration and Asylum Chamber'),
+    };
 
-  store = new Vuex.Store({
-    modules: {
+    actions = {
+      resetNewExercise: jest.fn(),
+    };
+
+    store = new Vuex.Store({
+      modules: {
         createExercise: {
           namespaced: true,
           getters,
+          actions,
         },
       },
-  }); 
+    }); 
 
-  router = new VueRouter();
+    router = new VueRouter();
 
-  wrapper = shallowMount(CreateExercise, {
-    store,
-    router,
-    localVue,
+    wrapper = shallowMount(CreateExercise, {
+      store,
+      router,
+      localVue,
+    });
   });
-});
 
-describe('CreateExercise', () => {
   it('renders the the component', () => {
     expect(wrapper.exists()).toBe(true);
   });
 
-  it('renders the child NewExerciseName component', () => {
-    expect(wrapper.find(NewExerciseName).exists()).toBe(true);
-  });
-
   it('calls saveNewExercise and redirects to dashboard page', (done) => {
-    wrapper.vm.$refs.newExerciseNameComponent.$emit('submitted');
+    wrapper.vm.$refs.newExerciseTypeComponent.$emit('submitted');
     
     setTimeout(() => {
-      expect(saveNewExercise).toHaveBeenCalledWith({ title: 'Test' });
+      expect(saveNewExercise).toHaveBeenCalledWith({ title: 'Test', type: 'legal' });
       expect(wrapper.vm.$route.path).toBe('/dashboard');
+      expect(actions.resetNewExercise.mock.calls.length).toBe(1);
       done();
     }, 0);
+  });
+
+  describe("when title doesn't exist", () => {
+    let store, wrapper, router, getters;
+    beforeEach(() => {
+      getters = {
+        exerciseData: jest.fn(() => ({ title: 'Test', type: 'legal' })),
+        exerciseTitle: jest.fn(() => (null)),
+      };
+
+      store = new Vuex.Store({
+        modules: {
+          createExercise: {
+            namespaced: true,
+            getters,
+          },
+        },
+      }); 
+
+      router = new VueRouter();
+
+      wrapper = shallowMount(CreateExercise, {
+        store,
+        router,
+        localVue,
+      });
+    });
+
+    it('renders the child NewExerciseName component if the title does not exist', () => {
+      expect(wrapper.find(NewExerciseName).exists()).toBe(true);
+    });
+  });
+
+  describe("when title doesn't exist", () => {
+    let store, wrapper, router;
+    beforeEach(() => {
+      const getters = {
+        exerciseData: jest.fn(() => ({ title: 'Test', type: 'legal' })),
+        exerciseTitle: jest.fn(() => ' Salaried Judge of the Upper Tribunal, Immigration and Asylum Chamber'),
+      };
+
+      store = new Vuex.Store({
+        modules: {
+          createExercise: {
+            namespaced: true,
+            getters,
+          },
+        },
+      }); 
+
+      router = new VueRouter();
+
+      wrapper = shallowMount(CreateExercise, {
+        store,
+        router,
+        localVue,
+      });
+    });
+
+    it('renders the child NewExerciseType component if the title exists', () => {
+      expect(wrapper.find(NewExerciseType).exists()).toBe(true);
+    });
   });
 });
