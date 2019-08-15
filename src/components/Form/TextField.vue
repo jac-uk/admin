5569447ae6e9f7aa20958b74540c91d6adabf109
alexbokii <vanilinka33@gmail.com>
2019-08-15 14:50:23 +0100

    Create a TextField component

---
+++ src/components/Form/TextField.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="govuk-form-group">
+    <label
+      :for="id"
+      class="govuk-heading-m" >
+      {{label}}
+    </label>
+    <span 
+      v-if="hint"
+      id="event-name-hint" 
+      class="govuk-hint">
+      {{hint}}
+    </span>
+    <input 
+      :class="[ narrow ? 'govuk-input--width-5 govuk-input' : 'govuk-input' ]"
+      id="key" 
+      name="key" 
+      type="text" 
+      v-model="textFieldValue">
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        textFieldValue: ''
+      };
+    },
+    props: {
+      label: String,
+      hint: String,
+      id: String,
+      narrow: Boolean
+    }
+  }
+</script>
