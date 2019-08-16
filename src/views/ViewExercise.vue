b02c7372fca6f67c1564975dcfe83a5f9de06f30
alexbokii <vanilinka33@gmail.com>
2019-08-16 12:38:22 +0100

    Add ViewExercise component

---
+++ src/views/ViewExercise.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="govuk-grid-row">
+      <div class="govuk-grid-column-full">
+        <span class="govuk-caption-xl">003</span>
+        <h1 class="govuk-heading-xl">
+          Salaried Judge of the Upper Tribunal, Immigration and Asylum Chamber
+        </h1>
+
+        <router-link 
+          :to="{name: 'number-of-vacancies'}" 
+          class="govuk-button"
+        >
+          Add information about the role
+        </router-link>
+
+        <div>
+          <a href="role-combined.html">Role combined form</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+};
+</script>
---
+++ tests/unit/views/ViewExercise.spec.js
@@ -0,0 +1,27 @@
+import ViewExercise from "@/views/ViewExercise";
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import Router from 'vue-router';
+
+const localVue = createLocalVue();
+localVue.use(Router);
+
+describe('ViewExercise view', () => {
+  it('renders the component', () => {
+    let wrapper = shallowMount(ViewExercise, {
+      localVue
+    });
+    
+    expect(wrapper.exists()).toBe(true);
+  });
+});
+
+describe('Accessibility:', () => {
+  it('page contains h1 element', () => { 
+    let wrapper = shallowMount(ViewExercise, {
+      localVue,
+    });
+
+    expect(wrapper.contains('h1')).toBe(true);
+  });
+});
+
