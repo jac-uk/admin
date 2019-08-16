8bfc454bf6b5cab7e9f1f33298e186f53d669070
alexbokii <vanilinka33@gmail.com>
2019-08-16 12:31:19 +0100

    Use CreateExercise only as empty parent component
    We are removing submit logic for now, so CreateExercise is not used for anything except for rendering child routes

--- src/views/CreateExercise.vue
+++ src/views/CreateExercise.vue
@@ -1,42 +1,10 @@
 <template>
   <div>
-    <NewExerciseName
-      v-if="!exerciseTitle"
-      ref="newExerciseNameComponent"
-    />
-    <NewExerciseType
-      v-else
-      ref="newExerciseTypeComponent"
-      @submitted="saveExercise"
-    />
+    <RouterView />
   </div>
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex';
-import saveNewExercise from '@/helpers/saveNewExercise';
-
-import NewExerciseName from '@/components/NewExercise/NewExerciseName';
-import NewExerciseType from '@/components/NewExercise/NewExerciseType';
-
-export default {
-  components: {
-    NewExerciseName,
-    NewExerciseType,
-  },
-  computed: {
-    ...mapGetters('createExercise', [
-      'exerciseData', 'exerciseTitle',
-    ]),
-  },
-  methods: {
-    ...mapActions('createExercise', [
-      'resetNewExercise']),
-    async saveExercise () {
-      await saveNewExercise(this.exerciseData);
-      this.$router.push('dashboard');
-      this.$store.dispatch('createExercise/resetNewExercise');
-    },
-  },
-};
+export default {};
 </script>
+
