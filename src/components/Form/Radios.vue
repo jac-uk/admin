df4faaede9f63ae0f7d1d036fe68189a14ae5c43
Ollie Treend <ollie@treend.com>
2019-08-19 15:03:54 +0100

    Initial commit of radios component
    This is an initial commit of `Radios` and and `RadioItem` components.
    
    These will output a group of GOV.UK-styled radio inputs, in line with the [official GDS documentation for radios][1].
    
    For now this is somewhat of a work in progress because I still need to add test coverage for these new components.
    
    [1]: https://design-system.service.gov.uk/components/radios/

---
+++ src/components/Form/Radios.vue
@@ -0,0 +1,67 @@
+<template>
+  <div
+    :id="id"
+    class="govuk-form-group"
+  >
+    <fieldset
+      class="govuk-fieldset"
+      :aria-describedby="hint ? hintId : false"
+    >
+      <legend class="govuk-fieldset__legend govuk-fieldset__legend--xl">
+        {{ label }}
+      </legend>
+      <span
+        v-if="hint"
+        :id="hintId"
+        class="govuk-hint"
+      >
+        {{ hint }}
+      </span>
+      <div class="govuk-radios">
+        <slot />
+      </div>
+    </fieldset>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Radios',
+  props: {
+    label: {
+      required: true,
+      type: String,
+    },
+    hint: {
+      default: '',
+      type: String,
+    },
+    id: {
+      required: true,
+      type: String,
+    },
+    value: {
+      required: true,
+      validator: (value) => (value instanceof Array || typeof value === 'string' || value === null || value === undefined),
+    },
+  },
+  data() {
+    return {
+      inputType: 'radio',
+    };
+  },
+  computed: {
+    inputValue: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit('input', value);
+      },
+    },
+    hintId() {
+      return `${this.id}__hint`;
+    },
+  },
+};
+</script>
