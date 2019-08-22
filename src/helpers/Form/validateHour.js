b11ce1d56d3cf239a8ac16be1891eaa5bc509a68
alexbokii <vanilinka33@gmail.com>
2019-08-22 15:27:28 +0100

    Add helpers for TimeInput

---
+++ src/helpers/Form/validateHour.js
@@ -0,0 +1,29 @@
+const validateHour = (value) => {
+  if(value === null) {
+    return null
+  }
+
+  if(value < 0) {
+    return 0;
+  }
+
+  if(value === 24) {
+    return 0; 
+  }
+
+  if(value === "00") {
+    return 0; 
+  }
+
+  if(value > 23) {
+    return 23;
+  }
+
+  if(isNaN(value)) {
+    return 0;
+  }
+
+  return value;
+}
+
+export default validateHour;
