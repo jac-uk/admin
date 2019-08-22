2d4e580baa4baa8a5295526ad7ef5b7810e29a74
alexbokii <vanilinka33@gmail.com>
2019-08-22 17:28:24 +0100

    Fix linting errors

--- tests/unit/helpers/Form/validateHour.spec.js
+++ tests/unit/helpers/Form/validateHour.spec.js
@@ -1,32 +1,32 @@
-import validateHour from "@/helpers/Form/validateHour";
+import validateHour from '@/helpers/Form/validateHour';
 
 
-describe("helpers/validateHour", () => {
-  it("returns null if value passed is null", () => {
+describe('helpers/validateHour', () => {
+  it('returns null if value passed is null', () => {
     expect(validateHour(null)).toBe(null);
-  })
+  });
 
-  it("returns 0 if value passed is less than 0", () => {
+  it('returns 0 if value passed is less than 0', () => {
     expect(validateHour(-1)).toBe(0);
-  })
+  });
 
-  it("returns 0 if value is not a number", () => {
-    expect(validateHour("Five")).toBe(0);
-  })
+  it('returns 0 if value is not a number', () => {
+    expect(validateHour('Five')).toBe(0);
+  });
 
   it("returns 0 if value is '00'", () => {
-    expect(validateHour("00")).toBe(0);
-  })
+    expect(validateHour('00')).toBe(0);
+  });
 
-  it("returns 24 if value is higher than 23", () => {
-    expect(validateHour(24)).toBe(23);
-  })
+  it('returns 23 if value is higher than 23 but not 24', () => {
+    expect(validateHour(34)).toBe(23);
+  });
 
-  it("returns 0 if value is 24", () => {
+  it('returns 0 if value is 24', () => {
     expect(validateHour(24)).toBe(0);
-  })
+  });
 
-  it("returns the value passed", () => {
+  it('returns the value passed', () => {
     expect(validateHour(3)).toBe(3);
-  })
-})
+  });
+});
--- tests/unit/helpers/Form/validateMinute.spec.js
+++ tests/unit/helpers/Form/validateMinute.spec.js
@@ -1,24 +1,24 @@
-import validateMinute from "@/helpers/Form/validateMinute";
+import validateMinute from '@/helpers/Form/validateMinute';
 
 
-describe("helpers/validateMinute", () => {
-  it("returns null if value passed is null", () => {
+describe('helpers/validateMinute', () => {
+  it('returns null if value passed is null', () => {
     expect(validateMinute(null)).toBe(null);
-  })
+  });
 
-  it("returns 0 if value passed is less than 0", () => {
+  it('returns 0 if value passed is less than 0', () => {
     expect(validateMinute(-1)).toBe(0);
-  })
+  });
 
-  it("returns 0 if value is not a number", () => {
-    expect(validateMinute("Five")).toBe(0);
-  })
+  it('returns 0 if value is not a number', () => {
+    expect(validateMinute('Five')).toBe(0);
+  });
 
-  it("returns 59 if value is higher than 59", () => {
+  it('returns 59 if value is higher than 59', () => {
     expect(validateMinute(60)).toBe(59);
-  })
+  });
 
-  it("returns the value passed", () => {
+  it('returns the value passed', () => {
     expect(validateMinute(3)).toBe(3);
-  })
-})
+  });
+});
