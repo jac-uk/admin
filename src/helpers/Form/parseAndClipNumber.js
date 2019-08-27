b6ae8b8bca729abb72556e4013f6ce53f22095a7
alexbokii <vanilinka33@gmail.com>
2019-08-27 15:35:24 +0100

    Use parseAndClipNumber instead of date/time helpers
    Implementation of Ollie's suggestion - instead of using the separate component for every type of date or time inputs, we will use a parseAndClipNumber helper. It means that the component is responsible for passing the range of acceptable values.

--- src/helpers/Form/parseAndClipNumber.js
+++ src/helpers/Form/parseAndClipNumber.js
@@ -1,13 +1,13 @@
 import clipNumber from '@/helpers/Form/clipNumber';
 
-const validateDay = (val) => {
+const parseAndClipNumber = (val, min, max) => {
   val = parseInt(val);
-  
+
   if (isNaN(val)) {
     return null;
   }
 
-  return clipNumber(val, 1, 31);
+  return clipNumber(val, min, max);
 };
 
-export default validateDay;
+export default parseAndClipNumber;
