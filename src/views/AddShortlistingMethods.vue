0200269219663c6cd89a5e54020df209622a9cd9
Lauren Qurashi <lauren.qurashi@me.com>
2019-08-30 14:37:31 +0100

    Added features to the Add Shortlisting Methods page
    - Scaffolding of checkbox components is now completed, along with page 
    routing.

---
+++ src/views/AddShortlistingMethods.vue
@@ -0,0 +1,80 @@
+<template>
+  <main class="govuk-main-wrapper">
+    <div class="govuk-grid-row">
+      <div class="govuk-grid-column-two-thirds">
+        <h1 class="govuk-heading-xl">
+          Add shortlisting methods
+        </h1>
+
+        <p class="govuk-body-l">
+          You can return to this page later to add or change methods.
+        </p>
+
+        <CheckboxGroup
+          id="shortlisting-methods"
+          v-model="shortlistingMethods"
+        >
+          <CheckboxItem
+            value="situational-judgement-test"
+            label="Situational judgement test"
+          />
+          <CheckboxItem
+            value="critical-analysis-test"
+            label="Critical analysis test"
+          />
+          <CheckboxItem
+            value="scenario-test"
+            label="Scenario test"
+          />
+          <CheckboxItem
+            value="paper-sift"
+            label="Paper sift"
+          />
+          <CheckboxItem
+            value="telephone-assessment"
+            label="Telephone assessment"
+          />
+          <CheckboxItem
+            value="other"
+            label="Other"
+          >
+            <div class="govuk-form-group">
+              <TextField
+                id="other-text-input"
+                v-model="otherText"
+                name="other-text-input"
+                label="Add method"
+              />
+              <p class="govuk_body">
+                <a
+                  href="#"
+                  class="govuk-link"
+                >Add another</a>
+              </p>
+            </div>
+          </CheckboxItem>
+        </CheckboxGroup>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+import CheckboxGroup from '@/components/Form/CheckboxGroup';
+import CheckboxItem from '@/components/Form/CheckboxItem';
+import TextField from '@/components/Form/TextField';
+
+export default {
+  components: {
+    CheckboxGroup,
+    CheckboxItem,
+    TextField,
+  },
+  data(){
+    return {
+      shortlistingMethods: [],
+      otherText: '',
+    };
+  },
+};
+</script>
