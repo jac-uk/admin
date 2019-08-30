5d07542f0eeb2063c88f2e5ddaa371eb72dd7923
alexbokii <vanilinka33@gmail.com>
2019-08-30 12:01:38 +0100

    Update Dashboard page styling
    Update styling and move buttons to the top as per prototype

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,34 +1,44 @@
 <template>
   <div>
-    <h1 class="govuk-heading-xl">
-      Dashboard
-    </h1>
-    <h2 class="govuk-heading-l">
+    <div class="govuk-grid-row">
+      <div class="govuk-grid-column-full">
+        <div class="text-right">
+          <a 
+            href="#" 
+            class="govuk-button govuk-button--secondary govuk-!-margin-right-3"
+          >
+            View all exercises
+          </a>
+          <router-link
+            ref="linkToNewExercise"
+            to="/exercise/new/create-an-exercise"
+            class="govuk-button govuk-!-margin-right-1"
+          >
+            Create an exercise
+          </router-link>
+        </div>
+      </div>
+    </div>
+    <h1 class="govuk-heading-xl govuk-!-margin-bottom-6">
       Your exercises
-    </h2>
-    <ul
-      class="govuk-list"
-    >
-      <li
-        v-for="vacancy in vacancies"
-        :key="vacancy.reference"
-      >
-        <router-link
-          to="/exercise/view"
-          class="govuk-link"
-        >
-          {{ vacancy.reference }} {{ vacancy.title }}
-        </router-link>
-      </li>
-    </ul>
-    <div>
-      <router-link
-        ref="linkToNewExercise"
-        to="/exercise/new/create-an-exercise"
-        class="govuk-button govuk-!-margin-top-9"
-      >
-        Create an exercise
-      </router-link>
+    </h1>
+    <div class="govuk-grid-row">
+      <div class="govuk-grid-column-full">
+        <dl class="govuk-summary-list govuk-!-margin-bottom-9">
+          <div 
+            v-for="vacancy in vacancies"
+            :key="vacancy.reference"
+            class="govuk-summary-list__row"
+          >
+            <dt class="govuk-summary-list__key">
+              <a href="detail">{{ vacancy.reference }}</a>
+            </dt>
+            <dd class="govuk-summary-list__value">
+              <a href="detail">{{ vacancy.title }}</a>
+            </dd>
+          </div>
+        </dl>
+      </div>
     </div>
   </div>
 </template>
@@ -42,3 +52,17 @@ export default {
   ]),
 };
 </script>
+
+<style>
+  .govuk-summary-list__key {
+    margin-bottom: 5px;
+    font-weight: 700;
+    width: 15%;
+  }
+
+  @media (min-width: 40.0625em) {
+    .govuk-summary-list__key {
+      width: 30px;
+    }
+  }
+</style>
