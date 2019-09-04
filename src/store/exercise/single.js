f7aab53a6628b623b96123e963f0d36855b7faee
Ollie Treend <ollie@treend.com>
2019-09-04 15:38:14 +0100

    Add `unbind` actions to exercise Vuex modules
    The `unbind` method unsubscribes from changes to the Firestore document or query, and clears the local state of those records.
    
    This can be used to ensure we don't keep old unused bindings in memory. For example, when navigating away from an exercise details page, we are no longer interested in that document so can free up memory by unbinding it.

--- src/store/exercise/single.js
+++ src/store/exercise/single.js
@@ -10,6 +10,9 @@ export default {
         .doc(id);
       return bindFirestoreRef('record', firestoreRef);
     }),
+    unbind: firestoreAction(({ unbindFirestoreRef }) => {
+      return unbindFirestoreRef('record');
+    }),
   },
   state: {
     record: null,
