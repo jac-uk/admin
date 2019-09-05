e9b39b701b477b8ec1ad40c8530b5cdfae8998f3
Ollie Treend <ollie@treend.com>
2019-09-05 11:05:54 +0100

    Move the 'Create an exercise' page
    - Move and rename the page component file
    - Change the route path for the page
    - Change associated tests

--- tests/unit/views/Exercises/New.spec.js
+++ tests/unit/views/Exercises/New.spec.js
@@ -1,32 +1,32 @@
-import CreateAnExercise from '@/views/CreateAnExercise';
+import New from '@/views/Exercises/New';
 import { shallowMount, createLocalVue } from '@vue/test-utils';
 import Router from 'vue-router';
 
 const localVue = createLocalVue();
 localVue.use(Router);
 
-describe('CreateAnExercise', () => {
+describe('views/Exercises/New', () => {
   it('renders the view', () => {
-    let wrapper = shallowMount(CreateAnExercise, {
+    let wrapper = shallowMount(New, {
       localVue,
     });
     expect(wrapper.exists()).toBe(true);
   });
 
   it('contains the link to Add Exercise Contacts page', () => {
-    let wrapper = shallowMount(CreateAnExercise, {
+    let wrapper = shallowMount(New, {
       localVue,
     });
     expect(wrapper.find({ ref: 'linkToAddExerciseContacts' }).isVisible()).toBe(true);
   });
-});
 
-describe('Accessibility:', () => {
-  it('page contains h1 element', () => {
-    let wrapper = shallowMount(CreateAnExercise, {
-      localVue,
+  describe('Accessibility:', () => {
+    it('page contains h1 element', () => {
+      let wrapper = shallowMount(New, {
+        localVue,
+      });
+
+      expect(wrapper.contains('h1')).toBe(true);
     });
-
-    expect(wrapper.contains('h1')).toBe(true);
   });
 });
