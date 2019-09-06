732a4e47326dcf284dcd6a5dd973769954332d2e
Ollie Treend <ollie@treend.com>
2019-09-06 15:26:54 +0100

    Save to database from 'Create new exercise' page
    The overall page template for 'Create new exercise' has been refactored so that all form fields are contained within a `<form>` element.
    
    ### Refactor form template
    
    - The page is now wrapped in a `<form>` element
    - The form element has a `@submit.prevent` handler to handle form submissions (and prevent the browser's default form submission behaviour)
    - The `@click` event handler has been removed from the 'Save and continue' button, as this will now submit the parent form by default
    - This improves accessibility by allowing users to hit 'Enter' in any form field to submit the form, which is the expected behaviour of forms in the browser
    
    ### Add `save` method
    
    - New method `save` dispatches a `exerciseDocument/create` event to the Vuex store
    - This results in a new document being created in the Firestore database
    - For now it will save the exercise name as `name`, and the exercise type as `type` fields in the database
    
    ### Refactored tests
    
    - `describe('template')` block is for testing template / DOM elements
    - `describe('methods')` block is for testing methods of the component – in this case, the `save` method

--- src/views/Exercises/New.vue
+++ src/views/Exercises/New.vue
@@ -1,79 +1,77 @@
 <template>
   <div class="govuk-grid-row">
-    <div class="govuk-grid-column-two-thirds">
-      <h1 class="govuk-heading-xl">
-        Create an exercise
-      </h1>
-      <div class="govuk-form-group">
-        <label
-          for="ref-number"
-          class="govuk-heading-m"
+    <form @submit.prevent="save">
+      <div class="govuk-grid-column-two-thirds">
+        <h1 class="govuk-heading-xl">
+          Create an exercise
+        </h1>
+        <div class="govuk-form-group">
+          <label
+            for="ref-number"
+            class="govuk-heading-m"
+          >
+            Reference number
+          </label>
+          <span class="govuk-hint">
+            This will be generated automatically when you save.
+          </span>
+          <input
+            id="ref-number"
+            class="govuk-input--width-5 govuk-input"
+            type="text"
+            placeholder="XXX"
+            disabled
+          >
+        </div>
+        <TextField
+          id="exercise-name"
+          v-model="exerciseName"
+          name="exercise-name"
+          label="Exercise name"
+          hint="Reference codes are automatically created - you do not need to enter one."
+        />
+        <RadioGroup
+          id="type-of-exercise"
+          v-model="exerciseType"
+          label="Type of exercise"
         >
-          Reference number
-        </label>
-        <span class="govuk-hint">
-          This will be generated automatically when you save.
-        </span>
-        <input
-          id="ref-number"
-          class="govuk-input--width-5 govuk-input"
-          type="text"
-          placeholder="XXX"
-          disabled
+          <RadioItem
+            value="legal"
+            label="Legal"
+          />
+          <RadioItem
+            value="non-legal"
+            label="Non legal"
+          />
+          <RadioItem
+            value="senior"
+            label="Senior"
+          />
+          <RadioItem
+            value="leadership"
+            label="Leadership"
+          />
+        </RadioGroup>
+        <RadioGroup
+          id="add-information"
+          v-model="isMoreInfoNeeded"
+          label="Do you want to add more information about this exercise now?"
+          hint="You can add exercise contacts, shortlisting methods, timeline dates, or information from HMCTS. You can also do this later"
         >
+          <RadioItem
+            :value="true"
+            label="Yes"
+          />
+          <RadioItem
+            :value="false"
+            label="No - I'll do this later"
+          />
+        </RadioGroup>
+        <button class="govuk-button">
+          Save and continue
+        </button>
       </div>
-      <TextField
-        id="exercise-name"
-        v-model="exerciseName"
-        name="exercise-name"
-        label="Exercise name"
-        hint="Reference codes are automatically created - you do not need to enter one."
-      />
-      <RadioGroup
-        id="type-of-exercise"
-        v-model="exerciseType"
-        label="Type of exercise"
-      >
-        <RadioItem
-          value="legal"
-          label="Legal"
-        />
-        <RadioItem
-          value="non-legal"
-          label="Non legal"
-        />
-        <RadioItem
-          value="senior"
-          label="Senior"
-        />
-        <RadioItem
-          value="leadership"
-          label="Leadership"
-        />
-      </RadioGroup>
-      <RadioGroup
-        id="add-information"
-        v-model="isMoreInfoNeeded"
-        label="Do you want to add more information about this exercise now?"
-        hint="You can add exercise contacts, shortlisting methods, timeline dates, or information from HMCTS. You can also do this later"
-      >
-        <RadioItem
-          :value="true"
-          label="Yes"
-        />
-        <RadioItem
-          :value="false"
-          label="No - I'll do this later"
-        />
-      </RadioGroup>
-      <button
-        ref="linkToAddExerciseContacts"
-        class="govuk-button"
-        @click="$router.push({ name: 'exercise-edit-contacts', params: { id: 'example' } })"
-      >
-        Save and continue
-      </button>
-    </div>
+    </form>
   </div>
 </template>
 
@@ -88,12 +86,21 @@ export default {
     RadioGroup,
     RadioItem,
   },
-  data(){
+  data() {
     return {
       exerciseName: null,
       exerciseType: null,
       isMoreInfoNeeded: null,
     };
+  },
+  methods: {
+    async save() {
+      const data = {
+        name: this.exerciseName,
+        type: this.exerciseType,
+      };
+      await this.$store.dispatch('exerciseDocument/create', data);
+    },
   },
 };
 </script>
