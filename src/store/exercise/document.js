2c9d4630cfa30e6f34f357b979a56620d1bca245
Ollie Treend <ollie@treend.com>
2019-09-06 14:16:25 +0100

    Add `create` action to `exerciseDocument` Vuex module
    The `create` action can be used to create a new document in the `exercises` collection.
    
    After successful creation, it will bind the Vuex state to the newly created document.

--- src/store/exercise/document.js
+++ src/store/exercise/document.js
@@ -1,18 +1,22 @@
 import { firestore } from '@/firebase';
 import { firestoreAction } from 'vuexfire';
 
+const collection = firestore.collection('exercises');
+
 export default {
   namespaced: true,
   actions: {
     bind: firestoreAction(({ bindFirestoreRef }, id) => {
-      const firestoreRef = firestore
-        .collection('exercises')
-        .doc(id);
+      const firestoreRef = collection.doc(id);
       return bindFirestoreRef('record', firestoreRef);
     }),
     unbind: firestoreAction(({ unbindFirestoreRef }) => {
       return unbindFirestoreRef('record');
     }),
+    create: async ({ dispatch }, data) => {
+      const ref = await collection.add(data);
+      return dispatch('bind', ref.id);
+    },
   },
   state: {
     record: null,
