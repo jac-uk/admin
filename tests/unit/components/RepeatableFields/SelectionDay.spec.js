0fbbedef1271d93ff48232e1b11ca788f555e1ec
alexbokii <vanilinka33@gmail.com>
2019-09-08 21:11:48 +0100

    Added SelectionDay component

---
+++ tests/unit/components/RepeatableFields/SelectionDay.spec.js
@@ -0,0 +1,68 @@
+import { shallowMount } from '@vue/test-utils';
+import SelectionDay from '@/components/RepeatableFields/SelectionDay';
+import DateInput from '@/components/Form/DateInput';
+
+const createTestSubject = (props) => {
+  return shallowMount(SelectionDay, {
+    propsData: {
+      ...props,
+    },
+  });
+};
+
+describe('@/components/RepeatableFields/SelectionDay', () => {
+  describe('name', () => {
+    it('component name is "SelectionDay"', () => {
+      expect(SelectionDay.name).toBe('SelectionDay');
+    });
+  });
+
+  describe('props', () => {
+    describe('row', () => {
+      it('is required', () => {
+        let prop = SelectionDay.props.row;
+        expect(prop.required).toBe(true);
+      });
+
+      it('has type object', () => {
+        let prop = SelectionDay.props.row;
+        expect(prop.type).toBe(Object);
+      });
+    });
+
+    describe('index', () => {
+      it('is required', () => {
+        let prop = SelectionDay.props.index;
+        expect(prop.required).toBe(true);
+      });
+
+      it('has type number', () => {
+        let prop = SelectionDay.props.index;
+        expect(prop.type).toBe(Number);
+      });
+    });
+  });
+
+  describe('computed properties', () => {
+    describe('selectionDayIdStart', () => {
+      it('returns the value that is created using index', () => {
+        let wrapper = createTestSubject({ index: 4, row: {} });
+        expect(wrapper.vm.selectionDayIdStart).toBe('selection_start_4');
+      });
+    });
+
+    describe('selectionDayIdEnd', () => {
+      it('returns the value that is created using index', () => {
+        let wrapper = createTestSubject({ index: 5, row: {} });
+        expect(wrapper.vm.selectionDayIdStart).toBe('selection_start_5');
+      });
+    });
+  });
+
+  describe('template', () => {
+    it('renders DateInput', () => {
+      let wrapper = createTestSubject({ index: 1, row: {} });
+      expect(wrapper.find(DateInput).exists()).toBe(true);
+    });
+  });
+});
