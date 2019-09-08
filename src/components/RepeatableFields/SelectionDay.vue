567b40acc477799d02260ae9b7c79cdfd507f0a5
alexbokii <vanilinka33@gmail.com>
2019-09-08 21:11:48 +0100

    Added SelectionDay component

---
+++ src/components/RepeatableFields/SelectionDay.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <DateInput
+      :id="selectionDayIdStart"
+      v-model="row.selectionDayStart"
+      label="Selection day start"
+    />
+    <DateInput
+      :id="selectionDayIdEnd"
+      v-model="row.selectionDayEnd"
+      label="Selection day end"
+    />
+    <slot name="removeButton" />
+  </div>
+</template>
+
+<script>
+import DateInput from '@/components/Form/DateInput';
+
+export default {
+  name: 'SelectionDay',
+  components: {
+    DateInput,
+  },
+  props: {
+    row: {
+      required: true,
+      type: Object,
+    },
+    index: {
+      required: true,
+      type: Number,
+    },
+  },
+  computed: {
+    selectionDayIdStart() {
+      return `selection_start_${this.index}`;
+    },
+    selectionDayIdEnd() {
+      return `selection_end_${this.index}`;
+    },
+  },
+};
+</script>
