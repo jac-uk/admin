969326781b0456cd8373c41471a986b943b76b82
alexbokii <vanilinka33@gmail.com>
2019-09-10 13:59:36 +0100

    Remove CSS that hides repeatable field label
    As we don't have this functionality in prototype at the moment, we don't want to do any changes to labels for now

--- src/components/RepeatableFields.vue
+++ src/components/RepeatableFields.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="repeatableContainer">
+  <div>
     <div
       v-for="(row, index) in rows"
       :key="index"
@@ -108,15 +108,4 @@ export default {
   .repeatableField .govuk-form-group {
     margin-bottom: 1em;
   }
-
-  .repeatableContainer .repeatableField:not(:first-child) label {
-    position: absolute;
-    width: 1px;
-    height: 1px;
-    padding: 0;
-    margin: -1px;
-    overflow: hidden;
-    clip: rect(0,0,0,0);
-    border: 0;
-  } 
 </style>
