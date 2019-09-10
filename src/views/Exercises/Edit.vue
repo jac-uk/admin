7d40eae5bf4a8045394d3961d2207cb87a8f671d
alexbokii <vanilinka33@gmail.com>
2019-09-10 17:45:49 +0100

    Fix linting errors

--- src/views/Exercises/Edit.vue
+++ src/views/Exercises/Edit.vue
@@ -10,31 +10,30 @@
     >
       <RouterView />
     </div>
-
   </div>
 </template>
 
 <script>
 import LoadingMessage from '@/components/LoadingMessage';
 export default {
+  components: {
+    LoadingMessage,
+  },
   data() {
     return {
       loaded: false,
       loadFailed: false,
-    }
-  },
-  components: {
-    LoadingMessage
+    };
   },
   mounted() {
     const id = this.$route.params.id;
     
     this.$store.dispatch('exerciseDocument/bind', id)
-    .then(() => {
-      this.loaded = true;
-    }).catch(() => {
-      this.loadFailed = true;
-    });
-  }
+      .then(() => {
+        this.loaded = true;
+      }).catch(() => {
+        this.loadFailed = true;
+      });
+  },
 };
 </script>
