781476da1717dadf81af7e04b8a9b6e13ff2cf60
alexbokii <vanilinka33@gmail.com>
2019-09-10 11:27:59 +0100

    Add repeatableTextFields and test

---
+++ src/components/RepeatableFields/StatutoryConsultee.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <TextField 
+      :id="`statutory_consultee_${index}`"
+      v-model="row.name"
+      type="text"
+      label="Statutory Consultee"
+    />
+    <slot name="removeButton" />
+  </div>
+</template>
+
+<script>
+import TextField from '@/components/Form/TextField';
+
+export default {
+  components: {
+    TextField,
+  },
+  props: {
+    row: {
+      required: true,
+      type: Object,
+    },
+    index: {
+      required: true,
+      type: Number,
+    },
+  },
+};
+</script>
