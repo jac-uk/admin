bbd7f2ab99a82e6504db94d63e9825d7eacae802
Ollie Treend <ollie@treend.com>
2019-09-10 11:41:37 +0100

    Use serializer on `exerciseCollection` module
    I've configured the `exerciseCollection` Vuex module to use the new `vuexfireSanitize` helper to sanitise document data. This converts Firestore document data before it enters our state.
    
    One other subtle change I've made is to remove the `orderBy` sorting on the exercises collection. This field is not used by records in this collection yet, so we shouldn't be sorting by this field.

--- src/store/exercise/collection.js
+++ src/store/exercise/collection.js
@@ -1,14 +1,14 @@
 import { firestore } from '@/firebase';
 import { firestoreAction } from 'vuexfire';
+import vuexfireSerialize from '@/helpers/vuexfireSerialize';
 
 export default {
   namespaced: true,
   actions: {
     bind: firestoreAction(({ bindFirestoreRef }) => {
       const firestoreRef = firestore
-        .collection('exercises')
-        .orderBy('openAt', 'desc');
-      return bindFirestoreRef('records', firestoreRef);
+        .collection('exercises');
+      return bindFirestoreRef('records', firestoreRef, { serialize: vuexfireSerialize });
     }),
     unbind: firestoreAction(({ unbindFirestoreRef }) => {
       return unbindFirestoreRef('records');
