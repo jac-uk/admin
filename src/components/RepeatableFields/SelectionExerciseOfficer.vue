1eb6baa5155eee4b2a978155761fb4e95c905654
Lauren Qurashi <lauren.qurashi@me.com>
2019-09-12 09:34:42 +0100

    Added a SelectionExerciseOfficer repeatable field.
    - Added the file to the repeatable fields file, copied from the other
    Drafting Judge component.
    - Added tests
    - Linted code.

---
+++ src/components/RepeatableFields/SelectionExerciseOfficer.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <TextField
+      :id="`selection_exercise_officer_${index}`"
+      v-model="row.name"
+      type="text"
+      label="Selection exercise officer"
+    />
+    <slot name="removeButton" />
+  </div>
+</template>
+
+<script>
+import TextField from '@/components/Form/TextField';
+
+export default {
+  components: {
+    TextField,
+  },
+  props: {
+    row: {
+      required: true,
+      type: Object,
+    },
+    index: {
+      required: true,
+      type: Number,
+    },
+  },
+};
+</script>
