8613abdff534dfd250201610c24c4e7b655700cc
Lauren Qurashi <lauren.qurashi@me.com>
2019-09-12 14:53:49 +0100

    Added save to db functionality to Timeline page
    - Timeline page has been set up in line with the Contacts page. 
    - Data saves to the DB
    - Code has been linted
    - Tests have been adapted and pass

--- tests/unit/views/Exercises/Edit/Timeline.spec.js
+++ tests/unit/views/Exercises/Edit/Timeline.spec.js
@@ -1,29 +1,79 @@
-import { shallowMount } from '@vue/test-utils';
 import ExerciseEditTimeline from '@/views/Exercises/Edit/Timeline';
-import RepeatableFields from '@/components/RepeatableFields';
+import { shallowMount } from '@vue/test-utils';
+
+const mockStore = {
+  dispatch: jest.fn(),
+  state: {
+    exerciseDocument: {
+      record: {
+        id: '001',
+      },
+    },
+  },
+};
 
 describe('views/Exercises/Edit/Timeline', () => {
   let wrapper;
-
   beforeEach(() => {
-    wrapper = shallowMount(ExerciseEditTimeline);
+    wrapper = shallowMount(ExerciseEditTimeline, {
+      mocks: {
+        $store: mockStore,
+      },
+    });
   });
 
-  it('renders the view', () => {
-    expect(wrapper.exists()).toBe(true);
-  });
+  describe('template', () => {
+    it('renders', () => {
+      expect(wrapper.exists()).toBe(true);
+    });
 
-  it('contains the link to About The Vacancy page', () => {
-    expect(wrapper.find({ ref: 'linkToAboutTheVacancy' }).isVisible()).toBe(true);
-  });
-
-  it('renders RepeatableFields component', () => {
-    expect(wrapper.find(RepeatableFields).exists()).toBe(true);
-  });
-
-  describe('Accessibility:', () => {
-    it('page contains h1 element', () => {
+    it('contains a <h1>', () => {
       expect(wrapper.contains('h1')).toBe(true);
+    });
+
+    it('contains a <form>', () => {
+      expect(wrapper.find('form').exists()).toBe(true);
+    });
+
+    it('the <form> calls the `save` method when submitted', () => {
+      const mockSave = jest.fn();
+      wrapper.setMethods({ save: mockSave });
+      wrapper.find('form').trigger('submit');
+      expect(mockSave).toHaveBeenCalledTimes(1);
+    });
+
+    it('the <form> contains a "Save and continue" submit button', () => {
+      const button = wrapper.find('form button');
+      expect(button.element.type).toBe('submit');
+      expect(button.text()).toBe('Save and continue');
+    });
+  });
+
+  describe('methods', () => {
+    describe('save', () => {
+      const exerciseData = {
+        applicationOpenDate: new Date(2019, 10, 21),
+        scenarioTestStartTime: new Date(0, 0, 0, 6, 35),
+      };
+
+      beforeEach(() => {
+        mockStore.dispatch.mockClear();
+        wrapper.setData({
+          exercise: exerciseData,
+        });
+        wrapper.vm.save();
+      });
+
+      it('dispatches `exerciseDocument/save` Vuex action', () => {
+        expect(mockStore.dispatch).toHaveBeenCalledTimes(1);
+        const dispatchedAction = mockStore.dispatch.mock.calls[0][0];
+        expect(dispatchedAction).toBe('exerciseDocument/save');
+      });
+
+      it('with the expected save payload', () => {
+        const dispatchedPayload = mockStore.dispatch.mock.calls[0][1];
+        expect(dispatchedPayload).toEqual(expect.objectContaining(exerciseData));
+      });
     });
   });
 });
