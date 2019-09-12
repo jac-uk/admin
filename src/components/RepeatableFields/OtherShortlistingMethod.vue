44f08772c09ac51a1b12b7c8416c83587c9e96b5
Lauren Qurashi <lauren.qurashi@me.com>
2019-09-12 13:06:37 +0100

    Added Database functionality to the Shortlisting page.
    - Had to create an emergency repeatable field as I forgot one lol. 
    - Tests seem to not be happy but gotta save anyway

---
+++ src/components/RepeatableFields/OtherShortlistingMethod.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <TextField
+      :id="`other_shortlisting_method_${index}`"
+      v-model="row.name"
+      type="text"
+      label="Add method"
+    />
+    <slot name="removeButton" />
+  </div>
+</template>
+
+<script>
+import TextField from '@/components/Form/TextField';
+
+export default {
+  components: {
+    TextField,
+  },
+  props: {
+    row: {
+      required: true,
+      type: Object,
+    },
+    index: {
+      required: true,
+      type: Number,
+    },
+  },
+};
+</script>
