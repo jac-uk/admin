388486ad2bd60c4b28f9f3520fb19b4f5e2eda9e
Lauren Qurashi <lauren.qurashi@me.com>
2019-09-12 13:30:05 +0100

    Added tests for the cute new Shortlisting page
    - Copied and adapted the tests from the contacts page. 
    - Code has been linted and tests are passing

--- tests/unit/views/Exercises/Edit/Shortlisting.spec.js
+++ tests/unit/views/Exercises/Edit/Shortlisting.spec.js
@@ -1,26 +1,79 @@
 import ExerciseEditShortlisting from '@/views/Exercises/Edit/Shortlisting';
-import { shallowMount, createLocalVue } from '@vue/test-utils';
-import Router from 'vue-router';
+import { shallowMount } from '@vue/test-utils';
 
-const localVue = createLocalVue();
-localVue.use(Router);
+const mockStore = {
+  dispatch: jest.fn(),
+  state: {
+    exerciseDocument: {
+      record: {
+        id: '001',
+      },
+    },
+  },
+};
 
 describe('views/Exercises/Edit/Shortlisting', () => {
-  it('renders the view', () => {
-    let wrapper = shallowMount(ExerciseEditShortlisting, {
-      localVue,
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallowMount(ExerciseEditShortlisting, {
+      mocks: {
+        $store: mockStore,
+      },
     });
-
-    expect(wrapper.exists()).toBe(true);
   });
 
-  describe('Accessibility:', () => {
-    it('page contains h1 element', () => {
-      let wrapper = shallowMount(ExerciseEditShortlisting, {
-        localVue,
+  describe('template', () => {
+    it('renders', () => {
+      expect(wrapper.exists()).toBe(true);
+    });
+
+    it('contains a <h1>', () => {
+      expect(wrapper.contains('h1')).toBe(true);
+    });
+
+    it('contains a <form>', () => {
+      expect(wrapper.find('form').exists()).toBe(true);
+    });
+
+    it('the <form> calls the `save` method when submitted', () => {
+      const mockSave = jest.fn();
+      wrapper.setMethods({ save: mockSave });
+      wrapper.find('form').trigger('submit');
+      expect(mockSave).toHaveBeenCalledTimes(1);
+    });
+
+    it('the <form> contains a "Save and continue" submit button', () => {
+      const button = wrapper.find('form button');
+      expect(button.element.type).toBe('submit');
+      expect(button.text()).toBe('Save and continue');
+    });
+  });
+
+  describe('methods', () => {
+    describe('save', () => {
+      const exerciseData = {
+        shortlistingMethods: ['method-1'],
+        otherShortlistingMethod: [{name:'additional method'}],
+      };
+
+      beforeEach(() => {
+        mockStore.dispatch.mockClear();
+        wrapper.setData({
+          exercise: exerciseData,
+        });
+        wrapper.vm.save();
       });
 
-      expect(wrapper.contains('h1')).toBe(true);
+      it('dispatches `exerciseDocument/save` Vuex action', () => {
+        expect(mockStore.dispatch).toHaveBeenCalledTimes(1);
+        const dispatchedAction = mockStore.dispatch.mock.calls[0][0];
+        expect(dispatchedAction).toBe('exerciseDocument/save');
+      });
+
+      it('with the expected save payload', () => {
+        const dispatchedPayload = mockStore.dispatch.mock.calls[0][1];
+        expect(dispatchedPayload).toEqual(expect.objectContaining(exerciseData));
+      });
     });
   });
 });
