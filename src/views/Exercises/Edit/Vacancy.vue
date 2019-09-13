1c19e12f8a9e4e87ec110effe914f988e9fd19aa
Lauren Qurashi <lauren.qurashi@me.com>
2019-09-13 11:36:16 +0100

    Added database functionality to the Vacancy page.
    - Data saves into the Database
    - Code has been linted
    - Tests are now passing.

--- src/views/Exercises/Edit/Vacancy.vue
+++ src/views/Exercises/Edit/Vacancy.vue
@@ -1,301 +1,299 @@
 <template>
   <div class="govuk-grid-row">
-    <div class="govuk-grid-column-two-thirds">
-      <a
-        ref="BackLinkToAddExerciseTimeline"
-        class="govuk-back-link"
-        @click="$router.push('add-exercise-timeline')"
-      >
-        Back
-      </a>
-
-      <h1 class="govuk-heading-xl">
-        About the vacancy
-      </h1>
-
-      <p class="govuk-body-l">
-        You'll find this information in the vacancy request (VR) from HMCTS.
-      </p>
-
-      <RadioGroup
-        id="type-of-exercise"
-        v-model="typeOfExercise"
-        label="Type of exercise"
-      >
-        <RadioItem
-          value="legal"
-          label="Legal"
-        />
-        <RadioItem
-          value="non-legal"
-          label="Non legal"
-        />
-        <RadioItem
-          value="senior"
-          label="Senior"
-        />
-        <RadioItem
-          value="leadership"
-          label="Leadership"
-        />
-      </RadioGroup>
-
-      <RadioGroup
-        id="is-court-or-tribunal"
-        v-model="isCourtOrTribunal"
-        label="Is the vacancy for a court or tribunal?"
-      >
-        <RadioItem
-          value="court"
-          label="Court"
-        />
-        <RadioItem
-          value="tribunal"
-          label="Tribunal"
-        />
-      </RadioGroup>
-
-      <RadioGroup
-        id="appointment-type"
-        v-model="appointmentType"
-        label="Appointment type"
-      >
-        <RadioItem
-          value="salaried"
-          label="Salaried"
+    <form @submit.prevent="save">
+      <div class="govuk-grid-column-two-thirds">
+        <a
+          ref="BackLinkToAddExerciseTimeline"
+          class="govuk-back-link"
+          @click="$router.push('add-exercise-timeline')"
         >
-          <div class="govuk-form-group">
-            <label
-              class="govuk-label"
-              for="salary-group"
-            >
-              Salary group
-            </label>
-            <select
-              id="salary-group"
-              class="govuk-select"
-            >
-              <option value="group1">
-                Group 1
-              </option>
-              <option value="group1.1">
-                Group 1.1
-              </option>
-              <option value="group2">
-                Group 2
-              </option>
-              <option value="group3">
-                Group 3
-              </option>
-              <option value="group4">
-                Group 4
-              </option>
-              <option value="group5+">
-                Group 5+
-              </option>
-              <option value="group5">
-                Group 5
-              </option>
-              <option value="group6.1">
-                Group 6.1
-              </option>
-              <option value="group6.2">
-                Group 6.7
-              </option>
-              <option value="group7">
-                Group 7
-              </option>
-            </select>
-          </div>
-        </RadioItem>
-        <RadioItem
-          value="fee-paid"
-          label="Fee paid"
+          Back
+        </a>
+
+        <h1 class="govuk-heading-xl">
+          About the vacancy
+        </h1>
+
+        <p class="govuk-body-l">
+          You'll find this information in the vacancy request (VR) from HMCTS.
+        </p>
+
+        <RadioGroup
+          id="type-of-exercise"
+          v-model="exercise.typeOfExercise"
+          label="Type of exercise"
         >
-          <Currency
-            id="fee-paid-fee"
-            v-model="feePaidFee"
-            label="Fee"
+          <RadioItem
+            value="legal"
+            label="Legal"
           />
-        </RadioItem>
-        <RadioItem
-          value="unpaid"
-          label="Unpaid"
-        />
-      </RadioGroup>
+          <RadioItem
+            value="non-legal"
+            label="Non legal"
+          />
+          <RadioItem
+            value="senior"
+            label="Senior"
+          />
+          <RadioItem
+            value="leadership"
+            label="Leadership"
+          />
+        </RadioGroup>
 
-      <RadioGroup
-        id="is-sptw-offered"
-        v-model="isSPTWOffered"
-        label="Is salaried part-time working (SPTW) offered?"
-      >
-        <RadioItem
-          value="true"
-          label="Yes"
+        <RadioGroup
+          id="is-court-or-tribunal"
+          v-model="exercise.isCourtOrTribunal"
+          label="Is the vacancy for a court or tribunal?"
         >
-          <div class="govuk-form-group">
-            <label
-              class="govuk-label"
-              for="yes-salary-details"
-            >
-              Detail of salaried part-time working (SPTW)
-            </label>
-            <textarea
-              id="yes-salary-details"
-              v-model="yesSalaryDetails"
-              class="govuk-textarea"
-              rows="5"
+          <RadioItem
+            value="court"
+            label="Court"
+          />
+          <RadioItem
+            value="tribunal"
+            label="Tribunal"
+          />
+        </RadioGroup>
+
+        <RadioGroup
+          id="appointment-type"
+          v-model="exercise.appointmentType"
+          label="Appointment type"
+        >
+          <RadioItem
+            value="salaried"
+            label="Salaried"
+          >
+            <div class="govuk-form-group">
+              <label
+                class="govuk-label"
+                for="salary-group"
+              >
+                Salary group
+              </label>
+              <select
+                id="salary-group"
+                class="govuk-select"
+              >
+                <option value="group1">
+                  Group 1
+                </option>
+                <option value="group1.1">
+                  Group 1.1
+                </option>
+                <option value="group2">
+                  Group 2
+                </option>
+                <option value="group3">
+                  Group 3
+                </option>
+                <option value="group4">
+                  Group 4
+                </option>
+                <option value="group5+">
+                  Group 5+
+                </option>
+                <option value="group5">
+                  Group 5
+                </option>
+                <option value="group6.1">
+                  Group 6.1
+                </option>
+                <option value="group6.2">
+                  Group 6.7
+                </option>
+                <option value="group7">
+                  Group 7
+                </option>
+              </select>
+            </div>
+          </RadioItem>
+          <RadioItem
+            value="fee-paid"
+            label="Fee paid"
+          >
+            <Currency
+              id="fee-paid-fee"
+              v-model="exercise.feePaidFee"
+              label="Fee"
             />
-          </div>
-        </RadioItem>
-        <RadioItem
-          value="no"
-          label="No"
+          </RadioItem>
+          <RadioItem
+            value="unpaid"
+            label="Unpaid"
+          />
+        </RadioGroup>
+
+        <RadioGroup
+          id="is-sptw-offered"
+          v-model="exercise.isSPTWOffered"
+          label="Is salaried part-time working (SPTW) offered?"
         >
-          <div class="govuk-form-group">
-            <label
-              class="govuk-label"
-              for="no-salary-details"
-            >
-              Add why SPTW is not offered
-            </label>
-            <textarea
-              id="no-salary-details"
-              v-model="noSalaryDetails"
-              class="govuk-textarea"
-              rows="5"
-            />
-          </div>
-        </RadioItem>
-      </RadioGroup>
+          <RadioItem
+            value="true"
+            label="Yes"
+          >
+            <div class="govuk-form-group">
+              <label
+                class="govuk-label"
+                for="yes-salary-details"
+              >
+                Detail of salaried part-time working (SPTW)
+              </label>
+              <textarea
+                id="yes-salary-details"
+                v-model="exercise.yesSalaryDetails"
+                class="govuk-textarea"
+                rows="5"
+              />
+            </div>
+          </RadioItem>
+          <RadioItem
+            value="no"
+            label="No"
+          >
+            <div class="govuk-form-group">
+              <label
+                class="govuk-label"
+                for="no-salary-details"
+              >
+                Add why SPTW is not offered
+              </label>
+              <textarea
+                id="no-salary-details"
+                v-model="exercise.noSalaryDetails"
+                class="govuk-textarea"
+                rows="5"
+              />
+            </div>
+          </RadioItem>
+        </RadioGroup>
 
-      <h2 class="govuk-label-input">
-        Number of vacancies
-      </h2>
+        <h2 class="govuk-label-input">
+          Number of vacancies
+        </h2>
 
-      <TextField
-        id="immediate-start"
-        v-model="immediateStart"
-        label="Immediate start"
-        input-class="govuk-input--width-3"
-        hint="These are also called Section 87 (S87) vacancies."
-      />
-
-      <TextField
-        id="future-start"
-        v-model="futureStart"
-        label="Future start"
-        input-class="govuk-input--width-3"
-        hint="These are also called Section 94 (S94) vacancies."
-      />
-
-      <div class="govuk-form-group">
-        <label
-          class="govuk-heading-m"
-          for="location"
-        >
-          Location
-        </label>
-        <textarea
-          id="location"
-          v-model="location"
-          class="govuk-textarea"
-          rows="2"
+        <TextField
+          id="immediate-start"
+          v-model="exercise.immediateStart"
+          label="Immediate start"
+          input-class="govuk-input--width-3"
+          hint="These are also called Section 87 (S87) vacancies."
         />
-      </div>
 
-      <div class="govuk-form-group">
-        <label
-          class="govuk-heading-m"
-          for="jurisdiction"
-        >
-          Jurisdiction
-        </label>
-        <p class="govuk-hint">
-          For example, Employment, Family.
-        </p>
-        <textarea
-          id="location"
-          v-model="jurisdiction"
-          class="govuk-textarea"
-          rows="5"
+        <TextField
+          id="future-start"
+          v-model="exercise.futureStart"
+          label="Future start"
+          input-class="govuk-input--width-3"
+          hint="These are also called Section 94 (S94) vacancies."
         />
+
+        <div class="govuk-form-group">
+          <label
+            class="govuk-heading-m"
+            for="location"
+          >
+            Location
+          </label>
+          <textarea
+            id="location"
+            v-model="exercise.location"
+            class="govuk-textarea"
+            rows="2"
+          />
+        </div>
+
+        <div class="govuk-form-group">
+          <label
+            class="govuk-heading-m"
+            for="jurisdiction"
+          >
+            Jurisdiction
+          </label>
+          <p class="govuk-hint">
+            For example, Employment, Family.
+          </p>
+          <textarea
+            id="location"
+            v-model="exercise.jurisdiction"
+            class="govuk-textarea"
+            rows="5"
+          />
+        </div>
+
+        <div class="govuk-form-group">
+          <label
+            class="govuk-heading-m"
+            for="about-the-role"
+          >
+            About the role
+          </label>
+          <p class="govuk-hint">
+            Add information about this role for the information page.
+          </p>
+          <textarea
+            id="about-the-role"
+            v-model="exercise.aboutTheRole"
+            class="govuk-textarea"
+            rows="5"
+          />
+        </div>
+
+        <CheckboxGroup
+          id="welsh-requirement"
+          v-model="exercise.welshRequirement"
+          label="Welsh requirement"
+          hint="Select all that apply."
+        >
+          <CheckboxItem
+            value="welsh-language"
+            label="Welsh language"
+          />
+          <CheckboxItem
+            value="devolution-questions"
+            label="Devolution questions"
+          />
+        </CheckboxGroup>
+
+        <h2 class="govuk-label-input">
+          Upload HMCTS documents
+        </h2>
+
+        <div class="govuk-form-group">
+          <label
+            class="govuk-heading-m"
+            for="file-upload-1"
+          >
+            Upload job description
+          </label>
+          <input
+            id="file-upload-1"
+            class="govuk-file-upload"
+            type="file"
+          >
+        </div>
+
+        <div class="govuk-form-group">
+          <label
+            class="govuk-heading-m"
+            for="file-upload-2"
+          >
+            Upload terms and conditions
+          </label>
+          <input
+            id="file-upload-2"
+            class="govuk-file-upload"
+            type="file"
+          >
+        </div>
+
+        <button class="govuk-button">
+          Save and continue
+        </button>
       </div>
-
-      <div class="govuk-form-group">
-        <label
-          class="govuk-heading-m"
-          for="about-the-role"
-        >
-          About the role
-        </label>
-        <p class="govuk-hint">
-          Add information about this role for the information page.
-        </p>
-        <textarea
-          id="about-the-role"
-          v-model="aboutTheRole"
-          class="govuk-textarea"
-          rows="5"
-        />
-      </div>
-
-      <CheckboxGroup
-        id="welsh-requirement"
-        v-model="welshRequirement"
-        label="Welsh requirement"
-        hint="Select all that apply."
-      >
-        <CheckboxItem
-          value="welsh-language"
-          label="Welsh language"
-        />
-        <CheckboxItem
-          value="devolution-questions"
-          label="Devolution questions"
-        />
-      </CheckboxGroup>
-
-      <h2 class="govuk-label-input">
-        Upload HMCTS documents
-      </h2>
-
-      <div class="govuk-form-group">
-        <label
-          class="govuk-heading-m"
-          for="file-upload-1"
-        >
-          Upload job description
-        </label>
-        <input
-          id="file-upload-1"
-          class="govuk-file-upload"
-          type="file"
-        >
-      </div>
-
-      <div class="govuk-form-group">
-        <label
-          class="govuk-heading-m"
-          for="file-upload-2"
-        >
-          Upload terms and conditions
-        </label>
-        <input
-          id="file-upload-2"
-          class="govuk-file-upload"
-          type="file"
-        >
-      </div>
-
-      <button
-        ref="linkToAddEligibilityInformation"
-        class="govuk-button"
-        @click="$router.push({ name: 'exercise-edit-eligibility', params: { id: 'example' } })"
-      >
-        Save and continue
-      </button>
-    </div>
+    </form>
   </div>
 </template>
 
@@ -317,22 +315,30 @@ export default {
     Currency,
   },
   data(){
-    return {
-      typeOfExercise: [],
-      isCourtOrTribunal: [],
-      appointmentType: [],
-      feePaidFee: null,
-      isSPTWOffered: [],
-      yesSalaryDetails: null,
-      noSalaryDetails: null,
-      immediateStart: null,
-      futureStart: null,
-      location: null,
-      jurisdiction: null,
-      aboutTheRole: null,
-      welshRequirement: [],
+    const exercise = this.$store.state.exerciseDocument.record;
 
+    return {
+      exercise: {
+        typeOfExercise: exercise.typeOfExercise || [],
+        isCourtOrTribunal: exercise.isCourtOrTribunal || [],
+        appointmentType: exercise.appointmentType || [],
+        feePaidFee: exercise.feePaidFee || null,
+        isSPTWOffered: exercise.isSPTWOffered || [],
+        yesSalaryDetails: exercise.yesSalaryDetails || null,
+        noSalaryDetails: exercise.noSalaryDetails || null,
+        immediateStart: exercise.immediateStart || null,
+        futureStart: exercise.futureStart || null,
+        location: exercise.location || null,
+        jurisdiction: exercise.jurisdiction || null,
+        aboutTheRole: exercise.aboutTheRole || null,
+        welshRequirement: exercise.welshRequirement || [],
+      },
     };
+  },
+  methods: {
+    async save() {
+      await this.$store.dispatch('exerciseDocument/save', this.exercise);
+    },
   },
 };
 </script>
