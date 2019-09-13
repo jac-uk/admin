dc2ea9afcc5a289ab03eda50a4e25279ea2b0bb2
alexbokii <vanilinka33@gmail.com>
2019-09-13 14:42:53 +0100

    Add Navigation component

---
+++ src/components/Page/Navigation.vue
@@ -0,0 +1,65 @@
+<template>
+  <nav :aria-label="label">
+    <ul class="dwp-vertical-navigation">
+      <li 
+        v-for="navItem in data"
+        :key="navItem.name"
+      >
+        <router-link 
+          class="nav-link"
+          :to="{name: navItem.name}"
+          :aria-current="isActive(navItem.name) ? 'page' : false"
+        >
+          {{ navItem.page }} 
+        </router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      required: true,
+      type: Array,
+    },
+    label: {
+      required: false,
+      default: 'navigation',
+      type: String,
+    },
+  },
+  methods: {
+    isActive(name) {
+      return name === this.$route.name;
+    },
+  },
+};
+</script>
+
+<style scoped>
+  .dwp-vertical-navigation {
+    margin: 0 0 1.25em 0;
+  }
+
+  .dwp-vertical-navigation li {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    font-size: 1em;
+  }
+
+  .dwp-vertical-navigation a[aria-current="page"] {
+    color: #1d70b8;
+    font-weight: bold;
+    text-decoration: none;
+    border-left: 4px solid #1d70b8;
+    background-color: #f3f2f1;
+  }
+
+  .dwp-vertical-navigation li a {
+    padding: 0.625em;
+    display: block;
+  }
+</style>
