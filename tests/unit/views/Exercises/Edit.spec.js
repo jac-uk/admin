d6ad9f19df58c0b821780cee49e2c3a1ea2a798b
Ollie Treend <ollie@treend.com>
2019-09-17 11:38:31 +0100

    Pass the current route name to `exerciseCreateJourney`
    On the parent edit page, pass the current route name to the `exerciseCreateJourney` Vuex module. This is done by dispatching the action `exerciseCreateJourney/setCurrentRoute`.
    
    - On initial load, it will pass the current route name.
    - Every time the route changes (since this is a parent page, it has several child routes), pass the new route name.

--- tests/unit/views/Exercises/Edit.spec.js
+++ tests/unit/views/Exercises/Edit.spec.js
@@ -67,6 +67,31 @@ describe('@/views/Exercises/Edit', () => {
       it('loads the exercise document identified by URL param `id`', () => {
         expect(mockStore.dispatch).toHaveBeenCalledWith('exerciseDocument/bind', 'abc123');
       });
+
+      it('updates `exerciseCreateJourney` with the current route name', () => {
+        expect(mockStore.dispatch).toHaveBeenCalledWith('exerciseCreateJourney/setCurrentRoute', wrapper.vm.$route.name);
+      });
+    });
+  });
+
+  describe.only('watchers', () => {
+    describe('when $route changes', () => {
+      it('updates `exerciseCreateJourney` with the current route name', () => {
+        // Trigger the $route watcher function
+        const watcher = wrapper.vm.$options.watch.$route;
+        const callWatcher = (newValue, oldValue) => (watcher.call(wrapper.vm, newValue, oldValue));
+
+        // Reset dispatch mock so it doesn't count calls made during mounted()
+        mockStore.dispatch.mockClear();
+
+        callWatcher({ name: 'page-one' });
+        callWatcher({ name: 'page-two' });
+
+        expect(mockStore.dispatch).toHaveBeenCalledTimes(2);
+        const calls = mockStore.dispatch.mock.calls;
+        expect(calls[0]).toEqual(['exerciseCreateJourney/setCurrentRoute', 'page-one']);
+        expect(calls[1]).toEqual(['exerciseCreateJourney/setCurrentRoute', 'page-two']);
+      });
     });
   });
 });
