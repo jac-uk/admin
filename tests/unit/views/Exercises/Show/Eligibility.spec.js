d1c523f2469edf6a2b75a5d62f5ea3645e6cd4c5
Ollie Treend <ollie@treend.com>
2019-09-17 17:40:12 +0100

    Add basic test for exercise eligibility show page
    These tests should be more in depth – but for lack of time, and also lack of clarity around how values should be displayed, I'm only implementing basic tests for now.

---
+++ tests/unit/views/Exercises/Show/Eligibility.spec.js
@@ -0,0 +1,65 @@
+import Eligibility from '@/views/Exercises/Show/Eligibility';
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import Vuex from 'vuex';
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+const exercise = {
+  postQualificationExperience: 'other',
+  otherYears: '9 years',
+  schedule2DApply: true,
+  qualifications: [
+    'Advocate',
+    'Solicitor',
+  ],
+  aSCApply: false,
+  reasonableLengthService: 'other',
+  otherLOS: '1 year',
+  retirementAge: 'other',
+  otherRetirement: '65 years',
+};
+
+const store = new Vuex.Store({
+  modules: {
+    exerciseDocument: {
+      namespaced: true,
+      state: {
+        record: exercise,
+      },
+    },
+  },
+});
+
+const createTestSubject = () => {
+  return shallowMount(Eligibility, {
+    store,
+    localVue,
+    stubs: {
+      'RouterLink': true,
+    },
+  });
+};
+
+describe('@/views/Exercises/Show/Eligibility', () => {
+  describe('template', () => {
+    it('renders the component', () => {
+      let wrapper = createTestSubject();
+      expect(wrapper.find('dl').exists()).toBe(true);
+    });
+
+    it('renders the router-link', () => {
+      let wrapper = createTestSubject();
+      expect(wrapper.find('RouterLink-stub').exists()).toBe(true);
+    });
+  });
+
+  describe('computed properties', () => {
+    describe('exercise', () => {
+      it('returns record object from state', () => {
+        let wrapper = createTestSubject();
+        expect(wrapper.vm.exercise).toEqual(exercise);
+      });
+    });
+  });
+});
