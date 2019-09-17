18bacdcccbc29e59f851533808c8df81743c1aa8
Lauren Qurashi <lauren.qurashi@me.com>
2019-09-17 17:46:13 +0100

    Added a Show shortlisting page
    - Amended the Edit Shortlisting page. The Values are now equal to the 
    same string that Label is equal to, in order to have human- readable 
    variables. 
    
    - Copied and amended the HTML from Alex's Contacts page. Have not made 
    any changes to the CSS so could still have the wrong details for that. 
    
    - Added tests for the Shortlisting Show page. all passing. 
    
    - Code has been linted.

--- src/views/Exercises/Show/Shortlisting.vue
+++ src/views/Exercises/Show/Shortlisting.vue
@@ -1,11 +1,50 @@
 <template>
   <div>
-    Shortlisting
+    <div class="text-right">
+      <router-link
+        class="govuk-link"
+        :to="{name: 'exercise-edit-shortlisting'}"
+      >
+        Update shortlisting methods
+      </router-link>
+    </div>
+    <h2 class="govuk-heading-l">
+      Shortlisting methods
+    </h2>
+    <table class="govuk-table">
+      <tbody class="govuk-table__body">
+        <tr class="govuk-table__row">
+          <th class="govuk-table__header">
+            Shortlisting methods
+          </th>
+          <td class="govuk-table__cell">
+            <ul class="govuk-list">
+              <li
+                v-for="item in exercise.shortlistingMethods"
+                :key="item"
+              >
+                {{ item }}
+              </li>
+              <li
+                v-for="item in exercise.otherShortlistingMethod"
+                :key="item.name"
+              >
+                {{ item.name }}
+              </li>
+            </ul>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
-
+  computed: {
+    exercise() {
+      return this.$store.state.exerciseDocument.record;
+    },
+  },
 };
 </script>
