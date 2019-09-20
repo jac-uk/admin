50cac8668d55965c58910db452c0447db35148ba
Lauren Qurashi <lauren.qurashi@me.com>
2019-09-20 11:03:15 +0100

    Added a TextareaInput component
    - Copied and amended TextField component’s tests and component code.
    - This component creates a textarea input that allows more than a single row of text. This is styled as a GOV.UK Textarea.

---
+++ tests/unit/components/Form/TextareaInput.spec.js
@@ -0,0 +1,77 @@
+import { shallowMount } from '@vue/test-utils';
+import TextareaInput from '@/components/Form/TextareaInput';
+
+describe('components/Form/TextareaInput', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallowMount(TextareaInput);
+  });
+
+  it('renders the component', () => {
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  describe('template', () => {
+    describe('label', () => {
+      it('sets the label to the value of the `label` prop', () => {
+        wrapper.setProps({ label: 'My Form Label' });
+        expect(wrapper.find('label').text()).toBe('My Form Label');
+      });
+    });
+
+    describe('hint', () => {
+      let hint;
+      describe('when the prop is set', () => {
+        beforeEach(() => {
+          wrapper.setProps({ hint: 'my_hint' });
+          hint = wrapper.find('.govuk-hint');
+        });
+
+        it('shows a hint', () => {
+          expect(hint.exists()).toBe(true);
+        });
+        it('sets the hint to the value of the `hint` prop', () => {
+          expect(hint.text()).toBe('my_hint');
+        });
+      });
+
+      describe('when the prop is not set', () => {
+        beforeEach(() => {
+          hint = wrapper.find('.govuk-hint');
+        });
+
+        it('does not show hint', () => {
+          expect(hint.exists()).toBe(false);
+        });
+      });
+    });
+
+    describe('id', () => {
+      it('sets <label> `for` attribute', () => {
+        wrapper.setProps({ id: 'my_unique_key' });
+        expect(wrapper.find('label').attributes().for).toBe('my_unique_key');
+      });
+
+      it('sets <input> `id` attribute', () => {
+        wrapper.setProps({ id: 'my_unique_key' });
+        expect(wrapper.find('textarea').attributes().id).toBe('my_unique_key');
+      });
+    });
+  });
+
+  describe('`v-model` interface', () => {
+    describe('when text changes', () => {
+      it('emits an input event with the new value', () => {
+        wrapper.setData({ text: 'new-value' });
+        expect(wrapper.emitted().input).toEqual([['new-value']]);
+      });
+    });
+
+    describe('when value prop changes', () => {
+      it('updates the `text` computed property', () => {
+        wrapper.setProps({ value: 'my_value' });
+        expect(wrapper.vm.text).toEqual('my_value');
+      });
+    });
+  });
+});
