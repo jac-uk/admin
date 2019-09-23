7c11aa45633242b3e3405093b356aed3cb73687e
alexbokii <vanilinka33@gmail.com>
2019-09-23 17:46:00 +0100

    Pass custom error message to LoadingMessage using slot
    As LoadingMessage component is reusable, we want to pass an error message depending on where we use this component.

--- src/views/Exercises/Show.vue
+++ src/views/Exercises/Show.vue
@@ -3,7 +3,27 @@
     <LoadingMessage
       v-if="loaded === false"
       :load-failed="loadFailed"
-    />
+    >
+      <template v-slot:failedLoadMessage>
+        <h1 class="govuk-heading-xl">
+          Page not found
+        </h1>
+        <p class="govuk-body">
+          If you typed the web address, check it is correct.
+        </p>
+        <p class="govuk-body">
+          If you pasted the web address, check you copied the entire address.
+        </p>
+        <p class="govuk-body">
+          Come back to Dashboard page: 
+          <router-link
+            to="/dashboard"
+          >
+            Go to Dashboard
+          </router-link>
+        </p>
+      </template>
+    </LoadingMessage>
     <div v-else>
       <div class="govuk-grid-row">
         <div class="govuk-grid-column-full">
@@ -71,10 +91,15 @@ export default {
     const id = this.$route.params.id;
     
     this.$store.dispatch('exerciseDocument/bind', id)
-      .then(() => {
-        this.loaded = true;
+      .then((data) => {
+        if(!(data === null)) {
+          this.loaded = true;
+        }
+        else {
+          this.loadFailed = true;
+        }
       }).catch((e) => {
-        this.loadFailed = true;
+        
         throw e;
       });
   },
