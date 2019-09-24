0fed95f2dccb586b867dabc148168a11be3cce0b
alexbokii <vanilinka33@gmail.com>
2019-09-24 10:03:48 +0100

    Refactor test for LoadingMessage

--- tests/unit/components/LoadingMessage.spec.js
+++ tests/unit/components/LoadingMessage.spec.js
@@ -2,52 +2,42 @@ import { shallowMount } from '@vue/test-utils';
 import LoadingMessage from '@/components/LoadingMessage';
 
 describe('components/LoadingMessage', () => {
-  it('shows the "Loading" message if loadFailed is equal to false', () => {
-    const wrapper = shallowMount(LoadingMessage, {
+  const createWrapper = (loadFailedValue) => {
+    return shallowMount(LoadingMessage, {
       propsData: {
-        loadFailed: false,
+        loadFailed: loadFailedValue,
+      },
+      slots: {
+        failedLoadMessage: "<h1 class='slot'>Test error message</h1>",
       },
     });
+  };
 
-    expect(wrapper.find({ ref: 'loadingMessage' }).isVisible()).toBe(true);
-    expect(wrapper.find({ ref: 'errorMessage' }).exists()).toBe(false);
-  });
+  describe('when loadFailed is equal to false', () => {
+    it('shows the "Loading" message', () => {
+      const wrapper = createWrapper(false);
 
-  it('shows the errorMessage message if loadFailed is equal to true', () => {
-    const wrapper = shallowMount(LoadingMessage, {
-      propsData: {
-        loadFailed: true,
-      },
+      expect(wrapper.find({ ref: 'loadingMessage' }).isVisible()).toBe(true);
+      expect(wrapper.find({ ref: 'errorMessage' }).exists()).toBe(false);
     });
 
-    expect(wrapper.find({ ref: 'errorMessage' }).isVisible()).toBe(true);
-    expect(wrapper.find({ ref: 'loadingMessage' }).exists()).toBe(false);
+    it('does not render the slot', () => {
+      let slotContainer = createWrapper(false).find('h1.slot');
+      expect(slotContainer.exists()).not.toBe(true);
+    });
   });
 
-  describe('slot container', () => {
-    const createWrapper = (loadFailedValue) => {
-      return shallowMount(LoadingMessage, {
-        propsData: {
-          loadFailed: loadFailedValue,
-        },
-        slots: {
-          failedLoadMessage: "<h1 class='slot'>Test error message</h1>",
-        },
-      });
-    };
+  describe('when loadFailed is equal to true', () => {
+    it('shows the errorMessage', () => {
+      const wrapper = createWrapper(true);
 
-    describe('when loadFailed is equal to true', () => {
-      it('renders', () => {
+      expect(wrapper.find({ ref: 'errorMessage' }).isVisible()).toBe(true);
+      expect(wrapper.find({ ref: 'loadingMessage' }).exists()).toBe(false);
+    });
+
+    it('renders the slot', () => {
         let slotContainer = createWrapper(true).find('h1.slot');
         expect(slotContainer.exists()).toBe(true);
       });
-    });
-
-    describe('when loadFailed is equal to false', () => {
-      it('does not render', () => {
-        let slotContainer = createWrapper(false).find('h1.slot');
-        expect(slotContainer.exists()).not.toBe(true);
-      });
-    });
   });
 });
