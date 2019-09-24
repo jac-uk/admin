72026e0efbb7db5b1958b7c8bab861768faa4755
alexbokii <vanilinka33@gmail.com>
2019-09-24 10:13:51 +0100

    Pass error message into Slot from Edit page

--- src/views/Exercises/Edit.vue
+++ src/views/Exercises/Edit.vue
@@ -3,7 +3,27 @@
     <LoadingMessage
       v-if="loaded === false"
       :load-failed="loadFailed"
-    />
+    >
+      <template v-slot:failedLoadMessage>
+        <h1 class="govuk-heading-xl">
+          Page not found
+        </h1>
+        <p class="govuk-body">
+          If you typed the web address, check it is correct.
+        </p>
+        <p class="govuk-body">
+          If you pasted the web address, check you copied the entire address.
+        </p>
+        <p class="govuk-body">
+          Come back to Dashboard page: 
+          <router-link
+            to="/dashboard"
+          >
+            Go to Dashboard
+          </router-link>
+        </p>
+      </template>
+    </LoadingMessage>
     <RouterView v-else />
   </div>
 </template>
@@ -29,8 +49,13 @@ export default {
     const id = this.$route.params.id;
     
     this.$store.dispatch('exerciseDocument/bind', id)
-      .then(() => {
-        this.loaded = true;
+      .then((data) => {
+        if(!(data === null)) {
+          this.loaded = true;
+        }
+        else {
+          this.loadFailed = true;
+        }
       }).catch((e) => {
         this.loadFailed = true;
         throw e;
