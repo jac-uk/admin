b83e6de70194b6ebb4a886a2a1aad3d1ca011575
alexbokii <vanilinka33@gmail.com>
2019-09-24 16:48:05 +0100

    Create error pages

---
+++ src/views/Errors/ExerciseNotFound.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="govuk-grid-row">
+    <div class="govuk-grid-column-two-thirds">
+      <h1 class="govuk-heading-xl">
+        Exercise not found
+      </h1>
+      <p class="govuk-body">
+        There is not an exercise associated with this link address.
+      </p>
+      <p class="govuk-body">
+        You could check:
+        <ul>
+          <li>
+            you've copied and pasted the entire link address
+          </li>
+          <li>there are no obvious misspellings</li>
+        </ul>
+      </p>
+      <p class="govuk-body">
+        If your link still does not work, try searching for the exercise by name.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
--- src/views/Exercises/Show.vue
+++ src/views/Exercises/Show.vue
@@ -3,27 +3,7 @@
     <LoadingMessage
       v-if="loaded === false"
       :load-failed="loadFailed"
-    >
-      <template v-slot:failedLoadMessage>
-        <h1 class="govuk-heading-xl">
-          Page not found
-        </h1>
-        <p class="govuk-body">
-          If you typed the web address, check it is correct.
-        </p>
-        <p class="govuk-body">
-          If you pasted the web address, check you copied the entire address.
-        </p>
-        <p class="govuk-body">
-          Come back to Dashboard page: 
-          <router-link
-            to="/dashboard"
-          >
-            Go to Dashboard
-          </router-link>
-        </p>
-      </template>
-    </LoadingMessage>
+    />
     <div v-else>
       <div class="govuk-grid-row">
         <div class="govuk-grid-column-full">
@@ -96,12 +76,17 @@ export default {
           this.loaded = true;
         }
         else {
-          this.loadFailed = true;
+          this.redirectToErrorPage();
         }
       }).catch((e) => {
         this.loadFailed = true;
         throw e;
       });
   },
+  methods: {
+    redirectToErrorPage() {
+      this.$router.replace({ name: 'exercise-not-found' })
+    }
+  }
 };
 </script>
