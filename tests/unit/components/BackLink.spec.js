b555fedea0168c957d3571eb4a5e2461744f8c6d
Lauren Qurashi <lauren.qurashi@me.com>
2019-09-24 15:56:28 +0100

    Amended spacing and tests for BackLink
    - Fixed the spacing issues in the BackLink component.
    - Refactored the BackLink tests to remove unnecessary describe blocks. 
    - Added a test to check the text of the link.

--- tests/unit/components/BackLink.spec.js
+++ tests/unit/components/BackLink.spec.js
@@ -3,28 +3,31 @@ import BackLink from '@/components/BackLink';
 
 describe('components/BackLink', () => {
   let wrapper;
+  let link;
   const mockRouter = {
     back: jest.fn(),
   };
+
   beforeEach(() => {
-    wrapper= shallowMount(BackLink, {
-    mocks: {
-      $router: mockRouter,
-    },
-});
+    wrapper = shallowMount(BackLink, {
+      mocks: {
+        $router: mockRouter,
+      },
+    });
+    link = wrapper.find('.govuk-back-link');
   });
 
   it('renders the component', () => {
     expect(wrapper.exists()).toBe(true);
   });
 
-  describe('functionality', () => {
-    describe('router.back', () => {
-      it('calls the back function', () => {
-        const link = wrapper.find('.govuk-back-link');
-        link.trigger('click');
-        expect(mockRouter.back).toHaveBeenCalled();
-      });
-    });
+  it('has the class name govuk-back-link', () => {
+    expect(link.is('.govuk-back-link')).toBe(true);
+    expect(link.text()).toBe('Back');
+  });
+
+  it('calls the back function', () => {
+    link.trigger('click');
+    expect(mockRouter.back).toHaveBeenCalled();
   });
 });
