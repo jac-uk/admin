92acf1470413327c4ec57485949a87329dbea9d0
alexbokii <vanilinka33@gmail.com>
2019-09-27 13:33:20 +0100

    Use computed properties to render conditionally date fields for shortlisting methods

--- tests/unit/views/Exercises/Edit/Timeline.spec.js
+++ tests/unit/views/Exercises/Edit/Timeline.spec.js
@@ -4,6 +4,7 @@ import { shallowMount } from '@vue/test-utils';
 const exercise = {
   applicationOpenDate: new Date(2019, 1, 1),
   applicationCloseDate: new Date(2019, 3, 3),
+  shortlistingMethods: [],
 };
 
 const mockStore = {
@@ -89,6 +90,54 @@ describe('views/Exercises/Edit/Timeline', () => {
 
       it('navigates to the next page of the create journey', () => {
         expect(mockRouter.push).toHaveBeenCalledWith(mockStore.getters['exerciseCreateJourney/nextPage']);
+      });
+    });
+  });
+
+  describe('computed properties', () => {
+    describe('scenarioQT', () => {
+      describe('when scenarioQT is on the list of selected shortlisting methods', () => {
+        it('displays the block with scenarioQT dates', () => {
+          wrapper.setData({ exerciseShortlistingMethods: ['some method', 'Scenario test qualifying test (QT)', 'other'] });
+          expect(wrapper.find({ ref: 'scenarioQT' }).exists()).toBe(true);
+        });
+      });
+
+      describe('when scenarioQT is not on the list of selected shortlisting methods', () => {
+        it('does not display the block with scenarioQT dates', () => {
+          wrapper.setData({ exerciseShortlistingMethods: ['some method', 'Situational judgement qualifying test (QT)', 'other'] });
+          expect(wrapper.find({ ref: 'scenarioQT' }).exists()).toBe(false);
+        });
+      });
+    });
+
+    describe('situationalJudgementOrCriticalAnalysisQT', () => {
+      describe('when Situational Judgement QT is on the list of selected shortlisting methods', () => {
+        it('displays the block with situationalJudgementOrCriticalAnalysisQT dates', () => {
+          wrapper.setData({ exerciseShortlistingMethods: ['some method', 'Situational judgement qualifying test (QT)', 'other'] });
+          expect(wrapper.find({ ref: 'situationalJudgementOrCriticalAnalysisQT' }).exists()).toBe(true);
+        });
+      });
+
+      describe('when Critical Analysis QT is on the list of selected shortlisting methods', () => {
+        it('displays the block with situationalJudgementOrCriticalAnalysisQT dates', () => {
+          wrapper.setData({ exerciseShortlistingMethods: ['some method', 'other method', 'Critical analysis qualifying test (QT)'] });
+          expect(wrapper.find({ ref: 'situationalJudgementOrCriticalAnalysisQT' }).exists()).toBe(true);
+        });
+      });
+
+      describe('when both Situational Judgement QT and Critical Analysis QT is on the list of selected shortlisting methods', () => {
+        it('displays the block with situationalJudgementOrCriticalAnalysisQT dates', () => {
+          wrapper.setData({ exerciseShortlistingMethods: ['Situational judgement qualifying test (QT)', 'some method', 'other method', 'Critical analysis qualifying test (QT)'] });
+          expect(wrapper.find({ ref: 'situationalJudgementOrCriticalAnalysisQT' }).exists()).toBe(true);
+        });
+      });
+
+      describe('when Situational Judgement QT and Critical Analysis QT are not on the list of selected shortlisting methods', () => {
+        it('does not display the block with situationalJudgementOrCriticalAnalysisQT dates', () => {
+          wrapper.setData({ exerciseShortlistingMethods: ['some method', 'other method'] });
+          expect(wrapper.find({ ref: 'situationalJudgementOrCriticalAnalysisQT' }).exists()).toBe(false);
+        });
       });
     });
   });
