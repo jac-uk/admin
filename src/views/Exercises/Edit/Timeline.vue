45d670effe4b4f306d66214c552dd3ec7154c7d1
alexbokii <vanilinka33@gmail.com>
2019-09-27 13:33:20 +0100

    Use computed properties to render conditionally date fields for shortlisting methods

--- src/views/Exercises/Edit/Timeline.vue
+++ src/views/Exercises/Edit/Timeline.vue
@@ -40,43 +40,65 @@
           Shortlisting dates
         </h2>
 
-        <h3 class="govuk-heading-m">
-          Situational judgement qualifying test (QT), Critical analysis qualifying test (QT)
-        </h3>
-        <DateInput
-          id="test-date"
-          v-model="exercise.sjcaTestDate"
-          label="Test date"
-        />
-        <TimeInput
-          id="test-start-time"
-          v-model="exercise.sjcaTestStartTime"
-          label="Start time"
-        />
-        <TimeInput
-          id="test-end-time"
-          v-model="exercise.sjcaTestEndTime"
-          label="End time"
-        />
+        <div
+          v-if="situationalJudgementOrCriticalAnalysisQT"
+          ref="situationalJudgementOrCriticalAnalysisQT"
+        >
+          <h3 class="govuk-heading-m">
+            Situational judgement qualifying test (QT), Critical analysis qualifying test (QT)
+          </h3>
+          <DateInput
+            id="test-date"
+            v-model="exercise.sjcaTestDate"
+            label="Test date"
+          />
+          <TimeInput
+            id="test-start-time"
+            v-model="exercise.sjcaTestStartTime"
+            label="Start time"
+          />
+          <TimeInput
+            id="test-end-time"
+            v-model="exercise.sjcaTestEndTime"
+            label="End time"
+          />
+          <DateInput
+            id="test-outcome"
+            v-model="exercise.sjcaTestOutcome"
+            label="Outcome to candidates"
+            type="month"
+          />
+        </div>
 
-        <h3 class="govuk-heading-m">
-          Scenario test qualifying test (QT)
-        </h3>
-        <DateInput
-          id="scenario-test-date"
-          v-model="exercise.scenarioTestDate"
-          label="Test date"
-        />
-        <TimeInput
-          id="scenario-test-start-time"
-          v-model="exercise.scenarioTestStartTime"
-          label="Start time"
-        />
-        <TimeInput
-          id="test-end-time"
-          v-model="exercise.scenarioTestEndTime"
-          label="End time"
-        />
+        <div
+          v-if="scenarioQT"
+          ref="scenarioQT"
+        >
+          <h3 class="govuk-heading-m">
+            Scenario test qualifying test (QT)
+          </h3>
+          <DateInput
+            id="scenario-test-date"
+            v-model="exercise.scenarioTestDate"
+            label="Test date"
+          />
+          <TimeInput
+            id="scenario-test-start-time"
+            v-model="exercise.scenarioTestStartTime"
+            label="Start time"
+          />
+          <TimeInput
+            id="test-end-time"
+            v-model="exercise.scenarioTestEndTime"
+            label="End time"
+          />
+          <DateInput
+            id="test-outcome"
+            v-model="exercise.scenarioTestOutcome"
+            label="Outcome to candidates"
+            type="month"
+          />
+        </div>
 
         <h2 class="govuk-heading-l">
           Independent assessors
@@ -138,6 +160,7 @@ export default {
       repeatableFields: {
         SelectionDay,
       },
+      exerciseShortlistingMethods: exercise.shortlistingMethods,
       exercise: {
         applicationOpenDate: exercise.applicationOpenDate || null,
         applicationCloseDate: exercise.applicationCloseDate || null,
@@ -154,6 +177,14 @@ export default {
         selectionDays: exercise.selectionDays || [],
       },
     };
+  },
+  computed: {
+    scenarioQT() {
+      return this.exerciseShortlistingMethods && this.exerciseShortlistingMethods.includes('Scenario test qualifying test (QT)');
+    },
+    situationalJudgementOrCriticalAnalysisQT() {
+      return this.exerciseShortlistingMethods && this.exerciseShortlistingMethods.includes('Situational judgement qualifying test (QT)') || this.exerciseShortlistingMethods.includes('Critical analysis qualifying test (QT)');
+    },
   },
   methods: {
     async save() {
