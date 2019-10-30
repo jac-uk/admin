231da14cd13f47e34c4a4e509a1310ac2f93fce9
Lauren Qurashi <laurenqurashi@me.com>
2019-10-30 10:34:18 +0000

    Hide Qualifications for non legal and show Memberships for non legal
    The Qualifications question will only render if the typeOfExercise is 
    not non-legal.
    
    The Memberships question will only render if the typeOfExercise is 
    non-legal
    
    Tests are passing.
    
    Code has been linted.

--- tests/unit/views/Exercises/Edit/Eligibility.spec.js
+++ tests/unit/views/Exercises/Edit/Eligibility.spec.js
@@ -75,25 +75,47 @@ describe('views/Exercises/Edit/Eligibility', () => {
       });
     });
 
-    describe('Post qualification experience', () => {
+    describe('Qualifications', () => {
       it('does not show the question if the role is a non-legal role', () => {
         wrapper.setData({ typeOfExercise: 'non-legal' });
-        expect(wrapper.find('#post-qualification-experience').exists()).toBe(false);
+        expect(wrapper.find('#qualifications').exists()).toBe(false);
       });
 
       it('does show the question if the role is a legal role', () => {
         wrapper.setData({ typeOfExercise: 'legal' });
-        expect(wrapper.find('#post-qualification-experience').exists()).toBe(true);
+        expect(wrapper.find('#qualifications').exists()).toBe(true);
       });
 
       it('does show the question if the role is a senior role', () => {
         wrapper.setData({ typeOfExercise: 'senior' });
-        expect(wrapper.find('#post-qualification-experience').exists()).toBe(true);
+        expect(wrapper.find('#qualifications').exists()).toBe(true);
       });
 
       it('does show the question if the role is a leadership role', () => {
+        wrapper.setData({ typeOfExercise: 'leadershop' });
+        expect(wrapper.find('#qualifications').exists()).toBe(true);
+      });
+    });
+
+    describe('Memberships', () => {
+      it('does not show the question if the role is a legal role', () => {
+        wrapper.setData({ typeOfExercise: 'legal' });
+        expect(wrapper.find('#memberships').exists()).toBe(false);
+      });
+
+      it('does not show the question if the role is a senior role', () => {
+        wrapper.setData({ typeOfExercise: 'senior' });
+        expect(wrapper.find('#memberships').exists()).toBe(false);
+      });
+
+      it('does not show the question if the role is a leadership role', () => {
         wrapper.setData({ typeOfExercise: 'leadership' });
-        expect(wrapper.find('#post-qualification-experience').exists()).toBe(true);
+        expect(wrapper.find('#memberships').exists()).toBe(false);
+      });
+
+      it('does show the question if the role is a non-legal role', () => {
+        wrapper.setData({ typeOfExercise: 'non-legal' });
+        expect(wrapper.find('#memberships').exists()).toBe(true);
       });
     });
   });
