c655d693c4a967942791b469caf637f276c145ba
Lauren Qurashi <laurenqurashi@me.com>
2019-11-05 08:36:20 +0000

    Made applications summary page and baseline test
    Made summary page amongst the show pages.
    Added very simple test.

---
+++ tests/unit/views/Exercises/Show/Applications.spec.js
@@ -0,0 +1,15 @@
+import Applications from '@/views/Exercises/Show/Applications';
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+
+const createTestSubject = () => {
+  return shallowMount(Applications);
+};
+
+describe('@/views/Exercises/Show', () => {
+  describe('template', () => {
+    it('renders the component', () => {
+      let wrapper = createTestSubject();
+      expect(wrapper.find('table').exists()).toBe(true);
+    });
+  });
+});
