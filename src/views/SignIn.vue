2e54be6cee3e9d3053ff810e245c7e007d0ae5a6
londonprojects <jeremy.suarez@judicialappointments.digital>
2019-12-03 16:48:22 +0000

    move-logo-left-aligned

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,15 +1,6 @@
 <template>
   <div class="govuk-grid-row">
     <div class="govuk-grid-column-full">
-      <div class="text-center">
-        <img
-          src="@/assets/jac-logo.svg"
-          alt="Judicial Appointments Commission"
-          width="197"
-          height="66"
-          style="text-align: center"
-        >
-      </div>
       <p class="govuk-hint govuk-body govuk-!-margin-bottom-7">
         Sign in to admin dashboard with your judicialappointments.digital Google account.
       </p>
