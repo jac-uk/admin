70020578250a44cb526d5e5bfed832e49285359e
Lauren Qurashi <laurenqurashi@me.com>
2019-12-06 16:38:45 +0000

    Added content to the assessment options page

--- src/views/Exercises/Edit/AssessmentOptions.vue
+++ src/views/Exercises/Edit/AssessmentOptions.vue
@@ -7,9 +7,35 @@
           Assessment options
         </h1>
 
-        <p class="govuk-body-l">
-          [form]
-        </p>
+        <RadioGroup
+          id="shortlisting-methods"
+          v-model="exercise.assessmentOptions"
+        >
+          <RadioItem
+            value="self-assessment-with-competencies"
+            label="Self Assessment with competencies"
+          />
+          <RadioItem
+            value="statement-of-suitability-with-competencies"
+            label="Statement of Suitability with competencies"
+          />
+          <RadioItem
+            value="statement-of-suitability-with-skills-and-abilities"
+            label="Statement of Suitability with skills and abilities"
+          />
+          <RadioItem
+            value="statement-of-suitability-with-skills-and-abilities-and-cv"
+            label="Statement of Suitability with skills and abilities and CV"
+          />
+          <RadioItem
+            value="statement-of-eligibility"
+            label="Statement of eligibility"
+          />
+          <RadioItem
+            value="none"
+            label="None"
+          />
+        </RadioGroup>
 
         <button class="govuk-button">
           Save and continue
@@ -21,13 +47,21 @@
 
 <script>
 import BackLink from '@/components/BackLink';
+import RadioGroup from '@/components/Form/RadioGroup';
+import RadioItem from '@/components/Form/RadioItem';
 
 export default {
   components: {
     BackLink,
+    RadioGroup,
+    RadioItem,
   },
-  data() {
-    const exercise = this.$store.getters['exerciseDocument/data']();
+  data(){
+    const defaults = {
+      assessmentOptions: null,
+    };
+    const data = this.$store.getters['exerciseDocument/data']();
+    const exercise = { ...defaults, ...data };
     return {
       exercise: exercise,
     };
