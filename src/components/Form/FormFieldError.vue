9cf93d114c442d4814c87e87ba36a16c68506627
Warren Searle <warren@precise-minds.co.uk>
2019-12-09 17:06:10 +0000

    Form validation pattern for roll out across forms

---
+++ src/components/Form/FormFieldError.vue
@@ -0,0 +1,24 @@
+<template>
+  <span
+    v-if="errorMessage"
+    :id="`${id}-error`"
+    class="govuk-error-message"
+  >
+    <span class="govuk-visually-hidden">Error:</span> {{ errorMessage }}
+  </span>  
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: String,
+      default: '',
+    },
+    errorMessage: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
