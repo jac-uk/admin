beabc0f060cc78ce8fa470e4811bc1637e17acd8
Warren Searle <warren@precise-minds.co.uk>
2019-12-09 17:06:10 +0000

    Form validation pattern for roll out across forms

--- src/components/Form/TextField.vue
+++ src/components/Form/TextField.vue
@@ -15,27 +15,31 @@
     >
       {{ hint }}
     </span>
-    <span v-if="hasError" :id="`${id}-error`" class="govuk-error-message">
-      <span class="govuk-visually-hidden">Error:</span> {{ errorMessage }}
-    </span>
+    <FormFieldError
+      :id="id"
+      :error-message="errorMessage"
+    />
     <input
       :id="id"
       v-model="text"
       class="govuk-input"
       :class="{inputClass, 'govuk-input--error': hasError}"
-      :type="type"
-      @input="validate"
-    />
+      :type="fieldType"
+      @change="validate"
+    >
   </div>
 </template>
 
 <script>
+import FormField from '@/components/Form/FormField';
+import FormFieldError from '@/components/Form/FormFieldError';
+
 export default {
+  components: {
+    FormFieldError,
+  },
+  extends: FormField,
   props: {
-    label: {
-      default: '',
-      type: String,
-    },
     inputClass: {
       default: '',
       type: String,
@@ -44,28 +48,10 @@ export default {
       default: '',
       type: String,
     },
-    hint: {
-      default: '',
-      type: String,
-    },
-    id: {
-      default: '',
-      type: String,
-    },
     type: {
       default: 'text',
       type: String,
     },
-    required: Boolean,
-    checkErrors: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  data() {
-    return {
-      errorMessage: '',
-    };
   },
   computed: {
     text: {
@@ -76,33 +62,15 @@ export default {
         this.$emit('input', val);
       },
     },
-    hasError() {
-      return this.errorMessage ? true :  false;
-    },
-  },
-  watch: {
-    checkErrors: function (newVal, oldVal) {
-      console.log('showErrors changed', oldVal, newVal);
-      this.validate();
-    },
-  }, 
-  methods: {
-    validate() {
-      console.log('validate');
-      this.setError('');
-      if (this.checkErrors) {
-        if (this.required && !this.value) {
-          this.setError(`Enter your ${this.label}`);
-        }
+    fieldType() {
+      switch(this.type) {
+      case 'text':
+      case 'email':
+        return 'text'; // we are using custom email validation, so don't use html5 input types
+      default:
+        return this.type;
       }
     },
-    setError(message) {
-      this.errorMessage = message;
-      // this.errors[this.id] = message;
-      // console.log('update:errors', this.errors);
-      //this.$emit('update:errors', this.errorMessage);
-      this.$emit('handle-error', { id: this.id, message: this.errorMessage });
-    },
-  }, 
+  },
 };
 </script>
