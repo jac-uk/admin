a53124ec758ffa7ed6818f74e64c24e789ddef82
Warren Searle <warrensearle@Warrens-iMac.local>
2019-12-30 16:13:45 +0000

    Edit working preferences

---
+++ src/components/RepeatableFields/Answer.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <TextField
+      :id="`answer_${ident}_${index}`"
+      v-model="row.answer"
+      :label="`Answer option ${1 + index}`"
+    />
+    <slot name="removeButton" />
+  </div>
+</template>
+
+<script>
+import TextField from '@/components/Form/TextField';
+
+export default {
+  name: 'Answer',
+  components: {
+    TextField,
+  },
+  props: {
+    row: {
+      required: true,
+      type: Object,
+    },
+    index: {
+      required: true,
+      type: Number,
+    },
+    ident: {
+      required: false,
+      type: String,
+      default: '',
+    },    
+  },
+};
+</script>
