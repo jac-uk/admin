d871eb3103dd60f69ceb2e3b09ab3fb8565177a4
Lauren Qurashi <laurenqurashi@me.com>
2020-01-02 16:09:33 +0000

    Added two RF components to the contacts page
    - Added Senior Selection Exercise Manager and Selection Exercise Manager to the Repeatable Fields components. 
    
    - Imported and used these RF components on the Edit/Contacts page.
    
    - Amended the rendering of these RF components on the Show/Contacts page.

---
+++ src/components/RepeatableFields/SeniorSelectionExerciseManager.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <TextField
+      :id="`senior_selection_exercise_manager_${index}`"
+      v-model="row.name"
+      type="email"
+      label="Senior selection exercise manager"
+      required
+    />
+    <slot name="removeButton" />
+  </div>
+</template>
+
+<script>
+import TextField from '@/components/Form/TextField';
+
+export default {
+  components: {
+    TextField,
+  },
+  props: {
+    row: {
+      required: true,
+      type: Object,
+    },
+    index: {
+      required: true,
+      type: Number,
+    },
+  },
+};
+</script>
