ec3c43c21335b14d5c2c73e02f972c52a7de6833
Warren Searle <warren@precise-minds.co.uk>
2020-01-10 11:58:44 +0000

    Update exercise collection to filter by favourites

--- src/store/exercise/collection.js
+++ src/store/exercise/collection.js
@@ -5,16 +5,40 @@ import vuexfireSerialize from '@/helpers/vuexfireSerialize';
 export default {
   namespaced: true,
   actions: {
-    bind: firestoreAction(({ bindFirestoreRef }) => {
-      const firestoreRef = firestore
-        .collection('exercises').orderBy('referenceNumber', 'desc');
+    bind: firestoreAction(({ rootState, state, commit, bindFirestoreRef }) => {
+      let firestoreRef;
+      if (state.isFavourites === null) { commit('updateFavourites', true); }
+      if (state.isFavourites) {
+        firestoreRef = firestore
+        .collection('exercises')
+        .where('favouriteOf', 'array-contains', rootState.auth.currentUser.uid)
+        .orderBy('referenceNumber', 'desc');
+      } else {
+        firestoreRef = firestore
+        .collection('exercises')
+        .orderBy('referenceNumber', 'desc');
+      }
       return bindFirestoreRef('records', firestoreRef, { serialize: vuexfireSerialize });
     }),
     unbind: firestoreAction(({ unbindFirestoreRef }) => {
       return unbindFirestoreRef('records');
     }),
+    showFavourites: ({ commit, dispatch }) => {
+      commit('updateFavourites', true);
+      dispatch('bind');
+    },  
+    showAll: ({ commit, dispatch }) => {
+      commit('updateFavourites', false);
+      dispatch('bind');
+    },       
   },
+  mutations: {
+    updateFavourites(state, isFavourites) {
+      state.isFavourites = isFavourites;
+    },
+  },  
   state: {
     records: [],
+    isFavourites: null,
   },
 };
