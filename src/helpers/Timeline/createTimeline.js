662ddbc642aaa526a0cbcb753ea808c5a5011e26
warrensearle <warren@precise-minds.co.uk>
2020-01-20 00:33:36 +0000

    Revert "Timeline. Display in chronological order"
    This reverts commit bbccd9c4b8145c4c764ff16ca43648eafc97c3af.

--- src/helpers/Timeline/createTimeline.js
+++ src/helpers/Timeline/createTimeline.js
@@ -1,21 +1,11 @@
 const createTimeline = (timeline, maxEntriesNum) => {
-  let timelineArray = timeline.filter((item) => item.date !== null).sort(ascendingDate);
+  let timelineArray = timeline.filter((item) => item.date !== null);
 
   if(maxEntriesNum) {
     return timelineArray.slice(0, maxEntriesNum);
   }
 
   return timelineArray;
-};
-
-const ascendingDate = ( item1, item2 ) => {
-  if (item1.date < item2.date) {
-    return -1;
-  } else if (item1.date > item2.date) {
-    return 1;
-  } else {
-    return 0;
-  }
 };
 
 export default createTimeline;
