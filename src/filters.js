693af64fe835b1dd72dacb6667f643b85116909f
Warren Searle <warren@precise-minds.co.uk>
2020-01-20 00:19:25 +0000

    New filters for displaying human readable data

--- src/filters.js
+++ src/filters.js
@@ -13,6 +13,18 @@ Vue.filter('formatDate',
   }
 });
 
+Vue.filter('toHumanCase',
+(value) => {
+  if (value) {
+    return value.replace(/([A-Z0-9])/g, ' $1');
+  }
+});
+
+Vue.filter('toYesNo',
+(value) => {
+  return value ? 'Yes' : 'No';
+});
+
 Vue.filter('lookup',
 (value) => {
   if (value) {
