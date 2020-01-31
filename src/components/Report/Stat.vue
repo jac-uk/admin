370339dde57b772f430eb19323b8abb061c5d182
Warren Searle <warren@precise-minds.co.uk>
2020-01-31 16:31:58 +0000

    Diversity report view and new `Stat` component

---
+++ src/components/Report/Stat.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <span class="percent">{{ stat.percent | formatNumber(2) }} %</span> <span class="total">({{ stat.total }})</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    stat: {
+      required: true,
+      type: Object,
+    },
+  },
+};
+</script>
