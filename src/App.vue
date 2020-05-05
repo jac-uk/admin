faacdc412d14ad7a317a78fc57ec4b5efc734742
Tom Russell <TR115251@hotmail.co.uk>
2020-05-05 14:25:49 +0100

    header updated

--- src/App.vue
+++ src/App.vue
@@ -1,82 +1,91 @@
 <template>
   <div class="govuk-width-container">
-    <div
-      id="header"
-      style="padding-top: 0px"
-      class="govuk-main-wrapper"
-    >
-      <header class="header">
-        <!-- HEADER SECTION -->
-        <div>
-          <div>
-            <div class="govuk-grid-row">
-              <div class="govuk-grid-column-two-thirds organisation__margin-bottom">
-                <div class="gem-c-organisation-logo brand--ministry-of-justice">
-                  <a href="https://judicialappointments.gov.uk/">
-                    <img
-                      src="@/assets/jac-logo.svg"
-                      alt="Judicial Appointments Commission"
-                      width="197"
-                      height="66"
-                      style="text-align: center"
-                    >
-                  </a>
-                </div>
-              </div>
-              <div class="govuk-grid-column-one-third organisation__margin-bottom">
-                <button
-                  v-if="$route.name !== 'sign-in'"
-                  class="govuk-button govuk-!-margin-right-1"
-                  @click="signOut"
-                >
-                  Sign Out
-                </button>
-                <button
-                  style="background-color: #753880;"
-                  target="_blank"
-                  class="govuk-button"
-                  onclick="location.href='https://apply.judicialappointments.digital/vacancies';"
-                >
-                  Vacancies
-                </button>
-                <br>
-                <span
-                  v-if="isSignedIn"
-                  class="app-c-topic-list__item nostyle"
-                >
-                  <b>You are now signed in as {{ userName }}</b>
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <!-- HEADER SECTION -->
-      </header>
-      <div class="govuk-grid-row">
-        <div class="govuk-grid-column-full">
-          <div class="govuk-phase-banner">
-            <p class="govuk-phase-banner__content">
-              <strong class="govuk-tag govuk-phase-banner__content__tag">
-                alpha
-              </strong>
-              <span class="govuk-phase-banner__text">
-                This is a new service – your feedback will help us improve it.
-              </span>
-            </p>
-          </div>
-        </div>
+    <div class="header-background clearfix">
+      <div class="header-title govuk-!-margin-bottom-2">
+        <a
+          href="https://judicialappointments.gov.uk/"
+          class="govuk-link govuk-link--no-visited-state govuk-!-font-size-24 govuk-!-font-weight-bold"
+        >
+          Internal service name
+        </a>
+        <nav class="float-right">
+          <ul
+            id="navigation"
+            class="govuk-header__navigation user-menu"
+          >
+            <li class="govuk-header__navigation-item">
+              <a
+                :disabled="isVacanciesPage"
+                class="govuk-header__link"
+                @click="gotoVacancies"
+              >
+                Vacancies
+              </a>
+            </li>
+            <li class="govuk-header__navigation-item">
+              <a
+                v-if="isSignedIn"
+                class="govuk-header__link"
+                @click="signOut"
+              >
+                Sign out
+              </a>
+              <a
+                v-else
+                :disabled="isSignInPage"
+                class="govuk-header__link"
+                @click="signIn"
+              >
+                Sign In
+              </a>
+              <!-- <span
+                v-if="isSignedIn"
+                class="app-c-topic-list__item nostyle"
+              >
+                <b>You are now signed in as {{ userName }}</b>
+              </span> -->
+            </li>
+          </ul>
+        </nav>
       </div>
+    </div>
+    <div class="govuk-phase-banner">
+      <p class="govuk-phase-banner__content">
+        <strong class="govuk-tag govuk-phase-banner__content__tag">beta</strong>
+        <span class="govuk-phase-banner__text">
+          This is a new service – your 
+          <a 
+            class="govuk-link govuk-link--no-visited-state"
+            target="_blank" 
+            href="https://docs.google.com/forms/d/e/1FAIpQLSexm0qgMV0tOQTFP4QUSegOOX89VeYhWwuofV---JZTOEXGIQ/viewform"
+          >
+            feedback
+          </a>
+          will help us to improve it.
+        </span>
+      </p>
     </div>
     <main id="main-content">
       <RouterView />
     </main>
   </div>
 </template>
+
 <script>
 import { auth } from '@/firebase';
+
 export default {
   name: 'App',
+  data: () => ({
+    //
+  }),
   computed: {
+    isSignInPage() {
+      return this.$route.name === 'sign-in';
+    },
+    isVacanciesPage() {
+      return this.$route.name === 'vacancies';
+    },
     isSignedIn() {
       return this.$store.getters['auth/isSignedIn'];
     },
@@ -85,20 +94,65 @@ export default {
     },
   },
   methods: {
+    signIn() {
+      this.$router.push({ name: 'sign-in' });
+    },
     signOut() {
       auth().signOut();
-      this.$router.go('/sign-in');
+      if (this.$route.name != 'vacancies') {
+        this.$router.push({ name: 'vacancies' });
+      }
+    },
+    gotoVacancies() {
+      this.$router.push({ name: 'vacancies' });
     },
   },
 };
 </script>
+
 <style lang="scss">
-  // Required to include global main.scss styles
+  .govuk-grid-column-one-half {
+    width: 100%;
+    float: left;
+}
   .header {
     background-color: #fafafa;
     padding: 1.25em 0.625em 0 0.625em;
   };
+
   .govuk-main-wrapper {
     padding-top: 0px;
   }
+
+  .float-right {
+      float: right;
+  }
+
+.govuk-main-wrapper {
+	padding-top: 0 !important;
+}
+
+.header-background {
+    background-color: #f7f7f7;
+	padding: 20px 20px 10px 20px;
+	margin-top: 0 !important;
+}
+
+.header-background h1 a,
+.header-background .header-title a,
+#navigation li a  {
+	color: #753880 !important;
+	text-decoration: none;
+}
+
+.header-background h1 a:hover,
+.header-background .header-title a:hover,
+#navigation li a:hover {
+	text-decoration: underline;
+}
+
+.header-background span {
+	color: #753880 !important;
+}
+
 </style>
