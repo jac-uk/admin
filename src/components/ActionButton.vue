8b8faa5e0fff4a510fd6b216df262863471c608e
YaaL <YaaL@users.noreply.github.com>
2020-05-05 14:02:47 +0100

    ActionButton component

---
+++ src/components/ActionButton.vue
@@ -0,0 +1,78 @@
+<template>
+  <button
+    class="govuk-button govuk-button--secondary jac-actionbutton"
+    :class="computedClasses"
+    :disabled="isLoading"
+    v-on="listeners"
+  >
+    <span
+      v-if="isLoading"
+      class="spinner-border spinner-border-sm"
+    />
+    <slot
+      name="success"
+      v-else-if="isSuccess"
+    >
+      Success!
+    </slot>
+    <slot
+      name="error"
+      v-else-if="hasError"
+    >
+      Error!
+    </slot>
+    <slot>Submit</slot>
+  </button>
+</template>
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    timeout: {
+      type: Number,
+      default: 2000,
+    },
+  },
+  data: () => ({
+    isLoading: false,
+    hasError: false,
+    isSuccess: false,
+  }),
+  computed: {
+    listeners() {
+      return {
+        ...this.$listeners,
+        click: this.handleClick,
+      };
+    },
+    computedClasses() {
+      return {
+        'jac-actionbutton--warning': this.hasError,
+        'jac-actionbutton--success': this.isSuccess,
+        'jac-actionbutton--loading': this.isLoading,
+      };
+    },
+  },
+  methods: {
+    async handleClick(e) {
+      try {
+        this.isLoading = true;
+
+        await this.$listeners.click(e);
+
+        this.resetDelayed('isSuccess');
+      } catch (error) {
+        this.resetDelayed('hasError');
+      } finally {
+        this.isLoading = false;
+      }
+    },
+    resetDelayed(property) {
+      this[property] = true;
+      setTimeout(() => {
+        this[property] = false;
+      }, this.timeout);
+    },
+  },
+};
+</script>
