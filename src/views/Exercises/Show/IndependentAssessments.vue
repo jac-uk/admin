0e8a0d233df855e283f47503d1dffc06eff9f937
YaaL <YaaL@users.noreply.github.com>
2020-05-19 13:24:05 +0100

    #449 tweaks to IAs  #230 Updated Download button

--- src/views/Exercises/Show/IndependentAssessments.vue
+++ src/views/Exercises/Show/IndependentAssessments.vue
@@ -35,26 +35,41 @@
     <div
       v-if="!assessments.length"
     >
-      <p class="govuk-body">
-        No assessments requests sent yet.
-      </p>
-
       <ActionButton
-        class="moj-button-menu__item"
         @click="initialiseAssessments()"
       >
-        Request assessments
+        Start Assessments
+      </ActionButton>
+    </div>
+
+    <div v-else>
+      <ActionButton
+        @click="sendRequests()"
+      >
+        Send to all
       </ActionButton>
 
-      <table
-        v-if="applications.length"
-        class="govuk-table govuk-!-margin-top-4"
-      >
-        <caption class="govuk-table__caption">
-          Assessors listed by candidates
-        </caption>
+      <table class="govuk-table">
         <thead class="govuk-table__head">
           <tr class="govuk-table__row">
+            <!--<th
+              scope="col"
+              class="govuk-table__header app-custom-class"
+            >
+              <div class="govuk-checkboxes govuk-checkboxes&#45;&#45;small">
+                <div class="govuk-checkboxes__item">
+                  <input
+                    id="checkboxes"
+                    class="govuk-checkboxes__input"
+                    type="checkbox"
+                  >
+                  <label
+                    class="govuk-label govuk-checkboxes__label"
+                    for="checkboxes"
+                  />
+                </div>
+              </div>
+            </th>-->
             <th
               scope="col"
               class="govuk-table__header app-custom-class"
@@ -71,215 +86,111 @@
               scope="col"
               class="govuk-table__header app-custom-class"
             >
-              Assessors
+              Assessor
             </th>
             <th
               scope="col"
               class="govuk-table__header app-custom-class"
             >
-              Date submitted
+              Status
+            </th>
+            <th
+              scope="col"
+              class="govuk-table__header app-custom-class"
+            >
+              Actions
             </th>
           </tr>
         </thead>
         <tbody class="govuk-table__body">
           <tr
-            v-for="application in applications"
-            :key="application.id"
+            v-for="assessment in assessments"
+            :key="assessment.id"
             class="govuk-table__row"
           >
+            <!--<td class="govuk-table__cell">
+              <div class="govuk-checkboxes govuk-checkboxes&#45;&#45;small">
+                <div class="govuk-checkboxes__item">
+                  <input
+                    id="checkbox-application"
+                    class="govuk-checkboxes__input"
+                    type="checkbox"
+                  >
+                  <label
+                    class="govuk-label govuk-checkboxes__label"
+                  />
+                </div>
+              </div>
+            </td>-->
             <th
               scope="row"
               class="govuk-table__header"
             >
               <router-link
                 class="govuk-link"
-                :to="{name: 'exercise-show-application', params: { applicationId: application.id }}"
+                :to="{name: 'exercise-show-application', params: { applicationId: assessment.application.id }}"
               >
-                {{ application.referenceNumber }}
+                {{ assessment.application.referenceNumber }}
               </router-link>
             </th>
             <td class="govuk-table__cell">
-              <span v-if="application.personalDetails">{{ application.personalDetails.fullName }}</span>
+              {{ assessment.candidate.fullName }}
             </td>
             <td class="govuk-table__cell">
               <p class="govuk-body">
                 <a
-                  :href="`mailto:${application.firstAssessorEmail}`"
+                  :href="`mailto:${assessment.assessor.email}`"
                   class="govuk-link govuk-link--no-visited-state"
                   target="_blank"
                 >
-                  {{ application.firstAssessorFullName }}
-                </a>
-              </p>
-              <p class="govuk-body">
-                <a
-                  :href="`mailto:${application.secondAssessorEmail}`"
-                  class="govuk-link govuk-link--no-visited-state"
-                  target="_blank"
-                >
-                  {{ application.secondAssessorFullName }}
+                  {{ assessment.assessor.fullName }}
                 </a>
               </p>
             </td>
             <td class="govuk-table__cell">
-              {{ application.appliedAt | formatDate }}
+              {{ assessment.status | lookup }}
+            </td>
+            <td class="govuk-table__cell">
+              <div class="moj-button-menu">
+                <div
+                  v-if="assessment.status === 'completed'"
+                  class="moj-button-menu__wrapper"
+                >
+                  <DownloadLink
+                    v-if="true || assessment.fileRef"
+                    class="moj-button-menu__item"
+                    :file-name="assessment.fileRef"
+                    :exercise-id="exercise.id"
+                    :application-id="assessment.application.id"
+                    :assessor-id="assessment.assessor.id"
+                    title="Download assessment"
+                    type="button"
+                  />
+                </div>
+                <div
+                  v-else
+                  class="moj-button-menu__wrapper"
+                >
+                  <ActionButton
+                    class="moj-button-menu__item"
+                    @click="resendRequest(assessment.id)"
+                  >
+                    Request
+                  </ActionButton>
+
+                  <ActionButton
+                    class="moj-button-menu__item"
+                    @click="sendReminder(assessment.id)"
+                  >
+                    Reminder
+                  </ActionButton>
+                </div>
+              </div>
             </td>
           </tr>
         </tbody>
       </table>
     </div>
-
-    <table
-      v-else
-      class="govuk-table"
-    >
-      <thead class="govuk-table__head">
-        <tr class="govuk-table__row">
-          <!--<th
-            scope="col"
-            class="govuk-table__header app-custom-class"
-          >
-            <div class="govuk-checkboxes govuk-checkboxes&#45;&#45;small">
-              <div class="govuk-checkboxes__item">
-                <input
-                  id="checkboxes"
-                  class="govuk-checkboxes__input"
-                  type="checkbox"
-                >
-                <label
-                  class="govuk-label govuk-checkboxes__label"
-                  for="checkboxes"
-                />
-              </div>
-            </div>
-          </th>-->
-          <th
-            scope="col"
-            class="govuk-table__header app-custom-class"
-          >
-            Reference number
-          </th>
-          <th
-            scope="col"
-            class="govuk-table__header app-custom-class"
-          >
-            Candidate name
-          </th>
-          <th
-            scope="col"
-            class="govuk-table__header app-custom-class"
-          >
-            Assessor
-          </th>
-          <th
-            scope="col"
-            class="govuk-table__header app-custom-class"
-          >
-            Status
-          </th>
-          <th
-            scope="col"
-            class="govuk-table__header app-custom-class"
-          >
-            Uploaded
-          </th>
-          <th
-            scope="col"
-            class="govuk-table__header app-custom-class"
-          >
-            Actions
-          </th>
-        </tr>
-      </thead>
-      <tbody class="govuk-table__body">
-        <tr
-          v-for="assessment in assessments"
-          :key="assessment.id"
-          class="govuk-table__row"
-        >
-          <!--<td class="govuk-table__cell">
-            <div class="govuk-checkboxes govuk-checkboxes&#45;&#45;small">
-              <div class="govuk-checkboxes__item">
-                <input
-                  id="checkbox-application"
-                  class="govuk-checkboxes__input"
-                  type="checkbox"
-                >
-                <label
-                  class="govuk-label govuk-checkboxes__label"
-                />
-              </div>
-            </div>
-          </td>-->
-          <th
-            scope="row"
-            class="govuk-table__header"
-          >
-            <router-link
-              class="govuk-link"
-              :to="{name: 'exercise-show-application', params: { applicationId: assessment.application.id }}"
-            >
-              {{ assessment.application.referenceNumber }}
-            </router-link>
-          </th>
-          <td class="govuk-table__cell">
-            {{ assessment.candidate.fullName }}
-          </td>
-          <td class="govuk-table__cell">
-            <p class="govuk-body">
-              <a
-                :href="`mailto:${assessment.assessor.email}`"
-                class="govuk-link govuk-link--no-visited-state"
-                target="_blank"
-              >
-                {{ assessment.assessor.fullName }}
-              </a>
-            </p>
-          </td>
-          <td class="govuk-table__cell">
-            {{ assessment.status | lookup }}
-          </td>
-          <td class="govuk-table__cell">
-            <span v-if="assessment.file && assessment.file.name">
-              {{ assessment.file.uploadedAt | formatDate }}
-            </span>
-            <span v-else>Not yet received</span>
-          </td>
-          <td class="govuk-table__cell">
-            <div class="moj-button-menu">
-              <div
-                v-if="assessment.status === 'uploaded'"
-                class="moj-button-menu__wrapper"
-              >
-                <DownloadLink
-                  :file-name="`assessment/assessment.fileRef`"
-                  :exercise-id="exercise.id"
-                  :user-id="assessment.candidate.id"
-                  title="Independent Assessment"
-                />
-              </div>
-              <div
-                class="moj-button-menu__wrapper"
-              >
-                <ActionButton
-                  class="moj-button-menu__item"
-                  @click="resendRequest(assessment.id)"
-                >
-                  Request
-                </ActionButton>
-
-                <ActionButton
-                  class="moj-button-menu__item"
-                  @click="sendReminder(assessment.id)"
-                >
-                  Reminder
-                </ActionButton>
-              </div>
-            </div>
-          </td>
-        </tr>
-      </tbody>
-    </table>
   </div>
 </template>
 
@@ -287,24 +198,16 @@
 import { functions } from '@/firebase';
 import { isDateInFuture } from '@/helpers/date';
 import ActionButton from '@/components/ActionButton';
+import DownloadLink from '@/components/DownloadLink';
 
 export default {
   components: {
     ActionButton,
-  },
-  data () {
-    return {
-      initialisingAssessments: false,
-      sendingReminder: [],
-      resendingRequest: [],
-    };
+    DownloadLink,
   },
   computed: {
     exercise() {
       return this.$store.state.exerciseDocument.record;
-    },
-    applications() {
-      return this.$store.state.applications.records;
     },
     assessments() {
       return this.$store.state.assessments.records;
@@ -317,11 +220,9 @@ export default {
     },
   },
   created() {
-    this.$store.dispatch('applications/bind', { exerciseId: this.exercise.id, status: 'applied' });
     this.$store.dispatch('assessments/bind', { exerciseId: this.exercise.id });
   },
   beforeRouteUpdate (to, from, next) {
-    this.$store.dispatch('applications/bind', { exerciseId: this.exercise.id, status: 'applied' });
     this.$store.dispatch('assessments/bind', { exerciseId: this.exercise.id });
     next();
   },
