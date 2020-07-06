bed632a239d635b49a1ae039585b02c0e6ec4479
Hugh Wells <hugh@crablab.co.uk>
2020-07-06 19:04:17 +0100

    Tested and working pagination component

---
+++ tests/unit/components/Page/Pagination.spec.js
@@ -0,0 +1,125 @@
+import { shallowMount } from '@vue/test-utils';
+
+import Pagination from '@/components/Page/Pagination';
+
+const createTestSubject = (propsData) => {
+    return shallowMount(Pagination, {
+        propsData: {
+        ...propsData,
+        },
+    });
+};
+
+const numberOfPages = 10;
+
+describe('components/Page/Pagination', () => {
+    let wrapper;
+    let next = numberOfPages + 1;
+    let previous = 0;
+    let lowIndex = 1;
+    
+    beforeEach(() => {
+        wrapper = createTestSubject({ highIndex: numberOfPages });
+    });  
+
+    it('renders the component', () => {
+      expect(wrapper.exists()).toBe(true);
+    });
+
+    describe('properties', () => {
+        let prop;
+    
+        describe('highIndex', () => {
+          beforeEach(() => {
+            prop = Pagination.props.highIndex;
+          });
+    
+          it('is required', () => {
+            expect(prop.required).toBe(true);
+          });
+    
+          it('should be Number', () => {
+            expect(prop.type).toBe(Number);
+          });
+        });
+
+        describe('lowIndex', () => {
+            beforeEach(() => {
+              prop = Pagination.props.lowIndex;
+            });
+      
+            it('is not required', () => {
+              expect(prop.required).toBe(false);
+            });
+      
+            it('should be Number', () => {
+              expect(prop.type).toBe(Number);
+            });
+        });
+    });
+
+    describe('markup', () => {
+
+        it('renders correct number of elements', () => {
+            // Pages + Next and Previous buttons
+            expect(wrapper.findAll('li')).toHaveLength(numberOfPages + 2);
+        });
+
+        it('first page active', () => {
+            expect(wrapper.findAll('li').at(1).attributes('class')).toContain('moj-pagination__item--active');
+        });
+
+        it('previous button', () => {
+            expect(wrapper.findAll('li').at(previous).html()).toContain('Previous');
+        });
+
+        it('next button', () => {
+            expect(wrapper.findAll('li').at(next).html()).toContain('Next');
+        });
+
+    });
+
+    describe('change page', () => {
+
+        it('page forward', async () => {
+            await wrapper.findAll('a').at(next).trigger('click');
+            // Current page is displayed and disabled
+            expect(wrapper.findAll('li').at(lowIndex + 1).attributes('class')).toContain('moj-pagination__item--active', 'disable');
+            // Page we came from isn't
+            expect(wrapper.findAll('li').at(lowIndex).attributes('class')).toContain('pointer');
+            // Previous is enabled
+            expect(wrapper.findAll('a').at(previous).attributes('class')).toContain('pointer');
+        });
+
+        it('jump to end', async () => {
+            await wrapper.findAll('a').at(numberOfPages).trigger('click');
+            // Current page is displayed and disabled
+            expect(wrapper.findAll('li').at(numberOfPages).attributes('class')).toContain('moj-pagination__item--active', 'disable');
+             // Next is disabled
+             expect(wrapper.findAll('a').at(next).attributes('class')).toContain('disable');
+        });
+
+        it('jump to middle', async () => {
+            await wrapper.findAll('a').at(Math.round(numberOfPages / 2)).trigger('click');
+            // Current page is displayed and disabled
+            expect(wrapper.findAll('li').at(Math.round(numberOfPages / 2)).attributes('class')).toContain('moj-pagination__item--active', 'disable');
+            // Next is enabled
+            expect(wrapper.findAll('a').at(next).attributes('class')).toContain('pointer');
+            // Previous is enabled
+            expect(wrapper.findAll('a').at(previous).attributes('class')).toContain('pointer');
+        });
+
+        it('page backwards', async () => {
+            await wrapper.findAll('a').at(numberOfPages).trigger('click');
+            await wrapper.findAll('a').at(0).trigger('click');
+            // Current page is displayed and disabled
+            expect(wrapper.findAll('li').at(numberOfPages - 1).attributes('class')).toContain('moj-pagination__item--active', 'disable');
+            // Page we came from isn't
+            expect(wrapper.findAll('li').at(numberOfPages).attributes('class')).toContain('pointer');
+            // Next is enabled
+            expect(wrapper.findAll('a').at(next).attributes('class')).toContain('pointer');
+            // Previous is enabled
+            expect(wrapper.findAll('a').at(previous).attributes('class')).toContain('pointer');
+        });
+    });
+});
