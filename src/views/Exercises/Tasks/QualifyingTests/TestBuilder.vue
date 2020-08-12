3cee3339f86b090f80dc7908cf8d70e14eea6f89
Hugh Wells <hugh@crablab.co.uk>
2020-08-12 14:47:08 +0100

    Basic static version for just Critical Analysis

--- src/views/Exercises/Tasks/QualifyingTests/TestBuilder.vue
+++ src/views/Exercises/Tasks/QualifyingTests/TestBuilder.vue
@@ -1,3 +1,63 @@
 <template>
-  <div>Part of the QT creation for building the test questions</div>
+  <div class="govuk-grid-row">
+    <form>
+      <div class="govuk-grid-column-two-thirds">
+        <BackLink />
+
+        <h1 class="govuk-heading-xl">
+          Add questions to the test
+        </h1>
+
+        <ErrorSummary 
+          :errors="errors" 
+          :show-save-button="true" 
+        />
+        <fieldset class="govuk-fieldset govuk-!-margin-bottom-5">
+          <legend class="govuk-fieldset__legend govuk-fieldset__legend--l">
+            <h3 class="govuk-fieldset__heading">
+              Critical Analysis
+            </h3>
+          </legend>
+          <TextField
+            id="qt-critical-analysis"
+            label="Question X"
+          />      
+          <RepeatableFields
+            :component="repeatableFields.Answer"
+            ident="qt-critical-analysis"
+            required
+          />
+        </fieldset>
+
+        <button class="govuk-button">
+          Save question
+        </button>
+      </div>
+    </form>
+  </div>
 </template>
+<script>
+import Form from '@/components/Form/Form';
+import ErrorSummary from '@/components/Form/ErrorSummary';
+import TextField from '@/components/Form/TextField';
+import RepeatableFields from '@/components/RepeatableFields';
+import Answer from '@/components/RepeatableFields/Answer';
+import BackLink from '@/components/BackLink';
+
+export default {
+  components: {
+    ErrorSummary,
+    BackLink,
+    TextField,
+    RepeatableFields,
+  },
+  extends: Form,
+  data () {
+    return {
+      repeatableFields: {
+        Answer,
+      },
+    };
+  },
+};
+</script>
