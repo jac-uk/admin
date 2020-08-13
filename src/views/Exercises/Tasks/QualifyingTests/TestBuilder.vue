52c04963eb3ac2deacbf5e12b841c87f5ac4a78a
Hugh Wells <hugh@crablab.co.uk>
2020-08-13 10:26:43 +0100

    WIP: working with v-bind

--- src/views/Exercises/Tasks/QualifyingTests/TestBuilder.vue
+++ src/views/Exercises/Tasks/QualifyingTests/TestBuilder.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="govuk-grid-row">
-    <form>
+    <form @submit.prevent="validateAndSave">
       <div class="govuk-grid-column-two-thirds">
         <BackLink />
 
@@ -12,6 +12,29 @@
           :errors="errors" 
           :show-save-button="true" 
         />
+
+        <div 
+          v-if="hasQuestions"
+          class="govuk-summary-list__row"
+        >
+          <dd class="govuk-summary-list__value">
+            <ul class="govuk-list">
+              <li
+                v-for="(question, index) in questionArray"
+                :key="index"
+              >
+                {{ question.question }}
+                <span
+                  v-for="(answer, index) in question.answers"
+                  :key="index"
+                >
+                  {{ answer.answer }}
+                </span>
+              </li>
+            </ul>
+          </dd>
+        </div>
+        
         <fieldset class="govuk-fieldset govuk-!-margin-bottom-5">
           <legend class="govuk-fieldset__legend govuk-fieldset__legend--l">
             <h3 class="govuk-fieldset__heading">
@@ -19,12 +42,15 @@
             </h3>
           </legend>
           <TextField
-            id="qt-critical-analysis"
+            id="qt-critical-analysis-question"
+            v-model="currentQuestion.question"
+            ident="qt-critical-analysis-question"
             label="Question X"
           />      
           <RepeatableFields
+            v-model="currentQuestion.answers"
             :component="repeatableFields.Answer"
-            ident="qt-critical-analysis"
+            ident="qt-critical-analysis-answer"
             required
           />
         </fieldset>
@@ -54,10 +80,31 @@ export default {
   extends: Form,
   data () {
     return {
+      questionNumber: 1,
+      currentQuestion: {
+        'question': null, 
+        'answers': [],
+      },
+      questionArray: [],
       repeatableFields: {
         Answer,
       },
     };
+  },
+  computed: {
+    hasQuestions(){
+      return this.questionArray.length > 0 ? true : false;
+    },
+  },
+  methods: {
+    save(isValid) {
+      if (isValid) {
+        console.log(this.currentQuestion.question);
+        console.log(this.currentQuestion.answers.toString());
+                
+        this.questionArray.push(this.currentQuestion);
+      }
+    },
   },
 };
 </script>
