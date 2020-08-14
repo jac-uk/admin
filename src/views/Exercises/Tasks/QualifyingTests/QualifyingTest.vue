3a63adc92fe1e1db4d0472599e0ab3f12982b0c9
YaaL <YaaL@users.noreply.github.com>
2020-08-14 15:10:14 +0100

    QualifyingText parent component

--- src/views/Exercises/Tasks/QualifyingTests/QualifyingTest.vue
+++ src/views/Exercises/Tasks/QualifyingTests/QualifyingTest.vue
@@ -1,3 +1,45 @@
 <template>
-  <RouterView />
+  <div>
+    <LoadingMessage
+      v-if="loaded === false"
+      :load-failed="loadFailed"
+    />
+    <RouterView v-else />
+  </div>
 </template>
+
+<script>
+import LoadingMessage from '@/components/LoadingMessage';
+export default {
+  components: {
+    LoadingMessage,
+  },
+  data() {
+    return {
+      loaded: false,
+      loadFailed: false,
+    };
+  },
+  mounted() {
+    const id = this.$route.params.id;
+
+    this.$store.dispatch('qualifyingTest/bind', id)
+      .then((data) => {
+        if (data === null) {
+          this.redirectToErrorPage();
+        }
+        else {
+          this.loaded = true;
+        }
+      }).catch((e) => {
+        this.loadFailed = true;
+        throw e;
+      });
+  },
+  methods: {
+    redirectToErrorPage() {
+      this.$router.replace({ name: 'exercise-not-found' });
+    },
+  },
+};
+</script>
