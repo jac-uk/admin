d76cf234c4aff4d2315b2ee3b7a3b17fc720eaf6
Lisias (Lee) Loback <lisias@loback.com>
2020-08-16 13:01:46 +0100

    #776 QT Store and others - Added exercise Id - Created template for Test Responses - Changed names of the functions in the View

--- src/views/Exercises/Tasks/QualifyingTests/QualifyingTest/Responses.vue
+++ src/views/Exercises/Tasks/QualifyingTests/QualifyingTest/Responses.vue
@@ -1,5 +1,54 @@
 <template>
   <div>
     [ Qualifying Test | Responses | {{ $route.params.status }} ]
+    <h2 class="govuk-heading-m">
+      Qualifying Test Responses: {{ $route.params.id }}
+    </h2>
+    <div>
+      <Table 
+        data-key="id"
+        :data="getPaginated"
+        :columns="[
+          { title: 'Reference number' },
+          { title: 'Name' },
+          { title: 'Status' },
+        ]"
+      >
+        <template #row="{row}">
+          <TableCell>
+            <RouterLink
+              :to="{ name: 'exercise-application', params: { applicationId: row.id } }"
+            >
+              {{ row.application.referenceNumber }}
+            </RouterLink> 
+          </TableCell>
+          <TableCell>
+            <RouterLink
+              :to="{ name: 'candidates-view', params: { id: row.candidate.id } }"
+            >
+              {{ row.candidate.fullName }}
+            </RouterLink> 
+          </TableCell>
+          <TableCell>{{ row.status | lookup }}</TableCell>
+        </template>
+      </Table>   
+    </div>
   </div>
 </template>
+
+<script>
+import Table from '@/components/Page/Table/Table'; 
+import TableCell from '@/components/Page/Table/TableCell'; 
+
+export default {
+  components: {
+    Table,
+    TableCell,
+  },
+  computed: {
+    getPaginated() {
+      return [];
+    },
+  },
+};
+</script>
