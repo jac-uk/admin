8d680cae3c141c505069cbb5d270c2114b3f2848
Lisias (Lee) Loback <lisias@loback.com>
2020-08-18 18:53:25 +0100

    #762 List of Response Status

--- src/store/qualifyingTest/qualifyingTestResponses.js
+++ src/store/qualifyingTest/qualifyingTestResponses.js
@@ -7,9 +7,15 @@ const collectionRef = firestore.collection('qualifyingTestResponses');
 export default {
   namespaced: true,
   actions: {
-    bind: firestoreAction(({ bindFirestoreRef }, { qualifyingTestId } ) => {
-      const firestoreRef = collectionRef
+    bind: firestoreAction(({ bindFirestoreRef }, { qualifyingTestId, searchStatus } ) => {
+      // eslint-disable-next-line prefer-const
+      let firestoreRef = collectionRef
         .where('qualifyingTest.id', '==', qualifyingTestId);
+
+      if (searchStatus !== 'all' && searchStatus !== '') {
+        firestoreRef = firestoreRef
+          .where('status', '==', searchStatus);
+      }
 
       return bindFirestoreRef('records', firestoreRef, { serialize: vuexfireSerialize });
     }),
--- src/views/Exercises/Tasks/QualifyingTests/QualifyingTest/Responses.vue
+++ src/views/Exercises/Tasks/QualifyingTests/QualifyingTest/Responses.vue
@@ -1,5 +1,66 @@
 <template>
-  <div>
-    [ Qualifying Test | Responses | {{ $route.params.status }} ]
-  </div>
+  <div class="govuk-grid-column-full govuk-!-margin-bottom-1">
+    <h3 
+      class="govuk-heading-m"
+    >
+      Qualifying Test: Recorder 2020 Qualifying Test [{{ qualifyingTestId }} / {{ searchStatus }}] 
+    </h3>
+
+    <Table 
+      data-key="id"
+      :data="applications"
+      :columns="[
+        { title: 'Reference number' },
+        { title: 'Name' },
+        { title: 'Status' },
+      ]"
+    >
+      <template #row="{row}">
+        <TableCell>
+          <RouterLink
+            :to="{ name: 'exercise-application', params: { applicationId: row.id } }"
+          >
+            {{ row.id }}
+          </RouterLink> 
+        </TableCell>
+        <TableCell>
+          <RouterLink
+            :to="{ name: 'candidates-view', params: { id: row.candidate.id } }"
+          >
+            {{ row.candidate.id }}
+          </RouterLink> 
+        </TableCell>
+        <TableCell>{{ row.status}}</TableCell>
+      </template>
+    </Table>
+  </div>    
 </template>
+
+<script>
+import Table from '@/components/Page/Table/Table'; 
+import TableCell from '@/components/Page/Table/TableCell'; 
+
+export default {
+  components: {
+    Table,
+    TableCell,
+  },
+  computed: {
+    applications() {
+      const responsesList = this.$store.state.qualifyingTestResponses.records;
+      // eslint-disable-next-line no-console
+      console.log('responsesList', responsesList);
+      return responsesList;
+    },
+    qualifyingTestId() {
+      return this.$route.params.qualifyingTestId;
+    },
+    searchStatus() {
+      return this.$route.params.status;
+    },
+  },
+  async created() {
+    this.$store.dispatch('qualifyingTestResponses/bind', { qualifyingTestId: this.qualifyingTestId, searchStatus: this.searchStatus });
+  },
+};
+</script>
