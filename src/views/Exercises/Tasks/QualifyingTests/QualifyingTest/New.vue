adac054b5b49a3d4ba8cd3555e9dfb13f5d37efb
Warren Searle <warren@precise-minds.co.uk>
2020-09-24 17:40:56 +0100

    #876 Create dry run QT

--- src/views/Exercises/Tasks/QualifyingTests/QualifyingTest/New.vue
+++ src/views/Exercises/Tasks/QualifyingTests/QualifyingTest/New.vue
@@ -25,6 +25,14 @@
         />
       </RadioGroup>
 
+      <Checkbox
+        id="is-dry-run"
+        v-model="isDryRun"
+        label="Dry run"
+      >
+        Yes, this is a dry run
+      </Checkbox>
+
       <button class="govuk-button">
         Save and continue
       </button>
@@ -37,6 +45,7 @@ import Form from '@/components/Form/Form';
 import ErrorSummary from '@/components/Form/ErrorSummary';
 import RadioGroup from '@/components/Form/RadioGroup';
 import RadioItem from '@/components/Form/RadioItem';
+import Checkbox from '@/components/Form/Checkbox';
 import { QUALIFYING_TEST } from '@/helpers/constants';
 
 export default {
@@ -44,6 +53,7 @@ export default {
     ErrorSummary,
     RadioGroup,
     RadioItem,
+    Checkbox,
   },
   extends: Form,
   data(){
@@ -51,6 +61,7 @@ export default {
 
     const defaults = {
       type: null,
+      isDryRun: false,
       vacancy: {
         mailbox: exercise.exerciseMailbox,
         contactPhone: exercise.exercisePhoneNumber,
@@ -74,6 +85,9 @@ export default {
   },
   methods: {
     async save() {
+      if (this.isDryRun) {
+        this.qualifyingTest.mode = QUALIFYING_TEST.MODE.DRY_RUN;
+      }
       const qualifyingTestId = await this.$store.dispatch('qualifyingTest/create', this.qualifyingTest);
 
       this.$router.push({
