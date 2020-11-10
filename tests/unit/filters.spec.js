585552c5d012c5d6bd5b7cbc65dc6301fa92b069
Maria Brookes <maria_brookes@yahoo.co.uk>
2020-11-10 15:04:20 +0000

    #1055 Added Held fee paid judicial role into the export file

---
+++ tests/unit/filters.spec.js
@@ -0,0 +1,31 @@
+import { heldFeePaidJudicialRole } from '@/filters';
+
+  describe('when input value is string `fee-paid-court-post`', () => {
+    it('returns string `Yes`', () => {
+      expect(heldFeePaidJudicialRole('fee-paid-court-post')).toBe('Yes');
+    });
+  });
+
+describe('when input value is string `fee-paid-tribunal-post`', () => {
+  it('returns string `Yes`', () => {
+    expect(heldFeePaidJudicialRole('fee-paid-tribunal-post')).toBe('Yes');
+  });
+});
+
+describe('when input value is string `other-fee-paid-judicial-office`', () => {
+  it('returns string `Yes`', () => {
+    expect(heldFeePaidJudicialRole('other-fee-paid-judicial-office')).toBe('Yes');
+  });
+});
+
+describe('when input value is string `prefer-not-to-say`', () => {
+  it('returns string `Prefer not to say`', () => {
+    expect(heldFeePaidJudicialRole('prefer-not-to-say')).toBe('Prefer not to say');
+  });
+});
+
+describe('when input value is boolean `false`', () => {
+  it('returns string `No`', () => {
+    expect(heldFeePaidJudicialRole(false)).toBe('No');
+  });
+});
