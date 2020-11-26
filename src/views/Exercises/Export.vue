9b800c27cd4d4254b23bc5963491256fa5b9b780
Warren Searle <warren@precise-minds.co.uk>
2020-11-26 11:21:52 +0000

    #959 Sort and paging for Exercises Dashboard
    Plus export selected data to XLSX (prototype)

---
+++ src/views/Exercises/Export.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="govuk-grid-row">
+    <div class="govuk-grid-column-full">
+      <BackLink />
+      <h1
+        class="govuk-heading-l"
+      >
+        Export Exercise Data
+      </h1>
+      <form @submit.prevent="validateAndSave">
+        <ErrorSummary
+          :errors="errors"
+        />
+        <button class="govuk-button">
+          Download XLSX
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import Form from '@/components/Form/Form';
+import ErrorSummary from '@/components/Form/ErrorSummary';
+import BackLink from '@/components/BackLink';
+
+export default {
+  components: {
+    ErrorSummary,
+    BackLink,
+  },
+  extends: Form,
+  computed: {
+    selectedItems() {
+      return this.$store.state.exerciseCollection.selectedItems;
+    },
+  },
+  created() {
+    if (this.selectedItems.length === 0) {
+      this.$router.push({ name: 'dashboard' });
+    }
+  },
+  methods: {
+    async save() {
+      console.log('download data', this.selectedItems);
+    },
+  },
+};
+</script>
