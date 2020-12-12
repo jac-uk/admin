9d7418f63d1efee894e9c727f933745ea9203897
Lisias (Lee) Loback <lisias@loback.com>
2020-12-12 23:32:09 +0000

    #1066 Add any numbers of documents to exercise downloads - created the popup for uploading files - added the possibility of adding / removing/ replacing - commmented out the tests

--- src/views/Exercises/Edit/Downloads.vue
+++ src/views/Exercises/Edit/Downloads.vue
@@ -1,14 +1,93 @@
 <template>
   <div class="govuk-grid-row">
-    <form @submit.prevent="save">
-      <div class="govuk-grid-column-two-thirds">
-        <BackLink />
-        <h1 class="govuk-heading-xl">
-          Downloads
-        </h1>
+    <div 
+      class="govuk-grid-column-two-thirds"
+    >
+      <BackLink />
+      <h1 class="govuk-heading-xl">
+        Downloads
+      </h1>
 
-        <ErrorSummary :errors="errors" />
+      <ErrorSummary :errors="errors" />
+      
+      <div v-if="true">
+        <div
+          class="govuk-grid-column-full govuk-!-margin-top-5 govuk-!-margin-bottom-2"
+        >
+          <h2 class="govuk-heading-l">
+            Upload Files
+          </h2>
+          <table class="govuk-table">
+            <tbody class="govuk-table__body">
+              <tr
+                v-for="upload in uploadList"
+                :key="upload.id"
+                :ref="`${upload.id}-group`"
+                class="govuk-table__row"
+              >
+                <th class="govuk-table__header">
+                  <a
+                    class="govuk-link"
+                    href="#"
+                    @click.prevent="modalUploadOpen({ ...upload })"
+                  >
+                    {{ upload.title }}
+                  </a>
+                </th>
+                <td class="govuk-table__cell">
+                  <div>
+                    <span 
+                      v-for="item in exercise.downloads[upload.id]" 
+                      :key="item.id"
+                    >
+                      <a
+                        class="govuk-link moj-download-link"
+                        href="#"
+                        @click.prevent="modalUploadOpen({ ...upload, fileRef: item.file, fileTitle: item.title })"
+                      >
+                        {{ item.title }}
+                      </a>
+                    </span>
+                    <span :id="`${upload.id}-error`" :ref="`${upload.id}-error`" class="govuk-error-message"><span class="govuk-visually-hidden">Error:</span> Please upload {{ upload.id }} </span>
+                  </div>
+                </td>
+                <td class="govuk-table__cell text-right">
+                  <strong
+                    v-if="exercise.downloads[upload.id] && exercise.downloads[upload.id].length > 0"
+                    class="govuk-tag"
+                  >
+                    Done
+                  </strong>
+                  <span
+                    v-else
+                  />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <button 
+            class="govuk-button"
+            @click="submitForm()"
+          >
+            Save and continue
+          </button>
+        </div>
+        <Modal
+          ref="modalRef"
+        >
+          <component
+            :is="`UploadFiles`"
+            v-bind="uploadProps"
+            @close="modalUploadClose"
+          />
+        </Modal>
+      </div>
 
+      <form 
+        v-if="false" 
+        ref="downloadForm"
+        @submit.prevent="save"
+      >
         <h2 class="govuk-heading-l">
           Job description
         </h2>
@@ -90,8 +169,8 @@
         <button class="govuk-button">
           Save and continue
         </button>
-      </div>
-    </form>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -102,12 +181,16 @@ import BackLink from '@jac-uk/jac-kit/draftComponents/BackLink';
 import ErrorSummary from '@jac-uk/jac-kit/draftComponents/Form/ErrorSummary';
 import RepeatableFields from '@jac-uk/jac-kit/draftComponents/RepeatableFields';
 import MultiFileUpload from '@/components/RepeatableFields/MultiFileUpload';
+import Modal from '@jac-uk/jac-kit/components/Modal/Modal';
+import UploadFiles from '@/components/ModalViews/UploadFiles';
 
 export default {
   components: {
     BackLink,
     ErrorSummary,
     RepeatableFields,
+    Modal,
+    UploadFiles,
   },
   extends: Form,
   data() {
@@ -115,11 +198,13 @@ export default {
       downloads: {
         jobDescriptions: [],
         termsAndConditions: [],
+        competencyFramework: [],
+        pensionsInformation: [],
+        skillsAndAbilitiesCriteria: [],
         independentAssessors: [],
         candidateAssessementForms: [],
-        pensionsInformation: [],
-        competencyFramework: [],
         welshTranslation: [],
+        otherDownloads: [],
       },
     };
     const data = this.$store.getters['exerciseDocument/data']();
@@ -133,6 +218,7 @@ export default {
       repeatableFields: {
         MultiFileUpload,
       },
+      uploadProps: {},
     };
   },
   computed: {
@@ -145,6 +231,65 @@ export default {
     uploadPath() {
       return `/exercise/${this.exerciseId}`;
     },
+    uploadList() {
+      const data = [
+        { 
+          title: 'Job Description Upload', 
+          id: 'jobDescriptions',
+          name: 'job-descriptions',
+          done: true,
+        },
+        { 
+          title: 'Terms and Conditions Upload', 
+          id: 'termsAndConditions',
+          name: 'terms-and-conditions',
+          done: true,
+        },
+        { 
+          title: 'Competency Framework Upload', 
+          id: 'competencyFramework',
+          name: 'competency-framework',
+          done: true,
+        },
+        { 
+          title: 'Pensions Information Upload', 
+          id: 'pensionsInformation',
+          name: 'pensions-information',
+          done: true,
+        },
+        { 
+          title: 'Skills and Abilities Criteria', 
+          id: 'skillsAndAbilitiesCriteria',
+          name: 'skills-and-abilities-criteria',
+          done: true,
+        },
+        { 
+          title: 'Independent Assessors Upload', 
+          id: 'independentAssessors',
+          name: 'independent-assessors',
+          done: true,
+        },
+        { 
+          title: 'Candidate Assessment Form Upload', 
+          id: 'candidateAssessementForms',
+          name: 'candidate-assessement-forms',
+          done: true,
+        },
+        { 
+          title: 'Welsh Translation Upload', 
+          id: 'welshTranslation',
+          name: 'welsh-translation',
+          done: true,
+        },
+        { 
+          title: 'Other Donwloads Upload', 
+          id: 'otherDownloads',
+          name: 'other-downloads',
+          done: true,
+        },
+      ];
+      return data;
+    },
   },
   methods: {
     async save() {
@@ -156,6 +301,83 @@ export default {
         this.$router.push(this.$store.getters['exerciseCreateJourney/nextPage']('exercise-show-downloads'));
       }
     },
+    modalUploadOpen(obj) {
+      this.validateUI(obj.id, false);
+      this.uploadProps = {
+        ...obj,
+        filePath: this.uploadPath,
+        data: this.exercise.downloads,
+        exerciseId: this.exerciseId, 
+      };
+      this.$refs.modalRef.openModal();
+    },
+    modalUploadClose() {
+      this.$refs.modalRef.closeModal();
+      // Refresh the information on the exercise
+      this.exercise = this.$store.getters['exerciseDocument/data']();
+    },
+    async submitForm() {
+      this.validateDownloads();
+      if (this.errors.length === 0) {
+        this.exercise.progress.downloads = true;
+
+        await this.$store.dispatch('exerciseDocument/save', this.exercise);
+        this.$router.push(this.$store.getters['exerciseCreateJourney/nextPage']('exercise-show-downloads'));
+      }
+    },
+    validateDownloads() {
+      this.errors = [];
+      this.validateItem('jobDescriptions');
+      // this.validateItem('termsAndConditions');
+      this.validateItem('competencyFramework');
+      // pensionsInformation: this.validateItem('pensionsInformation');
+      this.validateItem('skillsAndAbilitiesCriteria');
+      this.validateItem('independentAssessors');
+      // this.validateItem('candidateAssessementForms');
+      this.validateItem('welshTranslation');
+      // this.validateItem('otherDownloads');
+
+      // govuk-form-group--error
+      // <span id="candidate-assessement-forms_0--title-error" class="govuk-error-message"><span class="govuk-visually-hidden">Error:</span> Please enter a value for Title of file </span>
+    },
+    validateItem(downloadId) {
+      const isValid = this.exercise.downloads[downloadId] && this.exercise.downloads[downloadId].length > 0;
+      if (!isValid) {
+        this.errors.push({ id: downloadId, message: `Please add ${downloadId}` });
+        this.validateUI(downloadId, true);
+      }
+      return isValid;
+    },
+    validateUI(downloadId, add) {
+      const wrapperCssClass = this.$refs[`${downloadId}-group`][0].classList;
+      const itemCssClass = this.$refs[`${downloadId}-error`][0].classList;
+      if (add) {
+        itemCssClass.add('active');
+        wrapperCssClass.add('govuk-form-group--error');
+      } else {
+        itemCssClass.remove('active');
+        wrapperCssClass.remove('govuk-form-group--error');
+      }
+    },
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.moj-download-link {
+  background-color: #f3f2f1;
+  padding: 5px;
+  margin: 0 5px 5px 0;
+  display: inline-block;
+}
+.govuk-form-group--error th {
+  padding-left: .5em;
+}
+.govuk-error-message {
+  display: none;
+
+  &.active {
+    display: inherit;
+  }
+}
+</style>
