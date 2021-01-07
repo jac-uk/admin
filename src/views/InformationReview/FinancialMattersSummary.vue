5ea8750edfcf917c64c22e4a14893dd08456602f
Maria Brookes <maria_brookes@yahoo.co.uk>
2021-01-07 16:37:16 +0000

    #1120 Updated Character Info section in Applications

---
+++ src/views/InformationReview/FinancialMattersSummary.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <dl class="govuk-summary-list">
+      <div class="govuk-summary-list__row">
+        <dt class="govuk-summary-list__key widerColumn">
+          Has been declared bankrupt
+        </dt>
+        <dd class="govuk-summary-list__value">
+          <InformationReviewRenderer
+            :data="application.characterInformation.bankruptcyDetails"
+          />
+        </dd>
+      </div>
+    </dl>
+
+    <dl class="govuk-summary-list">
+      <div class="govuk-summary-list__row">
+        <dt class="govuk-summary-list__key widerColumn">
+          Has entered into an Individual Voluntary Agreement (IVA)
+        </dt>
+        <dd class="govuk-summary-list__value">
+          <InformationReviewRenderer
+            :data="application.characterInformation.ivaDetails"
+          />
+        </dd>
+      </div>
+    </dl>
+
+    <dl class="govuk-summary-list">
+      <div class="govuk-summary-list__row">
+        <dt class="govuk-summary-list__key widerColumn">
+          Has filed late tax returns
+        </dt>
+        <dd class="govuk-summary-list__value">
+          <InformationReviewRenderer
+            :data="application.characterInformation.lateTaxReturnDetails"
+          />
+        </dd>
+      </div>
+    </dl>
+
+    <dl class="govuk-summary-list">
+      <div class="govuk-summary-list__row">
+        <dt class="govuk-summary-list__key widerColumn">
+          Has filed late VAT returns
+        </dt>
+        <dd class="govuk-summary-list__value">
+          <InformationReviewRenderer
+            :data="application.characterInformation.lateVatReturnDetails"
+          />
+        </dd>
+      </div>
+    </dl>
+
+    <dl class="govuk-summary-list">
+      <div class="govuk-summary-list__row">
+        <dt class="govuk-summary-list__key widerColumn">
+          Has ever been fined by HMRC
+        </dt>
+        <dd class="govuk-summary-list__value">
+          <InformationReviewRenderer
+            :data="application.characterInformation.hmrcFineDetails"
+          />
+        </dd>
+      </div>
+    </dl>
+  </div>
+</template>
+
+<script>
+import InformationReviewRenderer from '@/components/Page/InformationReviewRenderer';
+
+export default {
+  name: 'FinancialMattersSummary',
+  components: {
+    InformationReviewRenderer,
+  },
+  props: {
+    application: {
+      type: Object,
+      required: true,
+      default: new Object(),
+    },
+  },
+};
+</script>
+
+<style scoped>
+  .widerColumn {
+    width: 70%;
+  }
+</style>
---
+++ src/views/InformationReview/MotoringOffencesSummary.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <dl class="govuk-summary-list">
+      <div class="govuk-summary-list__row">
+        <dt class="govuk-summary-list__key widerColumn">
+          Has been disqualified from driving
+        </dt>
+        <dd class="govuk-summary-list__value">
+          <InformationReviewRenderer
+            :data="application.characterInformation.drivingDisqualificationDetails"
+          />
+        </dd>
+      </div>
+    </dl>
+
+    <dl class="govuk-summary-list">
+      <div class="govuk-summary-list__row">
+        <dt class="govuk-summary-list__key widerColumn">
+          Has been convicted for driving under the influence of drink or drugs
+        </dt>
+        <dd class="govuk-summary-list__value">
+          <InformationReviewRenderer
+            :data="application.characterInformation.drivingUnderInfluenceConvictionDetails"
+          />
+        </dd>
+      </div>
+    </dl>
+
+    <dl class="govuk-summary-list">
+      <div class="govuk-summary-list__row">
+        <dt class="govuk-summary-list__key widerColumn">
+          Has endorsements on driving licence
+        </dt>
+        <dd class="govuk-summary-list__value">
+          <InformationReviewRenderer
+            :data="application.characterInformation.drivingLicenceEndorsementDetails"
+          />
+        </dd>
+      </div>
+    </dl>
+
+    <dl class="govuk-summary-list">
+      <div class="govuk-summary-list__row">
+        <dt class="govuk-summary-list__key widerColumn">
+          Was convicted of any motoring offences in the past 4 years
+        </dt>
+        <dd class="govuk-summary-list__value">
+          <InformationReviewRenderer
+            :data="application.characterInformation.recentDrivingConvictionDetails"
+          />
+        </dd>
+      </div>
+    </dl>
+  </div>
+</template>
+
+<script>
+import InformationReviewRenderer from '@/components/Page/InformationReviewRenderer';
+
+export default {
+  name: 'MotoringOffencesSummary',
+  components: {
+    InformationReviewRenderer,
+  },
+  props: {
+    application: {
+      type: Object,
+      required: true,
+      default: new Object(),
+    },
+  },
+};
+</script>
+
+<style scoped>
+  .widerColumn {
+    width: 70%;
+  }
+</style>
+
