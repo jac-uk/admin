b8515c32315d63fc63ed8f4a8046efc679e0a90e
Lisias (Lee) Loback <lisias@loback.com>
2021-01-19 09:15:21 +0000

    #1130 Create the navigation for Panel Packs - Sift and Selection Days

---
+++ src/views/Exercises/Tasks/SelectionDays.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1 class="govuk-heading-l">
+      Selection Days
+    </h1>
+    <p>Selection Days Under Construction</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      sift: null,
+    };
+  },
+  computed: {
+
+  },
+  methods: {
+    
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
