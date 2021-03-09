bec8c0e9930dc606f6a6a0665a4af27575180b6c
Tom Russell <TR115251@hotmail.co.uk>
2021-03-09 18:04:07 +0000

    slim equalityAndDiversity check

--- src/views/Exercises/Applications/Application.vue
+++ src/views/Exercises/Applications/Application.vue
@@ -328,7 +328,7 @@
               </h2>
 
               <dl
-                v-if="application.equalityAndDiversitySurvey && Object.keys(application.equalityAndDiversitySurvey).length"
+                v-if="Object.keys(application.equalityAndDiversitySurvey).length"
                 class="govuk-summary-list"
               >
                 <div class="govuk-summary-list__row">
