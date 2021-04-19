6367443b26a8aacf6282dbf571b1286ccbe689df
HalyconJAC <halcyon@judicialappointments.digital>
2021-04-19 20:34:07 +0100

    renamed test files to .spec.js

--- cypress/integration/auth/routes/applications.spec.js
+++ cypress/integration/auth/routes/applications.spec.js
@@ -1,10 +1,10 @@
 /* eslint-disable cypress/no-unnecessary-waiting */
 /// <reference types="cypress" />
 
-// TODO - Get a random exercise id (that has applications)
-const exerciseId = '3CI2dLS5qaa0iaCKxFBD';
-// TODO - Get a random application id for the above exercise
+// TODO - Get a random application
 const applicationId = 'bqtwu9opOL1yXwSnhesc';
+// TODO - Read the exercise from the application
+const exerciseId = '3CI2dLS5qaa0iaCKxFBD';
 
 const routes = [
   `/exercises/${exerciseId}/applications/draft`,
