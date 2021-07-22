e455474798d7ba03e7cd64269c7864773cc515ed
Tom Russell <TR115251@hotmail.co.uk>
2021-07-22 11:37:54 +0100

    modal styling

--- src/views/Exercise/Tasks/QualifyingTests/QualifyingTest/Response/View.vue
+++ src/views/Exercise/Tasks/QualifyingTests/QualifyingTest/Response/View.vue
@@ -181,7 +181,7 @@
           </div>
           <div class="modal__content govuk-!-padding-4">
             <p class="modal__message govuk-body-l">
-              Reseting this candidates test will overwrite their
+              Reseting this candidate's test will overwrite their
               'started', 'completed' and 'time taken' fields.
               <br>
               Please ensure there is a record of these before continuing.
@@ -653,50 +653,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  @mixin mobile-view {
-    @media (max-width: 599px) { @content; }
-  }
-
-  @include mobile-view { 
-    .modal {
-      min-width: 100%;
-      min-height: 100%;
-    }
-  }
-
-  .modal {
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    width: 30%;
-    transform: translate(-50%, -50%);
-    border: solid 2px #b1b4b6;
-    background-color: #ffffff;
-  }
-  .modal__title {
-    text-align: center;
-    vertical-align: middle;
-    border: solid 2px #1d70b8;
-    background-color: #1d70b8;
-    color: white;
-  }
-  .modal__message {
-    vertical-align: middle;
-  }
   .deny {
     background-color: #f3f2f1;
-  }
-  .modal-mask {
-    position: fixed;
-    z-index: 9998;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.5);
-    display: table;
-    transition: opacity 0.3s ease;
-    overflow: hidden;
   }
   .answer--right {
     font-weight: bold;
