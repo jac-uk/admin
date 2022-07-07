693b808f5c022788996ce370e25f8ba6bbcd0aa9
Tom Russell <TR115251@hotmail.co.uk>
2022-07-07 16:54:31 +0100

    remove comments blocking code

--- src/views/Exercise/Applications/Application.vue
+++ src/views/Exercise/Applications/Application.vue
@@ -297,14 +297,14 @@ import { saveAs } from 'file-saver';
 import Modal from '@jac-uk/jac-kit/components/Modal/Modal';
 import SubmissionExtension from '@/components/ModalViews/SubmissionExtension';
 import Notes from '@/components/Notes/Notes';
-// import PersonalDetailsSummary from '@/views/InformationReview/PersonalDetailsSummary';
-// import CharacterInformationSummary from '@/views/InformationReview/CharacterInformationSummary';
-// import EqualityAndDiversityInformationSummary from '@/views/InformationReview/EqualityAndDiversityInformationSummary';
-// import PreferencesSummary from '@/views/InformationReview/PreferencesSummary';
+import PersonalDetailsSummary from '@/views/InformationReview/PersonalDetailsSummary';
+import CharacterInformationSummary from '@/views/InformationReview/CharacterInformationSummary';
+import EqualityAndDiversityInformationSummary from '@/views/InformationReview/EqualityAndDiversityInformationSummary';
+import PreferencesSummary from '@/views/InformationReview/PreferencesSummary';
 import QualificationsAndMembershipsSummary from '@/views/InformationReview/QualificationsAndMembershipsSummary';
-// import ExperienceSummary from '@/views/InformationReview/ExperienceSummary';
-// import AssessmentsSummary from '@/views/InformationReview/AssessmentsSummary';
-// import AssessorsSummary from '@/views/InformationReview/AssessorsSummary';
+import ExperienceSummary from '@/views/InformationReview/ExperienceSummary';
+import AssessmentsSummary from '@/views/InformationReview/AssessmentsSummary';
+import AssessorsSummary from '@/views/InformationReview/AssessorsSummary';
 import InformationReviewRenderer from '@/components/Page/InformationReviewRenderer';
 import PageNotFound from '@/views/Errors/PageNotFound';
 import splitFullName from '@jac-uk/jac-kit/helpers/splitFullName';
@@ -329,14 +329,14 @@ export default {
     PageNotFound,
     InformationReviewRenderer,
     CharacterChecks,
-    // PersonalDetailsSummary,
-    // CharacterInformationSummary,
-    // EqualityAndDiversityInformationSummary,
-    // PreferencesSummary,
+    PersonalDetailsSummary,
+    CharacterInformationSummary,
+    EqualityAndDiversityInformationSummary,
+    PreferencesSummary,
     QualificationsAndMembershipsSummary,
-    // ExperienceSummary,
-    // AssessmentsSummary,
-    // AssessorsSummary,
+    ExperienceSummary,
+    AssessmentsSummary,
+    AssessorsSummary,
   },
   data() {
     return {
