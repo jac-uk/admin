ee429efd61d105294c80b652ba8d913cc8b9efde
warrensearle <warren@precise-minds.co.uk>
2022-10-18 14:12:38 +0100

    Dependency updates 13 Oct 2022 (#1753)
    * Bump glob-parent, webpack and @vue/cli-service (#1744)
    
    Bumps [glob-parent](https://github.com/gulpjs/glob-parent) to 5.1.2 and updates ancestor dependencies [glob-parent](https://github.com/gulpjs/glob-parent), [webpack](https://github.com/webpack/webpack) and [@vue/cli-service](https://github.com/vuejs/vue-cli/tree/HEAD/packages/@vue/cli-service). These dependencies need to be updated together.
    
    
    Updates `glob-parent` from 3.1.0 to 5.1.2
    - [Release notes](https://github.com/gulpjs/glob-parent/releases)
    - [Changelog](https://github.com/gulpjs/glob-parent/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/gulpjs/glob-parent/compare/v3.1.0...v5.1.2)
    
    Updates `webpack` from 4.46.0 to 5.74.0
    - [Release notes](https://github.com/webpack/webpack/releases)
    - [Commits](https://github.com/webpack/webpack/compare/v4.46.0...v5.74.0)
    
    Updates `@vue/cli-service` from 4.5.19 to 5.0.8
    - [Release notes](https://github.com/vuejs/vue-cli/releases)
    - [Changelog](https://github.com/vuejs/vue-cli/blob/dev/CHANGELOG.md)
    - [Commits](https://github.com/vuejs/vue-cli/commits/v5.0.8/packages/@vue/cli-service)
    
    ---
    updated-dependencies:
    - dependency-name: glob-parent
      dependency-type: indirect
    - dependency-name: webpack
      dependency-type: direct:development
    - dependency-name: "@vue/cli-service"
      dependency-type: direct:development
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Upgrade cli-plugin-unit-jest and eslint-config
    
    * Update
    
    * Add node plugins for webpack 5
    
    * Update package-lock
    
    * Update package-lock again
    
    * Fix constants file
    
    * Get tests running, and exiting
    
    * Package script names
    
    * Update `npm run test` to run unit tests
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- tests/unit/views/Exercises/Edit/EditName.spec.js
+++ tests/unit/views/Exercises/Edit/EditName.spec.js
@@ -42,7 +42,7 @@ xdescribe('@/views/Exercise/Edit/EditName', () => {
     });
 
     it('contains a <h1>', () => {
-      expect(wrapper.contains('h1')).toBe(true);
+      expect(wrapper.find('h1')).toBe(true);
     });
 
     it('contains a <form>', () => {
--- tests/unit/views/Exercises/Edit/Shortlisting.spec.js
+++ tests/unit/views/Exercises/Edit/Shortlisting.spec.js
@@ -40,7 +40,7 @@ xdescribe('views/Exercise/Edit/Shortlisting', () => {
     });
 
     it('contains a <h1>', () => {
-      expect(wrapper.contains('h1')).toBe(true);
+      expect(wrapper.find('h1')).toBe(true);
     });
 
     it('contains a <form>', () => {
