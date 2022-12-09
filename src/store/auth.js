857a1fe77a2e8dac5d723e629fae132b770ed696
halcyonjac <halcyon@judicialappointments.digital>
2022-12-09 11:21:30 +0000

    check property before read

--- src/store/auth.js
+++ src/store/auth.js
@@ -79,7 +79,7 @@ const module = {
       return state.currentUser.email;
     },
     hasPermissions: state => permissions => {
-      const rolePermissions = state.currentUser.rolePermissions;
+      const rolePermissions = state.currentUser ? state.currentUser.rolePermissions : null;
       return rolePermissions && Array.isArray(rolePermissions) && permissions.every(p => rolePermissions.includes(p));
     },
   },
