a269b29268b35a741f84401551e5ccaecfe3edc9
drieJac <jac.drie@digitalteam.uk>
2023-02-10 10:46:24 +0000

    Moved css for sticky headers in merti list table into jac-kit and added stickiness to the Activated component table. Allow column click in MeritList table to expand the data columns. Removed topmost header. Altered header column names.

--- src/views/Exercise/Reports/MeritList.vue
+++ src/views/Exercise/Reports/MeritList.vue
@@ -47,37 +47,10 @@
           :search="['fullName', 'referenceNumber']"
           search-placeholder="Candidate name or application ref"
           local-data
-          class="merit-list"
+          sticky
           @change="updateTableState"
+          @clickCol=clickColumn
         >
-          <template #header>
-            <tr class="govuk-table__row">
-              <th
-                scope="col"
-                class="govuk-table__header table-cell-application"
-              />
-              <template v-for="column in headerColumns">
-                <template v-if="column.expandable">
-                  <th
-                    :key="column.ref"
-                    scope="col"
-                    :colspan="column.colspan > 1 ? column.colspan : false"
-                    class="govuk-table__header text-center expandable"
-                    @click="toggleColumn(column.ref)"
-                  >
-                    <span class="elipses">{{ column.ref | lookup }}</span>
-                  </th>
-                </template>
-                <template v-else>
-                  <th
-                    :key="column.ref"
-                    scope="col"
-                    class="govuk-table__header"
-                  />
-                </template>
-              </template>
-            </tr>
-          </template>
           <template #row="{row}">
             <TableCell class="table-cell-application">
               <RouterLink
@@ -104,22 +77,6 @@
             <TableCell class="text-center">
               {{ row.totalScore | formatNumber(2) }}
             </TableCell>
-
-            <!-- @TODO: Integrate this in below
-            <TableCell :title="tableColumns[3].title">
-              {{ 100 * (row.cumulativeDiversity.female / (row.rank + row.count - 1)) | formatNumber(2) }}%
-            </TableCell>
-            <TableCell :title="tableColumns[4].title">
-              {{ 100 * (row.cumulativeDiversity.bame / (row.rank + row.count - 1)) | formatNumber(2) }}%
-            </TableCell>
-            <TableCell :title="tableColumns[5].title">
-              {{ 100 * (row.cumulativeDiversity.solicitor / (row.rank + row.count - 1)) | formatNumber(2) }}%
-            </TableCell>
-            <TableCell :title="tableColumns[6].title">
-              {{ 100 * (row.cumulativeDiversity.disability / (row.rank + row.count - 1)) | formatNumber(2) }}%
-            </TableCell>
-            -->
-
           </template>
         </Table>
       </div>
@@ -136,6 +93,7 @@ import FullScreenButton from '@/components/Page/FullScreenButton';
 import { TASK_TYPE, TASK_STATUS, getTaskTypes } from '@/helpers/exerciseHelper';
 import { lookup } from '@/filters';
 import { getTableData } from '@/helpers/tableHelper';
+import _startCase from 'lodash/startCase';
 
 const localLookup = {};
 localLookup[TASK_TYPE.CRITICAL_ANALYSIS] = 'CA';
@@ -246,40 +204,23 @@ export default {
     hasScoreSheet() {
       return this.completedTasks.length > 0;
     },
-    headerColumns() {
-      const columns = [];
-      this.completedTasks.forEach(task => {
-        if (task.markingScheme) {
-          task.markingScheme.forEach(item => {
-            if (item.type === 'group') {
-              columns.push({
-                ref: item.ref,
-                expandable: true,
-                colspan: this.isOpen(item.ref) ? item.children.length + 1 : 1,
-              });
-            } else {
-              columns.push({
-                ref: item.ref,
-              });
-            }
-          });
-        } else {
-          columns.push({
-            ref: task.id,
-          });
-        }
-      });
-      return columns;
-    },
     tableColumns() {
       const columns = [];
       if (!this.hasScoreSheet) return columns;
-      //columns.push({ title: 'Candidate', class: 'table-cell-application' });
       columns.push({ title: 'Candidate', class: 'table-cell-application', sort: 'fullName' });
       this.scoreSheetColumns.forEach(col => {
-        columns.push({ title: this.getColumnTitle(col), class: 'table-cell text-center' });
+        const column = { title: this.getColumnTitle(col), class: 'table-cell text-center' };
+        if (column.title.toLowerCase() === 'score') {
+          const prefix = localLookup[col.type] ? localLookup[col.type] : col.type;
+          column.title = _startCase(`${prefix} ${column.title}`);
+          // Specify a click event for the column header
+          column.eventParams = [col.path];
+          column.emitEvent = 'clickCol';
+        }
+        columns.push(column);
       });
-      columns.push({ title: 'Total score', class: 'text-center' });
+      // Default sort by total score (initially)
+      columns.push({ title: 'Total score', sort: 'totalScore', direction: 'desc', default: true, class: 'text-center' });
       return columns;
     },
     tableData() {
@@ -351,7 +292,6 @@ export default {
       return columns;
     },
     clipboardColumns() {
-      // @todo: Use the scoresheetColumnsNew here once you've got the data in there looking right
       const columns = [];
       columns.push({ title: 'ID', ref: 'id', editable: false });
       columns.push({ title: 'Ref', ref: 'ref', editable: false });
@@ -368,8 +308,9 @@ export default {
     await this.$store.dispatch('tasks/bind', { exerciseId: this.exercise.id } );
   },
   methods: {
-
-    // @TODO: WARREN CONTINUE HERE - copy to clipboard needs reworking for the data structure in here
+    clickColumn(columnRef) {
+      this.toggleColumn(columnRef);
+    },
     async copyToClipboard() {
       const rows = [];
       const headers = this.clipboardColumns.map(column => column.title);
@@ -426,81 +367,38 @@ export default {
       if (localLookup[title]) return localLookup[title];
       return title;
     },
-
-    // @TODO: Copy and paste should use the finalScores.scoresheet
-    // - not all tasks will have a scoresheet but will have id, ref, score, pass
-    // - eg critical analysis test (we do want to be able to adjust this score)
-    // - it will only be there temporarily
-
     getColValue(row, col) {
-
-      // console.log('--getColValue, row:');
-      // console.log(row);
-      // console.log('--getColValue, col:');
-      // console.log(col);
-
       const root = row[col.type];
       if (!root) return '';
       let scoreSheet = root.scoreSheet;
-
-      // console.log('root.scoreSheet:');
-      // console.log(root.scoreSheet);
-
       if (scoreSheet) {
         if (col.path) {
           scoreSheet = scoreSheet[col.path];
-
-          // console.log('scoreSheet:');
-          // console.log(scoreSheet);
         }
         if (col.value) {
           let value = scoreSheet;
           if (col.value.indexOf('.') >= 0) {
             const valueParts = col.value.split('.');
-
-            //console.log('valueParts:', valueParts);
-
             valueParts.forEach(valuePart => {
               value = value[valuePart];
             });
-
-            //console.log('1 - value:', value);
-
           } else {
             value = value[col.value];
-
-            //console.log('2 - value:', value);
-
           }
           if (col.showScore) {
-
-            //console.log(`return ${value} (${scoreSheet.score})`);
-
             return `${value} (${scoreSheet.score})`;
           } else {
-
-            //console.log(`return value: ${value}`);
-
             return value;
           }
         } else {
           if (col.showScore) {
-
-            //console.log(`return scoreSheet.score: ${scoreSheet.score}`);
-
             return scoreSheet.score;
           }
         }
       }
       if (col.showScore) {
-
-        //console.log(`return root.score: ${root.score}`);
-
         return root.score;
       }
-
-      //console.log('return empty string');
-
       return '';
     },
     toggleColumn(ref) {
@@ -514,92 +412,6 @@ export default {
     isOpen(ref) {
       return this.openColumns.indexOf(ref) >= 0;
     },
-    exportData() {
-      console.log('export data');
-    },
   },
 };
 </script>
-
-<style lang="scss">
-.merit-list {
-  table {
-    display: block;
-    overflow-x: auto;
-    overflow-y: auto;
-    scroll-behavior: smooth;
-    max-width: 100%;
-    max-height: 80vh;
-    margin: 0 !important;
-    border-spacing: 0;
-    table-layout: fixed;
-    border-collapse: collapse;
-    border-right: 1px solid #f3f2f1;
-    border-left: 1px solid #f3f2f1;
-  }
-  tbody {
-    white-space: nowrap;
-  }
-  //table,
-  th,
-  td {
-      border: 1px solid #f3f2f1;
-      vertical-align: middle;
-      white-space: nowrap;
-      &.v-top {
-        vertical-align: top;
-      }
-  }
-  tr > th:first-child,
-  tr > td:first-child {
-    position: sticky;
-    left: 0;
-    background-color: #f3f2f1;
-    z-index:1;
-  }
-  tr:first-child > th {
-    position: sticky;
-    top: 0;
-    background-color: white;
-    z-index: 2;
-    border: 0;
-    padding-left: 20px;
-    &.expandable {
-      text-decoration: underline;
-      cursor: pointer;
-    }
-  }
-  tr:nth-child(2) > th {
-    position: sticky;
-    top: 46px;
-    background-color: #f3f2f1;
-    z-index: 2;
-  }
-  tr:first-child > th:first-child,
-  tr:nth-child(2) > th:first-child {
-    z-index: 3;
-  }
-  tr > th:last-child,
-  tr > td:last-child {
-    padding-right: 10px;
-  }
-  .table-cell {
-    padding: 0 10px;
-  }
-  .table-cell-application {
-    min-width: 160px;
-    padding: 10px;
-  }
-  .table-cell-score {
-    min-width: 50px;
-    padding: 0 10px;
-    text-align: center;
-  }
-  .elipses {
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;  // IE 6+, FF 7+, Op 11+, Saf 1.3+, Chr 1+
-    -o-text-overflow: ellipsis;  // for Opera 9 & 10
-  }
-}
-</style>
