374c4bb4a060ee89056e18d5ab60dd6097373fe1
warrensearle <warren@precise-minds.co.uk>
2023-02-28 12:40:51 +0000

    #1911 Enable QTs to be set up while exercise is still open (#1920)
    * #1911 Enable QTs to be set up whilst exercise is still open
    
    * Tidy up wording and font size

--- src/views/Exercise/Tasks/Task/New/default.vue
+++ src/views/Exercise/Tasks/Task/New/default.vue
@@ -5,45 +5,32 @@
     </h1>
 
     <p
-      class="govuk-body-l"
+      class="govuk-body"
     >
-      Please check the following details before continuing
+      Please confirm the following
     </p>
 
-    <div class="govuk-grid-row">
-      <div class="govuk-grid-column-one-half">
-        <div class="panel govuk-!-margin-bottom-5 govuk-!-padding-4 background-light-grey">
-          <div class="govuk-caption-m">
-            Applications <span v-if="entryStatus">({{ entryStatus | lookup }})</span>
-          </div>
-          <h2
-            class="govuk-heading-m govuk-!-margin-bottom-0"
-          >
-            {{ totalApplications }}
-          </h2>
-        </div>
-      </div>
+    <Checkbox
+      v-for="(timelineTask, index) in timelineTasks"
+      :id="`timelineTask-${index}`"
+      :key="`timelineTask-${index}`"
+      v-model="formData.timelineTasks[index]"
+    >
+      {{ timelineTask.entry }} will happen on {{ timelineTask.dateString }}
+    </Checkbox>
 
-      <div
-        v-for="(timelineTask, index) in timelineTasks"
-        :key="`timelineTask-${index}`"
-        class="govuk-grid-column-one-half"
-      >
-        <div class="panel govuk-!-margin-bottom-5 govuk-!-padding-4 background-light-grey">
-          <span class="govuk-caption-m">{{ timelineTask.entry }}</span>
-          <h2
-            class="govuk-heading-m govuk-!-margin-bottom-0"
-          >
-            {{ timelineTask.dateString }}
-          </h2>
-        </div>
-      </div>
-    </div>
+    <Checkbox
+      v-if="entryStatus"
+      id="entryStatus"
+      v-model="formData.entryStatus"
+    >
+      Only '{{ entryStatus | lookup }}' applications will be included
+    </Checkbox>
 
     <ActionButton
       class="govuk-!-margin-bottom-0"
       type="primary"
-      :disabled="!totalApplications"
+      :disabled="!isFormCompleted"
       @click="btnInitialise"
     >
       Continue
@@ -56,17 +43,27 @@ import { btnNext } from '../helper';
 import { TASK_TYPE } from '@/helpers/constants';
 import { taskEntryStatus, previousTaskType, getTimelineTasks } from '@/helpers/exerciseHelper';
 import ActionButton from '@jac-uk/jac-kit/draftComponents/ActionButton';
+import Checkbox from '@jac-uk/jac-kit/draftComponents/Form/Checkbox';
 import { functions } from '@/firebase';
 
 export default {
   components: {
     ActionButton,
+    Checkbox,
   },
   props: {
     type: {
       required: true,
       type: String,
     },
+  },
+  data() {
+    return {
+      formData: {
+        entryStatus: null,
+        timelineTasks: [],
+      },
+    };
   },
   computed: {
     exercise() {
@@ -96,6 +93,17 @@ export default {
         return this.exercise._applicationRecords.review || 0;
       }
       return 0;
+    },
+    isFormCompleted() {
+      let expectedLength = 0;
+      let actualLength = 0;
+      if (this.entryStatus) {
+        expectedLength += 1;
+        if (this.formData.entryStatus === true) actualLength += 1;
+      }
+      expectedLength += this.timelineTasks.length;
+      actualLength += this.formData.timelineTasks.length;
+      return expectedLength === actualLength;
     },
   },
   methods: {
