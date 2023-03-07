1735bb53819ebb0112369509c9b95e132082822a
Tom Russell <TR115251@hotmail.co.uk>
2023-03-07 17:08:55 +0000

    move applications to admin

---
+++ src/views/Candidates/Applications.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <Table
+      v-if="!noApplications"
+      data-key="id"
+      :data="candidateApplications"
+      :columns="tableColumns"
+      @change="getTableData"
+    >
+      <template #row="{row}">
+        <TableCell>
+          <strong>
+            {{ row.exerciseRef }}
+          </strong>
+          - {{ row.exerciseName }}
+        </TableCell>
+        <TableCell>
+          {{ getStatus(row) | lookup }}{{ getStatus(row) && getStage(row) ? ' / ' : '' }}
+          <strong>
+            {{ getStage(row) }}
+          </strong>
+        </TableCell>
+        <TableCell>
+          <RouterLink
+            :to="{ name: 'exercise-application', params: { id: row.exerciseId, applicationId: row.id } }"
+            target="_blank"
+          >
+            View Application
+          </RouterLink>
+        </TableCell>
+      </template>
+    </Table>
+    <p v-if="noApplications">
+      There are no applications
+    </p>
+  </div>
+</template>
+
+<script>
+import Table from '@jac-uk/jac-kit/components/Table/Table';
+import TableCell from '@jac-uk/jac-kit/components/Table/TableCell';
+
+export default {
+  components: {
+    Table,
+    TableCell,
+  },
+  props: {
+    candidateId: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      tableColumns: [
+        { title: 'Exercise', sort: 'exerciseRef', direction: 'desc' },
+        { title: 'Outcome', sort: '_processing.status', direction: 'desc', default: true },
+        { title: 'Action' },
+      ],
+    };
+  },
+  computed: {
+    candidateApplications() {
+      const records = this.$store.state.candidateApplications.records;
+      return records;
+    },
+    noApplications() {
+      return this.candidateApplications.length === 0;
+    },
+  },
+  methods: {
+    getTableData(params) {
+      const queryParams = { ...params, candidateId: this.candidateId };
+      this.$store.dispatch('candidateApplications/bind', queryParams);
+    },
+    getStage(application) {
+      return application._processing ? application._processing.stage : null;
+    },
+    getStatus(application) {
+      return application._processing ? application._processing.status : application.status;
+    },
+  },
+};
+</script>
+
+<style scoped>
+  td {
+    text-transform: capitalize;
+  }
+</style>
