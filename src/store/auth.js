d577806ec72b7e2c0664d74522f173ab859c44e0
Tom Russell <TR115251@hotmail.co.uk>
2023-03-23 15:09:40 +0000

    add Kiran to auth

--- src/store/auth.js
+++ src/store/auth.js
@@ -47,6 +47,7 @@ const module = {
           'seniorleader@judicialappointments.digital',
           'jacstaff@judicialappointments.digital',
           'readonly@judicialappointments.digital',
+          'kiran.Annavarapu3@justice.gov.uk',
         ].indexOf((user.email).toLowerCase()) >= 0) {
           allOk = true;
         }
