85385698e53aaf15af7862680fabaadbeda271be
halcyonjac <halcyon@judicialappointments.digital>
2023-03-30 11:25:39 +0100

    change text back

--- src/components/ModalViews/ExercisePreApprovalTaskList.vue
+++ src/components/ModalViews/ExercisePreApprovalTaskList.vue
@@ -2,25 +2,18 @@
   <div>
     <div class="modal__title text-left govuk-!-padding-2 background-blue">
       <h2 class="govuk-heading-m govuk-!-margin-bottom-0">
-        Submit for approval
+        Submit for Approval
       </h2>
     </div>
     <div class="modal__content govuk-!-margin-6">
       <div style="text-align: left;">
-        <div class="govuk-inset-text govuk-!-margin-bottom-7">
-          <p class="govuk-body">
-            Please check that the following tasks are completed before submitting for approval.
-          </p>
-        </div>
         <p class="govuk-body">
-          List of tasks to complete:
+          Please make sure all of the following tasks are completed before submitting for approval.
         </p>
         <ul class="govuk-list govuk-list--bullet">
-          <li>Task to complete number one</li>
-          <li>Task number two to complete before submitting</li>
-          <li>This is the task number three to do.</li>
-          <li>The fourth task to make</li>
-          <li>Number fifth</li>
+          <li>Has the information page and application form (if relevant) been cleared by the JAC Policy and Diversity Teams?</li>
+          <li>Has the information page and application form (if relevant) been cleared by the JAC Digital Team?</li>
+          <li>Has the information page and application form (if relevant) been cleared by the Head of Operations and Digital?</li>
         </ul>
         <button
           type="button"
@@ -34,7 +27,7 @@
           class="govuk-button"
           @click="confirmModal"
         >
-          Submit for approval
+          Submit for Approval
         </button>
       </div>
     </div>
