b2c40a6ea139be8a3ac34043164776434d1baabe
halcyonjac <halcyon@judicialappointments.digital>
2023-04-06 10:01:13 +0100

    add hint

--- src/views/Exercise/Details/Contacts/Edit.vue
+++ src/views/Exercise/Details/Contacts/Edit.vue
@@ -40,6 +40,8 @@
           id="exercise-email-signature-name"
           v-model="formData.emailSignatureName"
           label="Email signature name"
+          hint="Enter Selection Exercise Manager name here"
+          type=""
           required
         />
 
