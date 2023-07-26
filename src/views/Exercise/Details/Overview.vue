b50bdcb433731efd41df11313570832cc1b595b7
drieJAC <jac.drie@digitalteam.uk>
2023-07-26 10:22:45 +0100

    #2045 Application Process (#2055)
    * Esnure Application Process always appears in Sidebar List.
    Ensure Submit For Approval button is always displayed.
    Ensure Application Process appears in task list.
    
    * Put the checks for the Save And Continue button in the Overview page back in and removed the checks for the Continue button in the Application Process page.
    
    * Remove unnecessary code which monitors whether the app process list has changed

--- src/views/Exercise/Details/Overview.vue
+++ src/views/Exercise/Details/Overview.vue
@@ -347,6 +347,7 @@ export default {
           data.push({ title: 'Working preferences', id: 'exercise-details-preferences', done: this.exerciseProgress.workingPreferences, approved: this.approvalProgress['workingPreferences'] });
           data.push({ title: 'Assessment options', id: 'exercise-details-assessments', done: this.exerciseProgress.assessmentOptions, approved: this.approvalProgress['assessmentOptions'] });
           data.push({ title: 'Exercise downloads', id: 'exercise-details-downloads', done: this.exerciseProgress.downloads, approved: this.approvalProgress['downloads'] });
+          data.push({ title: 'Application process', id: 'exercise-details-application-content', done: this.exerciseProgress.applicationProcess, approved: this.approvalProgress['applicationProcess'] });
           if (this.exercise.inviteOnly) {
             data.splice(1, 0, { title: 'Exercise invitations', id: 'exercise-details-invitations' , done: this.exerciseProgress.invitations, approved: this.approvalProgress['invitations'] });
           }
