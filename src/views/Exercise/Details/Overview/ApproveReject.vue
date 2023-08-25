9f7c5d7cfcb72da08d57d0d90d9016711dfba352
drieJac <jac.drie@digitalteam.uk>
2023-08-25 11:25:17 +0100

    Added compare functions into JAC-KIT and now using them here. Removed old compare functions that were in here. New jac-kit functions fix the bug whereby we were getting multiple occurences of the same field changed and date and timestamp compares werent working properly.

--- src/views/Exercise/Details/Overview/ApproveReject.vue
+++ src/views/Exercise/Details/Overview/ApproveReject.vue
@@ -67,6 +67,7 @@
 <script>
 import permissionMixin from '@/permissionMixin';
 import { isReadyForApprovalFromAdvertType } from '@/helpers/exerciseHelper';
+import { deepKeysDiff } from '@jac-uk/jac-kit/helpers/object';
 import { mapState } from 'vuex';
 import _has from 'lodash/has.js';
 import _get from 'lodash/get.js';
@@ -116,7 +117,7 @@ export default {
   },
   mounted() {
     this.$store.dispatch('vacancy/bind', this.exerciseId).then(() => {
-      this.changes = this.getDeepObjectDifferences(this.vacancy, this.exercise, ['state']);
+      this.changes = deepKeysDiff(this.vacancy, this.exercise, ['state']);
     });
   },
   methods: {
@@ -129,49 +130,6 @@ export default {
     },
     reject() {
       this.$emit('reject');
-    },
-    getDeepObjectDifferences(obj1, obj2, keysToIgnore) {
-      const differences = [];
-      this.compareObjects(obj1, obj2, [], differences, keysToIgnore);
-      return differences;
-    },
-    compareObjects(obj1, obj2, path = [], differences = [], keysToIgnore = []) {
-      for (const key in obj1) {
-        if (obj1.hasOwnProperty(key) && obj2.hasOwnProperty(key)) {
-          const newPath = [...path, key];
-          if (keysToIgnore.includes(key) || key.charAt(0) === '_') {
-            // If the key is in the keysToIgnore array or begins with an underscore, skip the comparison
-            continue;
-          }
-          //if (this.isObject(obj1[key]) && this.isObject(obj2[key]) && !this.isDate(obj2[key])) {
-          if (this.isObject(obj1[key]) && this.isObject(obj2[key])) {
-            this.compareObjects(obj1[key], obj2[key], newPath, differences, keysToIgnore);
-          } else if (!this.isEqual(obj1[key], obj2[key])) {
-            // If the values are different, store the top-level key in the differences array
-            differences.push(newPath[0]);
-          }
-        }
-      }
-    },
-    // Compare two values
-    isEqual(value1, value2) {
-      if (typeof value1 !== typeof value2) return false;
-      if (typeof value1 === 'object') {
-        // Use JSON.stringify to handle nested arrays and objects
-        return JSON.stringify(value1) === JSON.stringify(value2);
-      }
-      return value1 === value2;
-    },
-    isObject(input) {
-      return typeof input === 'object' &&
-        !Array.isArray(input) &&
-        input !== null &&
-        !this.isDate(input);  // Ignore Firestore timestamp objects
-    },
-    isDate(input) {
-      if (Object.prototype.toString.call(input) === '[object Date]')
-        return true;
-      return false;
     },
   },
 };
