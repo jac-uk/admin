a2fc17324a2de90f1d60fa6f80eb08dd218a9f80
halcyonjac <halcyon@judicialappointments.digital>
2023-10-22 22:45:06 +0100

    make commissioner conflicts editable

---
+++ src/views/InformationReview/CommissionerConflictsSummary.vue
@@ -0,0 +1,124 @@
+<template>
+  <div>
+    <div
+      v-if="exercise.commissioner"
+      class="govuk-!-margin-top-9"
+    >
+      <h2 class="govuk-heading-l govuk-!-margin-bottom-4">
+        Commissioner conflicts
+      </h2>
+
+      <dl
+        v-if="commissionerConflicts || editable"
+        class="govuk-summary-list"
+      >
+        <div
+          v-for="(commissionerConflict, index) in commissionerConflicts"
+          :key="index"
+          class="govuk-summary-list__row"
+        >
+          <dt class="govuk-summary-list__key widerColumn">
+            {{ commissionerConflict.name }}
+          </dt>
+          <dd class="govuk-summary-list__value">
+            <InformationReviewRenderer
+              :data="commissionerConflict.hasRelationship"
+              field="hasRelationship"
+              :edit="editable"
+              type="selection"
+              :options="[true, false]"
+              :is-asked="isApplicationPartAsked('commissionerConflicts')"
+              @change-field="(obj) => changeCommissionerConflict(obj, index)"
+            />
+            <div
+              v-if="commissionerConflict.hasRelationship"
+              class="govuk-body"
+            >
+              <dt class="govuk-summary-list__key">
+                Details
+              </dt>
+              <InformationReviewRenderer
+                v-if="commissionerConflict.hasRelationship"
+                :data="commissionerConflict.detail"
+                field="detail"
+                :edit="editable"
+                :is-asked="isApplicationPartAsked('commissionerConflicts')"
+                @change-field="(obj) => changeCommissionerConflict(obj, index)"
+              />
+            </div>
+          </dd>
+        </div>
+      </dl>
+    </div>
+  </div>
+</template>
+
+<script>
+import InformationReviewRenderer from '@/components/Page/InformationReviewRenderer.vue';
+import { isApplicationPartAsked } from '@/helpers/exerciseHelper';
+
+export default {
+  name: 'CommissionerConflictsSummary',
+  components: {
+    InformationReviewRenderer,
+  },
+  props: {
+    application: {
+      type: Object,
+      required: true,
+      default: () => {},
+    },
+    editable: {
+      type: [Boolean, Function, Promise],
+      required: true,
+      default: false,
+    },
+  },
+  emits: ['updateApplication'],
+  computed: {
+    exercise() {
+      return this.$store.state.exerciseDocument.record;
+    },
+    commissionerConflicts() {
+      return Array.isArray(this.exercise.commissioners) && this.exercise.commissioners.map((commissioner) => {
+        const match = this.application?.additionalInfo?.commissionerConflicts.find((commissionerConflict) => {
+          return commissionerConflict.name === commissioner.name;
+        });
+        return match ? match : {
+          name: commissioner.name,
+          hasRelationship: null,
+          detail: null,
+        };
+      });
+    },
+  },
+  methods: {
+    getCommissionerConflicts(name) {
+      return this.commissionerConflicts.find((commissionerConflict) => {
+        return commissionerConflict.name === name;
+      });
+    },
+    isApplicationPartAsked(part) {
+      return isApplicationPartAsked(this.exercise, part);
+    },
+    changeCommissionerConflict(obj, index) {
+      const commissionerConflicts = this.commissionerConflicts.map((commissionerConflict, i) => {
+        return i === index ? { ...commissionerConflict, ...obj } : commissionerConflict;
+      });
+      const data = {
+        additionalInfo: {
+          ...this.application.additionalInfo,
+          commissionerConflicts,
+        },
+      };
+      this.$emit('updateApplication', data);
+    },
+  },
+};
+</script>
+
+<style scoped>
+  .widerColumn {
+    width: 70%;
+  }
+</style>
