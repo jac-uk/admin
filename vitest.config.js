3cca6f24e4c79227847cd9853394c91ad0be9568
drieJac <jac.drie@digitalteam.uk>
2023-12-12 01:41:24 +0000

    Moved commented out files into oldTests dir and excluding them from the test runner

--- vitest.config.js
+++ vitest.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
+    exclude: ['**/oldTests/**', '**/node_modules/**', '**/dist/**', '**/cypress/**', '**/.{idea,git,cache,output,temp}/**', '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*'],
   },
    root: '.', // Define the root
 });
