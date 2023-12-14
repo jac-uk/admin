32cd3e91cc7faed14c65a316a1a84be09f133a44
drieJac <jac.drie@digitalteam.uk>
2023-12-14 15:27:20 +0000

    Moved link html and css into dedicated component

--- src/App.vue
+++ src/App.vue
@@ -124,32 +124,15 @@
       <RouterView />
     </main>
 
-    <!-- Link -->
-    <div
+    <UserFeedbackLink
       v-show="isSignedIn && isMounted"
-      ref="user-feedback-link"
-      class="user-feedback-link"
-    >
-      <button
-        class="report-issue"
-        type="button"
-        @click="openModal"
-      >
-        <svg
-          viewBox="0 0 17.583 19.237"
-          class="report-issue__icon"
-        >
-          <path
-            d="m10.198,0h-2.812C3.307,0,0,3.307,0,7.385v1.522c0,2.575,1.318,4.842,3.316,6.164.246.544.613,1.098.936,1.744.524,1.047,1.689,1.99,2.425,2.402.114.064.248-.037.22-.165-.135-.617-.195-1.875-.189-2.791.223.02.449.031.677.031h2.812c4.079,0,7.386-3.307,7.386-7.385v-1.522c0-4.079-3.307-7.385-7.386-7.385Zm-.397,3.331l-.29,5.716h-1.438l-.278-5.716h2.006Zm-1.009,9.584c-.682,0-1.123-.492-1.123-1.148.013-.669.454-1.148,1.123-1.148s1.136.479,1.136,1.148c0,.656-.455,1.148-1.136,1.148Z"
-            fill="#1d70b8"
-          />
-        </svg>
-        <span class="fourteenpx">Report an issue</span>
-      </button>
-    </div>
+      :style="{ 'bottom': linkBottom }"
+    />
 
-    <!-- Modal @TODO: RENAME! -->
-    <UserFeedbackModal v-if="isSignedIn" />
+    <UserFeedbackModal
+      v-if="isSignedIn"
+      @open-feedback-modal="openModal()"
+    />
 
     <footer
       class="govuk-footer"
@@ -222,12 +205,14 @@ import permissionMixin from '@/permissionMixin';
 import Messages from '@/components/Messages.vue';
 import UserFeedbackModal from '@/components/Feedback/UserFeedbackModal.vue';
 import _debounce from 'lodash/debounce';
+import UserFeedbackLink from '@/components/Feedback/UserFeedbackLink.vue';
 
 export default {
   name: 'App',
   components: {
     Messages,
     UserFeedbackModal,
+    UserFeedbackLink,
   },
   mixins: [permissionMixin],
   data() {
@@ -237,7 +222,8 @@ export default {
 
       // @TODO: May not need the ref of the button anymore!
 
-      buttonElement: null,
+      //buttonElement: null,
+      linkBottom: '',
       isMounted: false,
       observer: null,
     };
@@ -278,11 +264,12 @@ export default {
     }
   },
   mounted() {
-    this.buttonElement = document.querySelector('.user-feedback-link');
     this.handleDebouncedScroll = _debounce(this.calculateLinkPosition, 20);
     window.addEventListener('scroll', this.handleDebouncedScroll);
+
     // Need to observe changes to the main content div to recalculate the position of the help button
     this.bindObserver();
+
     this.isMounted = true;
   },
   beforeUnmount() {
@@ -352,26 +339,9 @@ export default {
       }
     },
     calculateLinkPosition() {
-      //  Ensure the feedback link sits above the footer when scrolled to the bottom of rthe page
-      //if (this.$refs['user-feedback-link']) {
-      if (this.buttonElement && this.isMounted) {
-        if (window.scrollY + window.innerHeight > document.documentElement.scrollHeight - 70) {
-
-          console.log('scroll 5em');
-
-          // @TODO: RENAME THE CLASS BELOW!
-
-          //this.buttonElement.style.bottom = '5em';
-          this.$refs['user-feedback-link'].style.bottom = '5em';
-        }
-        else {
-
-          console.log('scroll 1em');
-
-          //this.buttonElement.style.bottom = '1em';
-          this.$refs['user-feedback-link'].style.bottom = '1em';
-
-        }
+      // Ensure the feedback link sits above the footer when scrolled to the bottom of the page
+      if (this.isMounted) {
+        this.linkBottom = (window.scrollY + window.innerHeight > document.documentElement.scrollHeight - 70) ? '5em' : '1em';
       }
     },
   },
@@ -444,42 +414,6 @@ $jac-link-colour: #753880;
 .custom-html ol li::before {
   counter-increment: item;
   content: counters(item, '.') '. ';
-}
-
-.user-feedback-link {
-  position: fixed;
-  /*bottom: 2em;*/
-  left: 1em;
-  /*display: inline-flex;*/
-  background-color: white;
-  padding: 5px;
-}
-button.report-issue {
-    border-radius: 2em;
-    background-color: #f3f2f1;
-    padding: 0.5em 0.8em;
-    border: 1px solid #D2D2D2FF;
-    line-height: 1em;
-    align-items: center;
-    font-size: 1em !important;
-    text-align: center;
-    font-family: "GDS Transport", arial, sans-serif;
-    cursor: pointer;
-}
-button.report-issue:hover,
-button.report-issue:focus,
-button.report-issue:active {
-    background-color: #D2D2D2FF;
-}
-.fourteenpx {
-    font-size: 14px;
-}
-
-.report-issue__icon {
-    fill: #1d70b8;
-    width: 1em !important;
-    height: 1.05em !important;
-    margin-right: 0.3em;
 }
 
 </style>
--- src/components/Feedback/UserFeedbackLink.vue
+++ src/components/Feedback/UserFeedbackLink.vue
@@ -1,104 +1,61 @@
 <template>
-  <div>
-    <Modal
-      ref="feedbackModal"
-      data-html2canvas-ignore
-      class="fixed-width-modal"
+  <div class="user-feedback-link">
+    <button
+      class="report-issue"
+      type="button"
+      @click="emit('openFeedbackModal')"
     >
-      <SimpleModalInner
-        title="Raise A Support Issue With This Page"
-        @close="closeFeedbackModal(true)"
+      <svg
+        viewBox="0 0 17.583 19.237"
+        class="report-issue__icon"
       >
-        <div
-          v-if="showConfirmationModal"
-          class="body-content modal__message govuk-body-l"
-        >
-          <div>Your feedback has been forwarded to the team and you will be contacted shortly.</div>
-          <div class="govuk-!-margin-top-2">
-            Your ticket number is: <strong>{{ bugReportRefNo }}</strong>
-          </div>
-        </div>
-        <FeedbackForm
-          v-else
-          @close="closeFeedbackModal(false)"
-          @success="showConfirmationModal = true"
+        <path
+          d="m10.198,0h-2.812C3.307,0,0,3.307,0,7.385v1.522c0,2.575,1.318,4.842,3.316,6.164.246.544.613,1.098.936,1.744.524,1.047,1.689,1.99,2.425,2.402.114.064.248-.037.22-.165-.135-.617-.195-1.875-.189-2.791.223.02.449.031.677.031h2.812c4.079,0,7.386-3.307,7.386-7.385v-1.522c0-4.079-3.307-7.385-7.386-7.385Zm-.397,3.331l-.29,5.716h-1.438l-.278-5.716h2.006Zm-1.009,9.584c-.682,0-1.123-.492-1.123-1.148.013-.669.454-1.148,1.123-1.148s1.136.479,1.136,1.148c0,.656-.455,1.148-1.136,1.148Z"
+          fill="#1d70b8"
         />
-      </SimpleModalInner>
-    </Modal>
+      </svg>
+      <span class="fourteenpx">Report an issue</span>
+    </button>
   </div>
 </template>
 
-<script>
-import Modal from '@jac-uk/jac-kit/components/Modal/Modal.vue';
-import SimpleModalInner from '@/components/Feedback/SimpleModalInner.vue';
-import FeedbackForm from '@/components/Feedback/FeedbackForm.vue';
-
-export default {
-  name: 'UserFeedbackLink',
-  components: {
-    Modal,
-    SimpleModalInner,
-    FeedbackForm,
-  },
-  data() {
-    return {
-      showConfirmationModal: false,
-    };
-  },
-  computed: {
-    bugReport() {
-      return this.$store.state.bugReport.record;
-    },
-    bugReportRefNo() {
-      return this.bugReport ? this.bugReport.referenceNumber : '';
-    },
-  },
-  methods: {
-    openModal() {
-      this.$refs.feedbackModal.openModal();
-    },
-    async closeFeedbackModal(resetConfirmation) {
-      await this.$refs.feedbackModal.closeModal();
-      if (resetConfirmation) {
-        this.showConfirmationModal = false;
-      }
-    },
-    openConfirmationModal() {
-      this.$refs.feedbackModal.closeModal();
-      this.$refs.confirmFeedbackModal.openModal();
-    },
-    closeConfirmFeedbackModal() {
-      this.$refs.confirmFeedbackModal.closeModal();
-    },
-    isElementVisible(el) {
-      const rect     = el.getBoundingClientRect(),
-            vWidth   = window.innerWidth || document.documentElement.clientWidth,
-            vHeight  = window.innerHeight || document.documentElement.clientHeight,
-            efp      = function (x, y) { return document.elementFromPoint(x, y); };
-
-      // Return false if it's not in the viewport
-      if (rect.right < 0 || rect.bottom < 0
-        || rect.left > vWidth || rect.top > vHeight)
-        return false;
-
-      // Return true if any of its four corners are visible
-      return (
-        el.contains(efp(rect.left,  rect.top))
-        ||  el.contains(efp(rect.right, rect.top))
-        ||  el.contains(efp(rect.right, rect.bottom))
-        ||  el.contains(efp(rect.left,  rect.bottom))
-      );
-    },
-  },
-};
-</script>
+<script></script>
 
 <style type="text/css" rel="stylesheet/scss" lang="scss">
-/**
-* Important that this style block is not 'scoped' so that the .modal definition below applies to the modal in the sub component
-*/
-.modal {
-  width: 620px !important;
-  min-width: unset !important;
+.user-feedback-link {
+  position: fixed;
+  /*bottom: 2em;*/
+  left: 1em;
+  /*display: inline-flex;*/
+  background-color: white;
+  padding: 5px;
 }
+button.report-issue {
+    border-radius: 2em;
+    background-color: #f3f2f1;
+    padding: 0.5em 0.8em;
+    border: 1px solid #D2D2D2FF;
+    line-height: 1em;
+    align-items: center;
+    font-size: 1em !important;
+    text-align: center;
+    font-family: "GDS Transport", arial, sans-serif;
+    cursor: pointer;
+}
+button.report-issue:hover,
+button.report-issue:focus,
+button.report-issue:active {
+    background-color: #D2D2D2FF;
+}
+.fourteenpx {
+    font-size: 14px;
+}
+
+.report-issue__icon {
+    fill: #1d70b8;
+    width: 1em !important;
+    height: 1.05em !important;
+    margin-right: 0.3em;
+}
+
 </style>
