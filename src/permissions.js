a265390418f61281e2cfc488c53b61dd898075f1
drieJac <jac.drie@digitalteam.uk>
2023-12-15 12:39:04 +0000

    Remove unnecessary permissions for zenhub

--- src/permissions.js
+++ src/permissions.js
@@ -337,15 +337,6 @@ const PERMISSIONS = {
       },
     },
   },
-  zenhub: {
-    label: 'Zenhub',
-    permissions: {
-      canCreateZHIssues: {
-        label: 'Can create issues',
-        value: 'zh1',
-      },
-    },
-  },
 };
 
 export default PERMISSIONS;
