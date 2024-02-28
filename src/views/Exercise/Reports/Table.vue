02b52a0a998d535c4092fef14dd5b4eaee311ec7
Tom Russell <TR115251@hotmail.co.uk>
2024-02-28 17:59:33 +0000

    test

--- src/views/Exercise/Reports/Table.vue
+++ src/views/Exercise/Reports/Table.vue
@@ -246,12 +246,12 @@
 </template>
 
 <script>
-import Search from './Search.vue';
-import SidePanel from './SidePanel.vue';
-import Badge from './Badge.vue';
-import CustomForm from './CustomForm.vue';
-import LoadingMessage from '../LoadingMessage.vue';
-import EmptySearchResults from './EmptySearchResults.vue';
+import Search from '@jac-uk/jac-kit/components/Table/Search.vue';
+import SidePanel from '@jac-uk/jac-kit/components/Table/SidePanel.vue';
+import Badge from '@jac-uk/jac-kit/components/Table/Badge.vue';
+import CustomForm from '@jac-uk/jac-kit/components/Table/CustomForm.vue';
+import LoadingMessage from '@jac-uk/jac-kit/components/LoadingMessage.vue';
+import EmptySearchResults from '@jac-uk/jac-kit/components/Table/EmptySearchResults.vue';
 import _has from 'lodash/has';
 const ACTIONS = {
   LOAD: 'load',
