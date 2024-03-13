ad3aa3592cf15b384f8aef3ec5e2b562c82d703b
halcyonjac <halcyon@judicialappointments.digital>
2024-03-13 12:27:56 +0000

    add deployment report

---
+++ src/views/Exercise/Reports/Deployment.vue
@@ -0,0 +1,211 @@
+<template>
+  <div class="govuk-grid-column-full">
+    <div class="moj-page-header-actions">
+      <div class="moj-page-header-actions__title">
+        <h2 class="govuk-heading-l">
+          Deployment
+        </h2>
+      </div>
+
+      <div
+        class="moj-page-header-actions__actions float-right"
+      >
+        <div class="moj-button-menu">
+          <div class="moj-button-menu__wrapper">
+            <button
+              class="govuk-button govuk-button--secondary moj-button-menu__item moj-page-header-actions__action"
+              data-module="govuk-button"
+              :disabled="!hasReportData"
+              @click="exportData()"
+            >
+              Export data
+            </button>
+
+            <ActionButton
+              v-if="hasPermissions([
+                PERMISSIONS.exercises.permissions.canReadExercises.value,
+                PERMISSIONS.applicationRecords.permissions.canReadApplicationRecords.value,
+                PERMISSIONS.applications.permissions.canReadApplications.value
+              ])"
+              type="primary"
+              :action="refreshReport"
+            >
+              Refresh
+            </ActionButton>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="govuk-grid-row">
+      <div class="govuk-grid-column-one-half">
+        <div class="panel govuk-!-margin-bottom-9">
+          <span class="govuk-caption-m">
+            Approved for immediate appointment
+          </span>
+          <h2 class="govuk-heading-m govuk-!-margin-bottom-0">
+            {{ $filters.formatNumber(totalApplicationRecords) }}
+          </h2>
+        </div>
+      </div>
+      <div class="govuk-grid-column-one-half">
+        <div class="panel govuk-!-margin-bottom-9">
+          <span class="govuk-caption-m">Type of exercise</span>
+          <h2 class="govuk-heading-m govuk-!-margin-bottom-0">
+            {{ $filters.lookup(exerciseType) }}
+          </h2>
+        </div>
+      </div>
+    </div>
+
+    <Table
+      v-if="report != null"
+      data-key="id"
+      :data="report.rows"
+      :columns="tableColumns"
+      :page-size="1000"
+      local-data
+      @change="getTableData"
+    >
+      <template #row="{row}">
+        <TableCell :title="tableColumns[0].title">
+          <RouterLink
+            :to="{ name: 'exercise-application', params: { applicationId: row.applicationId } }"
+          >
+            {{ row.referenceNumber }}
+          </RouterLink>
+        </TableCell>
+        <TableCell :title="tableColumns[1].title">
+          <RouterLink
+            :to="{ name: 'candidates-view', params: { id: row.candidateId } }"
+          >
+            {{ row.fullName }}
+          </RouterLink>
+        </TableCell>
+      </template>
+    </Table>
+  </div>
+</template>
+
+<script>
+import { httpsCallable } from '@firebase/functions';
+import { onSnapshot, doc } from '@firebase/firestore';
+import { mapState } from 'vuex';
+import { firestore, functions } from '@/firebase';
+import vuexfireSerialize from '@jac-uk/jac-kit/helpers/vuexfireSerialize';
+import { downloadXLSX } from '@jac-uk/jac-kit/helpers/export';
+import Table from '@jac-uk/jac-kit/components/Table/Table.vue';
+import TableCell from '@jac-uk/jac-kit/components/Table/TableCell.vue';
+import ActionButton from '@jac-uk/jac-kit/draftComponents/ActionButton.vue';
+import { APPLICATION_STATUS } from '@jac-uk/jac-kit/helpers/constants';
+import permissionMixin from '@/permissionMixin';
+
+export default {
+  name: 'DeploymentReport',
+  components: {
+    Table,
+    TableCell,
+    ActionButton,
+  },
+  mixins: [permissionMixin],
+  data() {
+    return {
+      report: null,
+      tableColumns: [
+        {
+          title: 'Reference number',
+          class: ['govuk-!-width-one-third'],
+        },
+        { title: 'Name', sort: 'candidate.fullName', default: true },
+      ],
+    };
+  },
+  computed: {
+    ...mapState({
+      exercise: state => state.exerciseDocument.record,
+    }),
+    exerciseType() {
+      return this.exercise.typeOfExercise;
+    },
+    totalApplicationRecords() {
+      return this.report ? this.report.totalApplications : 0;
+    },
+    hasReportData() {
+      return this.report && this.report.headers;
+    },
+  },
+  created() {
+    this.unsubscribe = onSnapshot(
+      doc(firestore, `exercises/${this.exercise.id}/reports/deployment`),
+      (snap) => {
+        if (snap.exists) {
+          this.report = vuexfireSerialize(snap);
+        }
+      });
+  },
+  unmounted() {
+    if (this.unsubscribe) {
+      this.unsubscribe();
+    }
+  },
+  methods: {
+    getTableData(params) {
+      this.$store.dispatch(
+        'stageHandover/bind',
+        {
+          exerciseId: this.exercise.id,
+          status: APPLICATION_STATUS.APPROVED_FOR_IMMEDIATE_APPOINTMENT,
+          ...params,
+        }
+      );
+    },
+    async refreshReport() {
+      try {
+        await httpsCallable(functions, 'generateDeploymentReport')({ exerciseId: this.exercise.id });
+        return true;
+      } catch (error) {
+        return;
+      }
+    },
+    gatherReportData() {
+      const reportData = [];
+
+      // get headers
+      reportData.push(this.report.headers.map(header => header.title));
+
+      // get rows
+      this.report.rows.forEach((row) => {
+        reportData.push(this.report.headers.map(header => row[header.ref]));
+      });
+
+      return reportData;
+    },
+    async exportData() {
+      const title = 'Deployment Report';
+      const data = this.gatherReportData();
+      /**
+       * Make the 'Judicial experience' (column S) can display multiple lines.
+       *
+       * @link: https://github.com/dtjohnson/xlsx-populate?tab=readme-ov-file#styles-1
+       */
+      const styles = {
+        column: {
+          'S': {
+            wrapText: true,
+          },
+        },
+      };
+
+      downloadXLSX(
+        data,
+        {
+          title: `${this.exercise.referenceNumber} ${title}`,
+          sheetName: title,
+          fileName: `${this.exercise.referenceNumber} - ${title}.xlsx`,
+        },
+        styles
+      );
+    },
+  },
+};
+</script>
