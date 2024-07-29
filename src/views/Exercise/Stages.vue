d6960a29239faeb38310ff045db7e36a263e88cd
KoWeiJAC <kowei.hung@judicialappointments.digital>
2024-07-29 17:43:34 +0100

    remove stages sidebar

--- src/views/Exercise/Stages.vue
+++ src/views/Exercise/Stages.vue
@@ -1,10 +1,5 @@
 <template>
   <div class="govuk-grid-row">
-    <div class="govuk-grid-column-one-quarter print-none">
-      <SideNavigation
-        :pages="sideNavigation"
-      />
-    </div>
     <div class="exercise-stages govuk-grid-column-three-quarters print-full-width">
       <RouterView />
     </div>
@@ -12,28 +7,10 @@
 </template>
 
 <script>
-import SideNavigation from '@/components/Navigation/SideNavigation.vue';
-import { availableStages } from '../../helpers/exerciseHelper';
 
 export default {
   components: {
-    SideNavigation,
-  },
-  computed: {
-    sideNavigation() {
-      const exercise = this.$store.state.exerciseDocument.record;
-      const path = `/exercise/${exercise.id}/stages`;
-      const stages = availableStages(exercise);
-      const sideNavigation = [];
-      stages.forEach(stage => {
-        const count = (exercise._applicationRecords && exercise._applicationRecords[stage]) || 0;
-        sideNavigation.push({
-          title: `${this.$filters.lookup(stage)} (${this.$filters.formatNumber(count)})`, // TODO get label
-          path: `${path}/${stage}`,
-        });
-      });
-      return sideNavigation;
-    },
+
   },
 };
 </script>
