1025345928221a9e187199f9d9a2b1042df3e53f
Tom Russell <TR115251@hotmail.co.uk>
2024-08-13 16:41:11 +0100

    virus scanner status on releases page

--- src/views/ReleasesList.vue
+++ src/views/ReleasesList.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
-    <h1
-      class="govuk-heading-xl govuk-!-margin-bottom-6"
-    >
+    <h1 class="govuk-heading-xl govuk-!-margin-bottom-6">
       Latest Releases
     </h1>
+
+    <!-- New section for file upload status -->
+    <div>
+      <h2 class="govuk-heading-m">
+        Virus Scanner functionality: {{ fileUploadStatus }}
+      </h2>
+    </div>
+
     <Table
       data-key="id"
       :data="tableData"
@@ -35,6 +41,8 @@ import permissionMixin from '@/permissionMixin';
 import dayjs from 'dayjs';
 import Table from '@jac-uk/jac-kit/components/Table/Table.vue';
 import TableCell from '@jac-uk/jac-kit/components/Table/TableCell.vue';
+import { collection, doc, getDoc } from 'firebase/firestore';
+import { firestore } from '@/firebase';
 
 export default {
   name: 'ReleasesList',
@@ -51,15 +59,20 @@ export default {
         { title: 'Author' },
         { title: 'Published At' },
       ],
+      fileUploadEnabled: false, // To store the file upload status
     };
   },
   computed: {
     tableData() {
       return this.$store.state.releases.records;
     },
+    fileUploadStatus() {
+      return this.fileUploadEnabled ? 'Online' : 'Offline';
+    },
   },
   created() {
     this.$store.dispatch('releases/getLatestReleases');
+    this.fetchFileUploadStatus();
   },
   methods: {
     formatDate(dateStr) {
@@ -67,6 +80,28 @@ export default {
     },
     getTableData(params) {
       this.$store.dispatch('releases/getLatestReleases', params);
+    },
+    async fetchFileUploadStatus() {
+      try {
+        // Reference to the 'settings' collection
+        const settingsRef = collection(firestore, 'settings');
+
+        // Reference to the 'candidateSettings' subcollection within 'settings'
+        const candidateSettingsRef = collection(settingsRef, 'candidateSettings');
+
+        // Reference to the 'bypasses' subcollection within 'candidateSettings'
+        const bypassesRef = collection(candidateSettingsRef, 'bypasses');
+
+        // Reference to the 'fileUpload' document within 'bypasses'
+        const fileUploadDoc = doc(bypassesRef, 'fileUpload');
+
+        // Fetch the feature flag
+        const docSnapshot = await getDoc(fileUploadDoc);
+        this.fileUploadEnabled = docSnapshot.exists() ? docSnapshot.data().enabled : false;
+      } catch (error) {
+        console.error('Failed to fetch file upload status:', error);
+        this.fileUploadEnabled = false; // Default to false if there's an error
+      }
     },
   },
 };
