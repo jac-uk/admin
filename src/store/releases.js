a2fc2ddfce3e3a43285bdc600dead27a821413ae
drieJAC <jac.drie@digitalteam.uk>
2024-08-27 13:33:01 +0100

    Update releases to display last fetched date time (#2546)

--- src/store/releases.js
+++ src/store/releases.js
@@ -1,15 +1,20 @@
 import { httpsCallable } from '@firebase/functions';
 import { functions } from '@/firebase';
+import dayjs from 'dayjs';
 
 const module = {
   namespaced: true,
   state: {
     records: [],
-    lastFetched: null,
+    availability: false,
+    lastFetched: null,    // dayjs() object
   },
   mutations: {
     setRecords(state, value) {
       state.records = value;
+    },
+    setAvailability(state, value) {
+      state.availability = value;
     },
     setLastFetched(state, value) {
       state.lastFetched = value;
@@ -19,6 +24,13 @@ const module = {
     async getLatestReleases({ commit }) {
       const records = await httpsCallable(functions, 'getLatestReleases')();
       commit('setRecords', records.data);
+      commit('setAvailability', records.data.length > 0);
+      commit('setLastFetched', dayjs());
+    },
+  },
+  getters: {
+    getLastFetchedDT: (state) => {
+      return state.lastFetched ? state.lastFetched.format('YYYY-MM-DD HH:mm:ss') : '';
     },
   },
 };
