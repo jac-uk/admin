68d01a0bf23f8f6e69cd49cace01cbd171173aac
drieJac <jac.drie@digitalteam.uk>
2024-08-29 11:39:45 +0100

    Escaping values so that commas in strings do not mess up conversion to a csv. Also escaping double quotes as these are used to escape the commas. Added a helper to facilitate this. Added nowrap attribute to the keys array to flag columns which should not be wrapped when the table is displayed. This is being used in the template to prevent the td from wrapping.

---
+++ src/helpers/csv.js
@@ -0,0 +1,22 @@
+/**
+ * This function is used when converting arrays into csv strings
+ * It does the following:
+ * - Converts the value to a string using String(value)
+ * - If the value contains a comma (,), double quotes ("), or other special characters, it wraps the value in double quotes
+ * - If the value already contains double quotes, those quotes are escaped by replacing each " with ""
+ * @param {*} value
+ * @returns
+ */
+const escapeValue = (value) => {
+  // Convert the value to a string and wrap in quotes if it contains commas or quotes
+  const stringValue = String(value);
+  if (stringValue.includes(',') || stringValue.includes('"')) {
+    // Escape internal double quotes and wrap the value in quotes
+    return `"${stringValue.replace(/"/g, '""')}"`;
+  }
+  return stringValue;
+};
+
+export {
+  escapeValue
+};
