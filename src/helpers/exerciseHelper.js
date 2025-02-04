d0e9fbb7a8ae62d60120a07b3020f215d865a08a
warrensearle <warren@precise-minds.co.uk>
2025-02-04 10:55:13 +0000

    Entry status should be Shortlisting Passed

--- src/helpers/exerciseHelper.js
+++ src/helpers/exerciseHelper.js
@@ -581,6 +581,7 @@ function taskEntryStatus(exercise, type) {
   if (!exercise) return status;
   if (type === TASK_TYPE.EMP_TIEBREAKER) return APPLICATION_STATUS.SECOND_STAGE_INVITED;  // TODO: remove this eventually: override entry status for EMP tie-breakers
   if (type === TASK_TYPE.SELECTION_DAY) return APPLICATION_STATUS.SHORTLISTING_PASSED;
+  if (type === TASK_TYPE.PRE_SELECTION_DAY_QUESTIONNAIRE) return APPLICATION_STATUS.SHORTLISTING_PASSED;
   const prevTaskType = previousTaskType(exercise, type);
   if (prevTaskType) {
     switch (prevTaskType) {
